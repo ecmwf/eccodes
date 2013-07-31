cf3a27b03bc2c9c7e0f72871b5f5b49864add875
Shahram Najm <masn@ecmwf.int>
2013-07-31 14:07:01 +0100

    GRIB-411: Empty GRIB: computing the average causes division by zero

--- src/grib_accessor_class_statistics.c
+++ src/grib_accessor_class_statistics.c
@@ -139,147 +139,151 @@ static void init_class(grib_accessor_class* c)
 
 static void init(grib_accessor* a,const long l, grib_arguments* c)
 {
-  grib_accessor_statistics* self = (grib_accessor_statistics*)a;
-  int n = 0;
+    grib_accessor_statistics* self = (grib_accessor_statistics*)a;
+    int n = 0;
 
-  self->missing_value = grib_arguments_get_name(a->parent->h,c,n++);
-  self->values = grib_arguments_get_name(a->parent->h,c,n++);
-  a->flags  |= GRIB_ACCESSOR_FLAG_READ_ONLY;
-  a->flags |= GRIB_ACCESSOR_FLAG_FUNCTION;
-  a->flags |= GRIB_ACCESSOR_FLAG_HIDDEN;
+    self->missing_value = grib_arguments_get_name(a->parent->h,c,n++);
+    self->values = grib_arguments_get_name(a->parent->h,c,n++);
+    a->flags  |= GRIB_ACCESSOR_FLAG_READ_ONLY;
+    a->flags |= GRIB_ACCESSOR_FLAG_FUNCTION;
+    a->flags |= GRIB_ACCESSOR_FLAG_HIDDEN;
 
-  self->number_of_elements=8;
-  self->v=(double*)grib_context_malloc(a->parent->h->context,
-                sizeof(double)*self->number_of_elements);
+    self->number_of_elements=8;
+    self->v=(double*)grib_context_malloc(a->parent->h->context,
+            sizeof(double)*self->number_of_elements);
 
-  a->length=0;
-  a->dirty=1;
+    a->length=0;
+    a->dirty=1;
 }
 
-static int    unpack_double   (grib_accessor* a, double* val, size_t *len)
+static int unpack_double(grib_accessor* a, double* val, size_t *len)
 {
-  grib_accessor_statistics* self = (grib_accessor_statistics*)a;
-  int ret = 0,i=0;
-  double* values;
-  size_t size=0;
-  double max,min,avg,sd,value,skew,kurt,x;
-  double missing=0;
-  long number_of_missing=0;
-  grib_context* c=a->parent->h->context;
-  grib_handle* h=a->parent->h;
+    grib_accessor_statistics* self = (grib_accessor_statistics*)a;
+    int ret = 0,i=0;
+    double* values;
+    size_t size=0;
+    double max,min,avg,sd,value,skew,kurt,x;
+    double missing=0;
+    size_t number_of_missing=0;
+    grib_context* c=a->parent->h->context;
+    grib_handle* h=a->parent->h;
 
-  if (!a->dirty) return GRIB_SUCCESS;
+    if (!a->dirty) return GRIB_SUCCESS;
 
-  if ( (ret=grib_get_size(h,self->values,&size)) != GRIB_SUCCESS) return ret;
+    if ( (ret=grib_get_size(h,self->values,&size)) != GRIB_SUCCESS) return ret;
 
-  grib_context_log(a->parent->h->context,GRIB_LOG_DEBUG,
-    "grib_accessor_statistics: computing statistics for %d values",size);
+    grib_context_log(a->parent->h->context,GRIB_LOG_DEBUG,
+            "grib_accessor_statistics: computing statistics for %d values",size);
 
-  if((ret=grib_get_double(a->parent->h,self->missing_value,&missing))
-       != GRIB_SUCCESS) return ret;
+    if((ret=grib_get_double(a->parent->h,self->missing_value,&missing))
+            != GRIB_SUCCESS) return ret;
 
-  values=(double*)grib_context_malloc_clear(c,size*sizeof(double));
-  if (!values) return GRIB_OUT_OF_MEMORY;
+    values=(double*)grib_context_malloc_clear(c,size*sizeof(double));
+    if (!values) return GRIB_OUT_OF_MEMORY;
 
-  if((ret = grib_get_double_array_internal(h,self->values,values,&size))
-       != GRIB_SUCCESS) {
+    if((ret = grib_get_double_array_internal(h,self->values,values,&size))
+            != GRIB_SUCCESS) {
         grib_context_free(c,values);
         return ret;
-  }
-
-  number_of_missing=0;
-  i=0;
-  while (i<size && values[i] == missing ) {i++;number_of_missing++;}
-  max=values[i];
-  min=values[i];
-  avg=values[i];
-  for (i=number_of_missing+1;i<size;i++) {
-    value=values[i];
-    if (value > max && value != missing) max=value; 
-    if (value < min && value != missing) min=value; 
-    if (value != missing) avg+=value;                      
-    else number_of_missing++;
-  }
-
-  avg/=(size-number_of_missing);
-
-  sd=0; skew=0; kurt=0;
-  for (i=0;i<size;i++) {
-    if (values[i] != missing) {
-      x=(avg-values[i])*(avg-values[i]);
-      sd+=x;
-      x*=(avg-values[i]);
-      skew+=x;
-      kurt+=x*(avg-values[i]);
     }
-  }
 
-  if (size-number_of_missing!=0)
-	  sd=sqrt(sd/(size-number_of_missing));
-  
-  a->dirty=0;
+    number_of_missing=0;
+    i=0;
+    while (i<size && values[i] == missing ) {i++;number_of_missing++;}
+    max=values[i];
+    min=values[i];
+    avg=values[i];
+    for (i=number_of_missing+1;i<size;i++) {
+        value=values[i];
+        if (value > max && value != missing) max=value;
+        if (value < min && value != missing) min=value;
+        if (value != missing) avg+=value;
+        else number_of_missing++;
+    }
 
-  grib_context_free(c,values);
+    /* Don't divide by zero if all values are missing! */
+    if (size != number_of_missing) {
+        avg/=(size-number_of_missing);
+    }
 
-  self->v[0]=max;
-  self->v[1]=min;
-  self->v[2]=avg;
-  self->v[3]=number_of_missing;
-  self->v[4]=sd;
-  self->v[5]=skew;
-  self->v[6]=kurt;
-  self->v[7]= sd == 0 ? 1 : 0;
+    sd=0; skew=0; kurt=0;
+    for (i=0;i<size;i++) {
+        if (values[i] != missing) {
+            x=(avg-values[i])*(avg-values[i]);
+            sd+=x;
+            x*=(avg-values[i]);
+            skew+=x;
+            kurt+=x*(avg-values[i]);
+        }
+    }
 
-  for (i=0;i<self->number_of_elements;i++)
-    val[i]=self->v[i];
-  
-  return ret;
+    if (size-number_of_missing!=0)
+        sd=sqrt(sd/(size-number_of_missing));
+
+    a->dirty=0;
+
+    grib_context_free(c,values);
+
+    self->v[0]=max;
+    self->v[1]=min;
+    self->v[2]=avg;
+    self->v[3]=number_of_missing;
+    self->v[4]=sd;
+    self->v[5]=skew;
+    self->v[6]=kurt;
+    self->v[7]= sd == 0 ? 1 : 0;
+
+    for (i=0;i<self->number_of_elements;i++)
+        val[i]=self->v[i];
+
+    return ret;
 }
 
-static long value_count(grib_accessor* a) {
-  grib_accessor_statistics* self = (grib_accessor_statistics*)a;
-  return self->number_of_elements;
+static long value_count(grib_accessor* a)
+{
+    grib_accessor_statistics* self = (grib_accessor_statistics*)a;
+    return self->number_of_elements;
 }
 
 static void destroy(grib_context* c,grib_accessor* a)
 {
-  grib_accessor_statistics* self = (grib_accessor_statistics*)a;
-  grib_context_free(c,self->v);
+    grib_accessor_statistics* self = (grib_accessor_statistics*)a;
+    grib_context_free(c,self->v);
 }
 
-static int compare(grib_accessor* a, grib_accessor* b) {
-  int retval=0;
-  double *aval=0;
-  double *bval=0;
+static int compare(grib_accessor* a, grib_accessor* b)
+{
+    int retval=0;
+    double *aval=0;
+    double *bval=0;
 
-  size_t alen = (size_t)grib_value_count(a);
-  size_t blen = (size_t)grib_value_count(b);
+    size_t alen = (size_t)grib_value_count(a);
+    size_t blen = (size_t)grib_value_count(b);
 
-  if (alen != blen) return GRIB_COUNT_MISMATCH;
+    if (alen != blen) return GRIB_COUNT_MISMATCH;
 
-  aval=grib_context_malloc(a->parent->h->context,alen*sizeof(double));
-  bval=grib_context_malloc(b->parent->h->context,blen*sizeof(double));
+    aval=grib_context_malloc(a->parent->h->context,alen*sizeof(double));
+    bval=grib_context_malloc(b->parent->h->context,blen*sizeof(double));
 
-  b->dirty=1;
-  a->dirty=1;
+    b->dirty=1;
+    a->dirty=1;
 
-  grib_unpack_double(a,aval,&alen);
-  grib_unpack_double(b,bval,&blen);
+    grib_unpack_double(a,aval,&alen);
+    grib_unpack_double(b,bval,&blen);
 
-  retval = GRIB_SUCCESS;
-  while (alen != 0) {
-    if (*bval != *aval) retval = GRIB_DOUBLE_VALUE_MISMATCH;
-    alen--;
-  }
+    retval = GRIB_SUCCESS;
+    while (alen != 0) {
+        if (*bval != *aval) retval = GRIB_DOUBLE_VALUE_MISMATCH;
+        alen--;
+    }
 
-  grib_context_free(a->parent->h->context,aval);
-  grib_context_free(b->parent->h->context,bval);
+    grib_context_free(a->parent->h->context,aval);
+    grib_context_free(b->parent->h->context,bval);
 
-  return GRIB_SUCCESS;
+    return GRIB_SUCCESS;
 }
 
-static int unpack_string(grib_accessor*a , char*  v, size_t *len){
-	return GRIB_NOT_IMPLEMENTED;
+static int unpack_string(grib_accessor*a , char*  v, size_t *len)
+{
+    return GRIB_NOT_IMPLEMENTED;
 }
-
-
