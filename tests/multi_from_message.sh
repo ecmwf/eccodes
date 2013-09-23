f1de57d184c07a0d4403d5627bc80429ed33efa4
Shahram Najm <masn@ecmwf.int>
2013-09-23 15:56:08 +0100

    Reformatting

--- tests/multi_from_message.sh
+++ tests/multi_from_message.sh
@@ -16,23 +16,20 @@ out2=${data_dir}/multi2.out
 
 for infile in ${data_dir}/multi.grib2 ${data_dir}/reduced_gaussian_model_level.grib1
 do
+  if [ ! -f ${infile} ]
+  then
+    echo no data to test
+    exit 0
+  fi
 
-if [ ! -f ${infile} ]
-then
-  echo no data to test
-  exit 0
-fi
+  ${test_dir}/multi_from_message -m $infile > $out1
+  ${tools_dir}grib_get -p count,shortName,level $infile > $out2
+  diff $out1 $out2
 
-${test_dir}/multi_from_message -m $infile > $out1
-${tools_dir}grib_get -p count,shortName,level $infile > $out2
+  ${test_dir}/multi_from_message $infile > $out1
+  ${tools_dir}grib_get -M -p count,shortName,level $infile > $out2
+  diff $out1 $out2
 
-diff $out1 $out2
-
-${test_dir}/multi_from_message $infile > $out1
-${tools_dir}grib_get -M -p count,shortName,level $infile > $out2
-
-diff $out1 $out2
-
-rm -f $out1 $out2 | true
+  rm -f $out1 $out2 | true
 
 done
