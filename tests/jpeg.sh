450b5a399fbc7a31f9ec13595eb060293c1c4e03
Shahram Najm <masn@ecmwf.int>
2013-10-21 10:44:36 +0100

    Improve tests for JPEG packing

--- tests/jpeg.sh
+++ tests/jpeg.sh
@@ -12,15 +12,16 @@
 . ./include.sh
 
 REDIRECT=/dev/null
+BLACKLIST="totalLength,section5Length,section7Length,dataRepresentationTemplateNumber,typeOfPacking"
 
 infile=${data_dir}/jpeg.grib2
 
 rm -f $infile.1 $infile.2 || true
 
 ${tools_dir}grib_set -s packingType=grid_simple $infile $infile.1
-${tools_dir}grib_compare -P -b totalLength,section5Length,section7Length,dataRepresentationTemplateNumber,typeOfPacking,typeOfCompressionUsed,targetCompressionRatio $infile $infile.1 > $REDIRECT
+${tools_dir}grib_compare -P -b $BLACKLIST,typeOfCompressionUsed,targetCompressionRatio $infile $infile.1 > $REDIRECT
 ${tools_dir}grib_set -s packingType=grid_jpeg $infile.1 $infile.2
-${tools_dir}grib_compare -P -b totalLength,section5Length,section7Length,dataRepresentationTemplateNumber,typeOfPacking $infile.1 $infile.2 > $REDIRECT
+${tools_dir}grib_compare -P -b $BLACKLIST $infile.1 $infile.2 > $REDIRECT
 
 templateNumber=`${tools_dir}grib_get -p dataRepresentationTemplateNumber $infile.2`
 
@@ -35,8 +36,14 @@ rm -f $infile.1 $infile.2 || true
 infile=${data_dir}/reduced_latlon_surface.grib2
 
 ${tools_dir}grib_set -s packingType=grid_jpeg $infile $infile.1
-${tools_dir}grib_compare -P -b totalLength,section5Length,section7Length,dataRepresentationTemplateNumber,typeOfPacking $infile $infile.1 > $REDIRECT
+${tools_dir}grib_compare -P -b $BLACKLIST $infile $infile.1 > $REDIRECT
 ${tools_dir}grib_set -s packingType=grid_simple $infile.1 $infile.2
-${tools_dir}grib_compare -P -b totalLength,section5Length,section7Length,dataRepresentationTemplateNumber,typeOfPacking,typeOfCompressionUsed,targetCompressionRatio $infile.1 $infile.2  > $REDIRECT
+${tools_dir}grib_compare -P -b $BLACKLIST,typeOfCompressionUsed,targetCompressionRatio $infile.1 $infile.2  > $REDIRECT
+
+res1=`${tools_dir}grib_get '-F%1.2f' -p min,max,avg $infile`
+res2=`${tools_dir}grib_get '-F%1.2f' -p min,max,avg $infile.1`
+res3=`${tools_dir}grib_get '-F%1.2f' -p min,max,avg $infile.2`
+[ "$res1" = "$res2" ]
+[ "$res1" = "$res3" ]
 
 rm -f $infile.1 $infile.2 || true
