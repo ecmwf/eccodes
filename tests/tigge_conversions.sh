b228421510745debe3568604d5aa7e59a38efa78
Shahram Najm <masn@ecmwf.int>
2013-11-14 12:38:04 +0000

    Fix comments/indentation

--- tests/tigge_conversions.sh
+++ tests/tigge_conversions.sh
@@ -16,8 +16,6 @@
 
 REDIRECT=/dev/null
 
-#set -eax 
-
 dir="${data_dir}/tigge/"
 temp1="temp.grib1_"
 temp2="temp.grib2_"
@@ -32,15 +30,15 @@ exclude=`echo $file | awk " /$exclusion_pattern/ {print \"found\";} "`
 
 if [ -z "$exclude" ]; then
 
-	rm -f ${temp1} ${temp2} || true
+   rm -f ${temp1} ${temp2} || true
 
-    # 1 to 2 conversion check
-	${tools_dir}grib_set -s editionNumber=1 ${file} ${temp1} 2> $REDIRECT > $REDIRECT
-	${tools_dir}grib_compare -P -c data:n,geography:n ${temp1} ${file} 2> $REDIRECT > $REDIRECT
+   # 2 to 1 conversion check
+   ${tools_dir}grib_set -s editionNumber=1 ${file} ${temp1} 2> $REDIRECT > $REDIRECT
+   ${tools_dir}grib_compare -P -c data:n,geography:n ${temp1} ${file} 2> $REDIRECT > $REDIRECT
 
-    # 2 to 1 conversion check
-    ${tools_dir}grib_set -s editionNumber=2 ${temp1} ${temp2} 2> $REDIRECT > $REDIRECT
-	${tools_dir}grib_compare -P -c shortName,data:n,geography:n ${temp2} ${file} 2> $REDIRECT > $REDIRECT
+   # 1 to 2 conversion check
+   ${tools_dir}grib_set -s editionNumber=2 ${temp1} ${temp2} 2> $REDIRECT > $REDIRECT
+   ${tools_dir}grib_compare -P -c shortName,data:n,geography:n ${temp2} ${file} 2> $REDIRECT > $REDIRECT
 fi
 done
 
