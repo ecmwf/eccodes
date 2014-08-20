88962a93ccbe493f73c05a0884db4dca9bb428bf
Shahram Najm <masn@ecmwf.int>
2014-08-20 16:38:51 +0100

    GRIB-574: Polar stereographic support (Part 1)

--- src/grib_iterator_class_polar_stereographic.c
+++ src/grib_iterator_class_polar_stereographic.c
@@ -8,11 +8,6 @@
  * virtue of its status as an intergovernmental organisation nor does it submit to any jurisdiction.
  */
 
-/**************************************
- *  Enrico Fucile
- **************************************/
-
-
 #include "grib_api_internal.h"
 #include <math.h>
 
@@ -88,192 +83,175 @@ static void init_class(grib_iterator_class* c)
 
 static int next(grib_iterator* i, double *lat, double *lon, double *val)
 {
+    grib_iterator_polar_stereographic* self = (grib_iterator_polar_stereographic*)i;
 
-  grib_iterator_polar_stereographic* self = (grib_iterator_polar_stereographic*)i;
+    if((long)i->e >= (long)(i->nv-1))
+        return 0;
+    i->e++;
 
-  if((long)i->e >= (long)(i->nv-1))
-    return 0;
-  i->e++;
+    *lat = self->lats[i->e];
+    *lon = self->lons[i->e];
+    *val = i->data[i->e];
 
-  *lat = self->lats[i->e];
-  *lon = self->lons[i->e];
-  *val = i->data[i->e];
-
-  return 1;
+    return 1;
 }
 
+#define RAD2DEG   57.29577951308232087684  /* 180 over pi */
+#define DEG2RAD   0.01745329251994329576   /* pi over 180 */
 
 static int init(grib_iterator* iter,grib_handle* h,grib_arguments* args)
 {
-  int ret=0;
-  double *lats,*lons;
-  double lonFirstInDegrees,latFirstInDegrees,lonFirst,latFirst,radius=0;
-  long nx,ny,standardParallel,centralLongitude;
-  double phi,lambda0,xFirst,yFirst,x,y,Dx,Dy;
-  double kp,sinphi1,cosphi1,sinlambda0,coslambda0;
-  long alternativeRowScanning,iScansNegatively;
-  long jScansPositively,jPointsAreConsecutive;
-  double sinphi,cosphi,sinlambda,coslambda,cosdlambda,sindlambda;
-  double cosc,sinc;
-  long i,j;
-  
+    int ret=0;
+    double *lats,*lons;
+    double lonFirstInDegrees,latFirstInDegrees,lonFirst,latFirst,radius=0;
+    long nx,ny,standardParallel,centralLongitude;
+    double phi,lambda0,xFirst,yFirst,x,y,Dx,Dy;
+    double k,sinphi1,cosphi1;
+    long alternativeRowScanning,iScansNegatively;
+    long jScansPositively,jPointsAreConsecutive;
+    double sinphi,cosphi,cosdlambda,sindlambda;
+    double cosc,sinc;
+    long i,j;
 
-  grib_iterator_polar_stereographic* self =
-      (grib_iterator_polar_stereographic*)iter;
+    grib_iterator_polar_stereographic* self = (grib_iterator_polar_stereographic*)iter;
 
-  const char* sradius           = grib_arguments_get_name(h,args,self->carg++);
-  const char* snx               = grib_arguments_get_name(h,args,self->carg++);
-  const char* sny               = grib_arguments_get_name(h,args,self->carg++);
-  const char* slatFirstInDegrees = grib_arguments_get_name(h,args,self->carg++);
-  const char* slonFirstInDegrees = grib_arguments_get_name(h,args,self->carg++);
-  const char* sstandardParallel = grib_arguments_get_name(h,args,self->carg++);
-  const char* scentralLongitude = grib_arguments_get_name(h,args,self->carg++);
-  const char* sDx = grib_arguments_get_name(h,args,self->carg++);
-  const char* sDy = grib_arguments_get_name(h,args,self->carg++);
-  const char* siScansNegatively = grib_arguments_get_name(h,args,self->carg++);
-  const char* sjScansPositively = grib_arguments_get_name(h,args,self->carg++);
-  const char* sjPointsAreConsecutive = grib_arguments_get_name(h,args,self->carg++);
-  const char* salternativeRowScanning = grib_arguments_get_name(h,args,self->carg++);
-  double c,rho;
-  double d2r=acos(0.0)/90.0;
-  double pi4=acos(0.0)/2.0;
-  sinphi1 = cosphi1 = phi = 0.0;  /*TODO initialize properly*/
-  
-  if((ret = grib_get_double_internal(h, sradius,&radius)) != GRIB_SUCCESS)
-    return ret;
-  if((ret = grib_get_long_internal(h, snx,&nx)) != GRIB_SUCCESS)
-    return ret;
-  if((ret = grib_get_long_internal(h, sny,&ny)) != GRIB_SUCCESS)
-    return ret;
+    const char* sradius                 = grib_arguments_get_name(h,args,self->carg++);
+    const char* snx                     = grib_arguments_get_name(h,args,self->carg++);
+    const char* sny                     = grib_arguments_get_name(h,args,self->carg++);
+    const char* slatFirstInDegrees      = grib_arguments_get_name(h,args,self->carg++);
+    const char* slonFirstInDegrees      = grib_arguments_get_name(h,args,self->carg++);
+    const char* sstandardParallel       = grib_arguments_get_name(h,args,self->carg++);
+    const char* scentralLongitude       = grib_arguments_get_name(h,args,self->carg++);
+    const char* sDx                     = grib_arguments_get_name(h,args,self->carg++);
+    const char* sDy                     = grib_arguments_get_name(h,args,self->carg++);
+    const char* siScansNegatively       = grib_arguments_get_name(h,args,self->carg++);
+    const char* sjScansPositively       = grib_arguments_get_name(h,args,self->carg++);
+    const char* sjPointsAreConsecutive  = grib_arguments_get_name(h,args,self->carg++);
+    const char* salternativeRowScanning = grib_arguments_get_name(h,args,self->carg++);
+    double c,rho;
+    /*double pi4=acos(0.0)/2.0;*/
+    sinphi1 = cosphi1 = phi = 0.0;
 
-  if (iter->nv!=nx*ny) {
-    grib_context_log(h->context,GRIB_LOG_ERROR,
-                     "Wrong number of points (%ld!=%ldx%ld)",
-                     iter->nv,nx,ny);
-    return GRIB_WRONG_GRID;
-  }
-  if((ret = grib_get_double_internal(h, slatFirstInDegrees,&latFirstInDegrees))
-      != GRIB_SUCCESS)
-    return ret;
-  if((ret = grib_get_double_internal(h, slonFirstInDegrees,&lonFirstInDegrees))
-      != GRIB_SUCCESS)
-    return ret;
-  if((ret = grib_get_long_internal(h, sstandardParallel,&standardParallel))
-      != GRIB_SUCCESS)
-    return ret;
-  if((ret = grib_get_long_internal(h, scentralLongitude,&centralLongitude))
-      != GRIB_SUCCESS)
-    return ret;
-  if((ret = grib_get_double_internal(h, sDx,&Dx)) != GRIB_SUCCESS)
-    return ret;
-  if((ret = grib_get_double_internal(h, sDy,&Dy)) != GRIB_SUCCESS)
-    return ret;
-  if((ret = grib_get_long_internal(h,
-      sjPointsAreConsecutive,&jPointsAreConsecutive))
-      != GRIB_SUCCESS)
-    return ret;
-  if((ret = grib_get_long_internal(h, sjScansPositively,&jScansPositively))
-      != GRIB_SUCCESS)
-    return ret;
-  if((ret = grib_get_long_internal(h, siScansNegatively,&iScansNegatively))
-      != GRIB_SUCCESS)
-    return ret;
-  if((ret = grib_get_long_internal(h,
-      salternativeRowScanning,&alternativeRowScanning))
-      != GRIB_SUCCESS)
-    return ret;
+    if((ret = grib_get_double_internal(h, sradius,&radius)) != GRIB_SUCCESS)
+        return ret;
+    if((ret = grib_get_long_internal(h, snx,&nx)) != GRIB_SUCCESS)
+        return ret;
+    if((ret = grib_get_long_internal(h, sny,&ny)) != GRIB_SUCCESS)
+        return ret;
 
-  lambda0=d2r*standardParallel;
-  latFirst=latFirstInDegrees*d2r;
-  lonFirst=lonFirstInDegrees*d2r;
-
-  coslambda0=cos(lambda0);
-  sinlambda0=sin(lambda0);
-
-  Dx = iScansNegatively == 0 ? Dx : -Dx;
-  Dy = jScansPositively == 1 ? Dy : -Dy;
-  self->lats = grib_context_malloc(h->context,iter->nv*sizeof(double));
-  if (!self->lats) {
-    grib_context_log(h->context,GRIB_LOG_ERROR,
-                     "unable to allocate %ld bytes",iter->nv*sizeof(double));
-    return GRIB_OUT_OF_MEMORY;
-  }
-  self->lons = grib_context_malloc(h->context,iter->nv*sizeof(double));
-  if (!self->lats) {
-    grib_context_log(h->context,GRIB_LOG_ERROR,
-                     "unable to allocate %ld bytes",iter->nv*sizeof(double));
-    return GRIB_OUT_OF_MEMORY;
-  }
-  lats=self->lats;
-  lons=self->lons;
-
-  /* compute xFirst,yFirst in metres */
-  sinphi=sin(latFirst);
-  cosphi=cos(latFirst);
-  sinlambda=sin(lonFirst);
-  coslambda=cos(lonFirst);
-  cosdlambda=cos(lonFirst-lambda0);
-  sindlambda=sin(lonFirst-lambda0);
-  kp=radius*2.0*tan(pi4-phi/2);
-  xFirst=kp*cosphi*sindlambda;
-  yFirst=-kp*cosphi*cosdlambda;
-  
-  if (jPointsAreConsecutive) {
-  
-    x=xFirst;
-    for (i=0;i<nx;i++) {
-      y=yFirst;   
-      for (j=0;j<ny;j++) {
-        rho=sqrt(x*x+y*y);
-        c=2*atan2(rho,(2.0*radius));
-        cosc=cos(c);
-        sinc=sin(c);
-        *lats=asin(cosc)/d2r;
-        *lons=(lambda0+atan2(x*sinc,rho*cosphi1*cosc-y*sinphi1*sinc))/d2r;
-        if (*lons<0) *lons+=360;
-        lons++;
-        lats++;
-
-        y+=Dy;
-      }
-      x+=Dx;
+    if (iter->nv!=nx*ny) {
+        grib_context_log(h->context,GRIB_LOG_ERROR, "Wrong number of points (%ld!=%ldx%ld)", iter->nv,nx,ny);
+        return GRIB_WRONG_GRID;
     }
-    
-  } else {
+    if((ret = grib_get_double_internal(h, slatFirstInDegrees,&latFirstInDegrees)) != GRIB_SUCCESS)
+        return ret;
+    if((ret = grib_get_double_internal(h, slonFirstInDegrees,&lonFirstInDegrees)) != GRIB_SUCCESS)
+        return ret;
+    if((ret = grib_get_long_internal(h, sstandardParallel,&standardParallel)) != GRIB_SUCCESS)
+        return ret;
+    if((ret = grib_get_long_internal(h, scentralLongitude,&centralLongitude)) != GRIB_SUCCESS)
+        return ret;
+    if((ret = grib_get_double_internal(h, sDx,&Dx)) != GRIB_SUCCESS)
+        return ret;
+    if((ret = grib_get_double_internal(h, sDy,&Dy)) != GRIB_SUCCESS)
+        return ret;
+    if((ret = grib_get_long_internal(h, sjPointsAreConsecutive,&jPointsAreConsecutive)) != GRIB_SUCCESS)
+        return ret;
+    if((ret = grib_get_long_internal(h, sjScansPositively,&jScansPositively)) != GRIB_SUCCESS)
+        return ret;
+    if((ret = grib_get_long_internal(h, siScansNegatively,&iScansNegatively)) != GRIB_SUCCESS)
+        return ret;
+    if((ret = grib_get_long_internal(h, salternativeRowScanning,&alternativeRowScanning)) != GRIB_SUCCESS)
+        return ret;
 
-    y=yFirst;
-    for (j=0;j<ny;j++) {
-      x=xFirst;
-      for (i=0;i<nx;i++) {
-        rho=sqrt(x*x+y*y);
-        c=2*atan2(rho,(2.0*radius));
-        cosc=cos(c);
-        sinc=sin(c);
-        *lats=asin(cosc*sinphi1+y*sinc*cosphi1/rho)/d2r;
-        *lons=(lambda0+atan2(x*sinc,rho*cosphi1*cosc-y*sinphi1*sinc))/d2r;
-        if (*lons<0) *lons+=360;
-        lons++;
-        lats++;
+    sinphi1 = sin(standardParallel*DEG2RAD);
+    cosphi1 = cos(standardParallel*DEG2RAD);
+    lambda0 = centralLongitude*DEG2RAD;
+    latFirst= latFirstInDegrees*DEG2RAD;
+    lonFirst= lonFirstInDegrees*DEG2RAD;
 
-        x+=Dx;
-      }
-      y+=Dy;
+    Dx = iScansNegatively == 0 ? Dx : -Dx;
+    Dy = jScansPositively == 1 ? Dy : -Dy;
+    self->lats = grib_context_malloc(h->context,iter->nv*sizeof(double));
+    if (!self->lats) {
+        grib_context_log(h->context,GRIB_LOG_ERROR, "unable to allocate %ld bytes",iter->nv*sizeof(double));
+        return GRIB_OUT_OF_MEMORY;
     }
+    self->lons = grib_context_malloc(h->context,iter->nv*sizeof(double));
+    if (!self->lats) {
+        grib_context_log(h->context,GRIB_LOG_ERROR, "unable to allocate %ld bytes",iter->nv*sizeof(double));
+        return GRIB_OUT_OF_MEMORY;
+    }
+    lats=self->lats;
+    lons=self->lons;
 
-  }
-  
-  iter->e = -1;
+    /* compute xFirst,yFirst in metres */
+    sinphi=sin(latFirst);
+    cosphi=cos(latFirst);
+    cosdlambda=cos(lonFirst-lambda0);
+    sindlambda=sin(lonFirst-lambda0);
 
-  return ret;
+    k = 2.0 * radius / ( 1 + sinphi1*sinphi + cosphi1*cosphi*cosdlambda );
+    xFirst = k * cosphi * sindlambda;
+    yFirst = k * (cosphi1*sinphi - sinphi1*cosphi*cosdlambda);
+
+    /*kp=radius*2.0*tan(pi4-phi/2);
+    xFirst=kp*cosphi*sindlambda;
+    yFirst=-kp*cosphi*cosdlambda;*/
+
+    if (jPointsAreConsecutive)
+    {
+        x=xFirst;
+        for (i=0;i<nx;i++) {
+            y=yFirst;
+            for (j=0;j<ny;j++) {
+                rho=sqrt(x*x+y*y);
+                c=2*atan2(rho,(2.0*radius));
+                cosc=cos(c);
+                sinc=sin(c);
+                *lats = asin( cosc*sinphi1 + y*sinc*cosphi1/rho ) * RAD2DEG;
+                *lons = (lambda0+atan2(x*sinc, rho*cosphi1*cosc - y*sinphi1*sinc)) * RAD2DEG;
+                if (*lons<0) *lons+=360;
+                lons++;
+                lats++;
+
+                y+=Dy;
+            }
+            x+=Dx;
+        }
+    }
+    else
+    {
+        y=yFirst;
+        for (j=0;j<ny;j++) {
+            x=xFirst;
+            for (i=0;i<nx;i++) {
+                rho=sqrt(x*x+y*y);
+                c=2*atan2(rho,(2.0*radius));
+                cosc=cos(c);
+                sinc=sin(c);
+                *lats = asin( cosc*sinphi1 + y*sinc*cosphi1/rho ) * RAD2DEG;
+                *lons = (lambda0+atan2(x*sinc, rho*cosphi1*cosc - y*sinphi1*sinc)) * RAD2DEG;
+                if (*lons<0) *lons+=360;
+                lons++;
+                lats++;
+
+                x+=Dx;
+            }
+            y+=Dy;
+        }
+    }
+    iter->e = -1;
+
+    return ret;
 }
 
 static int destroy(grib_iterator* i)
 {
-  grib_iterator_polar_stereographic* self = (grib_iterator_polar_stereographic*)i;
-  const grib_context *c = i->h->context;
+    grib_iterator_polar_stereographic* self = (grib_iterator_polar_stereographic*)i;
+    const grib_context *c = i->h->context;
 
-  grib_context_free(c,self->lats);
-  grib_context_free(c,self->lons);
-  return 1;
+    grib_context_free(c,self->lats);
+    grib_context_free(c,self->lons);
+    return 1;
 }
-
