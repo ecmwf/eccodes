cd823e0b3f65f69697bb89fdc8868311c33b83dd
Shahram Najm <masn@ecmwf.int>
2014-10-24 17:36:19 +0100

    GRIB-618: Add test for grib_compare -r (messages not in same order)

--- tests/compare.sh
+++ tests/compare.sh
@@ -3,7 +3,7 @@
 #
 # This software is licensed under the terms of the Apache Licence Version 2.0
 # which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
-# 
+#
 # In applying this licence, ECMWF does not waive the privileges and immunities granted to it by
 # virtue of its status as an intergovernmental organisation nor does it submit to any jurisdiction.
 #
@@ -22,4 +22,15 @@ ${tools_dir}grib_set -s shortName=2d $infile $outfile
 
 ${tools_dir}grib_compare -b indicatorOfParameter,paramId,shortName $infile $outfile > $REDIRECT
 
-rm -f $outfile || true
+# Test the -r switch
+infile=${data_dir}/v.grib2
+for i in 1 2 3; do
+  ${tools_dir}grib_copy -wcount=$i $infile temp_comp.$i
+done
+cat temp_comp.1 temp_comp.2 temp_comp.3 > temp_comp.123
+cat temp_comp.3 temp_comp.2 temp_comp.1 > temp_comp.321
+
+# Compare files in which the messages are not in the same order
+${tools_dir}grib_compare -r temp_comp.123 temp_comp.321
+
+rm -f temp_comp.1 temp_comp.2 temp_comp.3 temp_comp.123 temp_comp.321
