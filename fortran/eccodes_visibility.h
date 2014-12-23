751016946230f4ef28c4241121fe9f5043d1bb16
Shahram Najm <masn@ecmwf.int>
2014-12-23 14:09:38 +0000

    ecCodes/ECC-1: install eccodes.h and add new fortran include

---
+++ fortran/eccodes_visibility.h
@@ -0,0 +1,26 @@
+public :: codes_get, codes_set, codes_set_force, codes_get_data, codes_is_missing, codes_is_defined
+public :: codes_open_file, codes_close_file,codes_read_bytes,codes_write_bytes
+public :: codes_multi_support_on, codes_multi_support_off
+public :: codes_keys_iterator_new, &
+          codes_keys_iterator_next, &
+          codes_keys_iterator_delete
+public :: codes_skip_computed, &
+          codes_skip_coded, &
+          codes_skip_duplicates, &
+          codes_skip_read_only
+public :: codes_keys_iterator_get_name, &
+          codes_keys_iterator_rewind
+public :: codes_new_from_message, &
+          codes_new_from_samples, codes_new_from_file, &
+		  codes_read_from_file,codes_headers_only_new_from_file
+public :: codes_release
+public :: codes_dump
+public :: codes_get_error_string
+public :: codes_get_size
+public :: codes_get_message_size, codes_copy_message
+public :: codes_write, codes_multi_append
+public :: codes_check
+public :: codes_clone, codes_copy_namespace
+public :: codes_index_get,codes_index_select,&
+          codes_index_create,codes_index_get_size,codes_index_release,&
+		  codes_util_sections_copy
