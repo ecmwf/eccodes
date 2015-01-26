d3e0029c3bd7b00bc1877ad4bdb8b338a083c1d2
Shahram Najm <masn@ecmwf.int>
2015-01-26 17:08:10 +0000

    GRIB-527: prepare for parallel test runs

--- tests/debug.sh
+++ tests/debug.sh
@@ -8,13 +8,72 @@
 # virtue of its status as an intergovernmental organisation nor does it submit to any jurisdiction.
 #
 
-
 . ./include.sh
 
 REDIRECT=/dev/null
 
-for file in ${data_dir}/*.grib2 ${data_dir}/*.grib1
-do
-${tools_dir}grib_dump -O $file 2> $REDIRECT > $REDIRECT
+files="
+constant_field.grib1
+lfpw.grib1
+missing_field.grib1
+reduced_gaussian_lsm.grib1
+reduced_gaussian_model_level.grib1
+reduced_gaussian_pressure_level.grib1
+reduced_gaussian_pressure_level_constant.grib1
+reduced_gaussian_sub_area.grib1
+reduced_gaussian_surface.grib1
+reduced_latlon_surface.grib1
+reduced_latlon_surface_constant.grib1
+reference_ensemble_mean.grib1
+reference_stdev.grib1
+regular_gaussian_model_level.grib1
+regular_gaussian_pressure_level.grib1
+regular_gaussian_pressure_level_constant.grib1
+regular_gaussian_surface.grib1
+regular_latlon_surface.grib1
+regular_latlon_surface_constant.grib1
+second_ord_rbr.grib1
+small_ensemble.grib1
+spectral_compex.grib1
+spectral_complex.grib1
+spherical_model_level.grib1
+spherical_pressure_level.grib1
+test.grib1
+
+constant_field.grib2
+gfs.c255.grib2
+jpeg.grib2
+missing.grib2
+multi.grib2
+multi_created.grib2
+reduced_gaussian_model_level.grib2
+reduced_gaussian_pressure_level.grib2
+reduced_gaussian_pressure_level_constant.grib2
+reduced_gaussian_sub_area.grib2
+reduced_gaussian_surface.grib2
+reduced_gaussian_surface_jpeg.grib2
+reduced_latlon_surface.grib2
+reduced_latlon_surface_constant.grib2
+regular_gaussian_model_level.grib2
+regular_gaussian_pressure_level.grib2
+regular_gaussian_pressure_level_constant.grib2
+regular_gaussian_surface.grib2
+regular_latlon_surface.grib2
+regular_latlon_surface_constant.grib2
+sample.grib2
+spherical_model_level.grib2
+spherical_pressure_level.grib2
+test_uuid.grib2
+tigge_af_ecmwf.grib2
+tigge_cf_ecmwf.grib2
+tigge_ecmwf.grib2
+tigge_pf_ecmwf.grib2
+v.grib2
+"
+
+for file in $files; do
+   if [ -f ${data_dir}/$file ]; then
+      ${tools_dir}grib_dump -O ${data_dir}/$file 2> $REDIRECT > $REDIRECT
+   fi
 done
 
--- tests/include.sh
+++ tests/include.sh
@@ -22,6 +22,12 @@ else
   # save current working dir
   save=`pwd`
 
+  # If this environment variable is set, then become verbose
+  # so one can see why and how a test failed
+  if test "x$ECCODES_TEST_VERBOSE_OUTPUT" != "x"; then
+     set -x
+  fi
+
   if [ -z "${data_dir}" ]
   then
     cd ../
