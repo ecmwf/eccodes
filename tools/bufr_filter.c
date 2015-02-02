7f0dfe13078e6dabcd8223af3028189ea82a0387
Shahram Najm <masn@ecmwf.int>
2015-02-02 14:46:15 +0000

    ECC-35: bufr_filter does not fail with a non-zero exit value for non-existent keys

--- tools/bufr_filter.c
+++ tools/bufr_filter.c
@@ -11,9 +11,6 @@
 /*
  * C Implementation: bufr_filter
  *
- * Author: Enrico Fucile <enrico.fucile@ecmwf.int>
- *
- *
  */
 
 #include "grib_tools.h"
@@ -38,17 +35,21 @@ char* grib_tool_description="Apply the rules defined in rules_file to each BUFR 
 char* grib_tool_name="bufr_filter";
 char* grib_tool_usage="[options] rules_file "
         "file file ...";
-int fail=0;
+
 int grib_options_count=sizeof(grib_options)/sizeof(grib_option);
 
-int main(int argc, char *argv[]) { return grib_tool(argc,argv);}
+int main(int argc, char *argv[])
+{
+    return grib_tool(argc,argv);
+}
 
-int grib_tool_before_getopt(grib_runtime_options* options) {
+int grib_tool_before_getopt(grib_runtime_options* options)
+{
     return 0;
 }
 
-int grib_tool_init(grib_runtime_options* options) {
-
+int grib_tool_init(grib_runtime_options* options)
+{
     options->action = grib_action_from_filter(options->infile_extra->name);
     if (!options->action) {
         fprintf(stderr,"%s: error unable to create action\n",options->infile_extra->name);
@@ -61,15 +62,18 @@ int grib_tool_init(grib_runtime_options* options) {
     return 0;
 }
 
-int grib_tool_new_filename_action(grib_runtime_options* options,const char* file) {
+int grib_tool_new_filename_action(grib_runtime_options* options,const char* file)
+{
     return 0;
 }  
 
-int grib_tool_new_file_action(grib_runtime_options* options,grib_tools_file* file) {
+int grib_tool_new_file_action(grib_runtime_options* options,grib_tools_file* file)
+{
     return 0;
 }
 
-int grib_tool_new_handle_action(grib_runtime_options* options, grib_handle* h) {
+int grib_tool_new_handle_action(grib_runtime_options* options, grib_handle* h)
+{
     int err=0;
 
     if (options->current_infile->name) {
@@ -78,23 +82,33 @@ int grib_tool_new_handle_action(grib_runtime_options* options, grib_handle* h) {
     }
 
     err=grib_handle_apply_action(h,options->action);
-    if (err != GRIB_SUCCESS && options->fail && err!=GRIB_NOT_FOUND) {
+    if (err != GRIB_SUCCESS && options->fail) {
         printf("ERROR: %s\n",grib_get_error_message(err));
         exit(err);
     }
     return 0;
 }
 
-int grib_tool_skip_handle(grib_runtime_options* options, grib_handle* h) {
+int grib_tool_skip_handle(grib_runtime_options* options, grib_handle* h)
+{
     grib_handle_delete(h);
     return 0;
 }
 
-void grib_tool_print_key_values(grib_runtime_options* options,grib_handle* h) {
+void grib_tool_print_key_values(grib_runtime_options* options,grib_handle* h)
+{
     grib_print_key_values(options,h);
 }
 
-int grib_tool_finalise_action(grib_runtime_options* options) {
+int grib_tool_finalise_action(grib_runtime_options* options)
+{
+    int err=0;
+    grib_file_close_all(&err);
+    if (err != GRIB_SUCCESS) {
+        perror(grib_tool_name);
+        exit(err);
+    }
+
     grib_file_pool_clean();
     return 0;
 }
