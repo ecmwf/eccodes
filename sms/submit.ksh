8198764240828946e3227e56f792875a9410d828
Shahram Najm <masn@ecmwf.int>
2015-02-10 22:50:50 +0000

    Removed ancient sms stuff

--- sms/submit.ksh
+++
@@ -1,5 +0,0 @@
-#!/bin/ksh
-set -e
-job=$1
-out=$2
-$job > $out 2>&1 &
--- sms/test_gribs/smsinclude/tail.h
+++
@@ -1,4 +0,0 @@
-smscomplete  # Notify SMS of a normal end
-trap 0       # Remove all traps
-. /usr/local/share/ecmwf/share/.epilog
-exit 0       # End the shell
