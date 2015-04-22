ce4635c3be9d90702c5c3c9df03da09f9e81a549
Shahram Najm <masn@ecmwf.int>
2015-04-22 12:49:18 +0100

    Python example: Added shell wrapper script

---
+++ examples/python/gts_get_keys.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# Copyright 2005-2015 ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# 
+# In applying this licence, ECMWF does not waive the privileges and immunities granted to it by
+# virtue of its status as an intergovernmental organisation nor does it submit to any jurisdiction.
+#
+
+. ./include.sh
+
+
+#Define a common label for all the tmp files
+label="gts_get_keys_test_p"
+
+#Define tmp file
+fTmp=${label}.tmp.txt
+rm -f $fTmp | true
+
+REDIRECT=/dev/null
+
+#Write the key values into a file
+$PYTHON gts_get_keys.py  2> $REDIRECT > $fTmp
+
+#TODO: check the results
+
+#Clean up
+rm -f $fTmp | true
