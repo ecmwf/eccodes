418bf818579acbe0aa357507395f2626fc322ba9
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2015-04-28 09:43:26 +0100

    intermediate gaussian

--- src/mir/repres/RegularLL.cc
+++ src/mir/repres/RegularLL.cc
@@ -51,7 +51,7 @@ void RegularLL::setNiNj() {
     if(long(ni) != ni) {
         eckit::StrStream os;
         os << "RegularLL: cannot compute Ni: east=" << bbox_.east() << ", west=" << bbox_.west()
-        << ", increment=" << increments_.west_east() << ", ni=" << ni << std::endl;
+        << ", increment=" << increments_.west_east() << ", ni=" << ni <<  ", diff=" << (long(ni) - ni) << std::endl;
         throw eckit::SeriousBug(std::string(os));
     }
     ni_ = ni + 1;
@@ -61,7 +61,7 @@ void RegularLL::setNiNj() {
     if(long(nj) != nj) {
         eckit::StrStream os;
         os << "RegularLL: cannot compute Nj: north=" << bbox_.north() << ", south=" << bbox_.south()
-        << ", increment=" << increments_.north_south() << ", nj=" << nj << std::endl;
+        << ", increment=" << increments_.north_south() << ", nj=" << nj << ", diff=" << (long(nj) - nj) << std::endl;
         throw eckit::SeriousBug(std::string(os));
     }
     nj_ = nj + 1;
