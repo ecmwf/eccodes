d3f4466d0e6140de2565209bf084ab0c3495005f
Shahram Najm <masn@ecmwf.int>
2015-05-15 10:21:54 +0100

    Bamboo updates

---
+++ bamboo/macosx1010-flags.cmake
@@ -0,0 +1,2 @@
+# All python tests fail due to error loading the SWIG generated extension module
+SET( ENABLE_PYTHON  OFF CACHE BOOL "Disable Python")
--- bamboo/opensuse131-CLANG-flags.cmake
+++ bamboo/opensuse131-CLANG-flags.cmake
@@ -1,1 +1,7 @@
-SET( CMAKE_C_FLAGS "-fsanitize=thread" CACHE STRING "Enable clang thread sanitizer" ) 
+#SET( CMAKE_C_FLAGS "-fsanitize=thread" CACHE STRING "Enable clang thread sanitizer" ) 
+
+# Compilation fails for fortran bindings and clang
+#SET( ENABLE_FORTRAN OFF CACHE BOOL "Disable Fortran")
+
+# All python tests fails due to error loading the shared python lib: undefined symbol: __tsan_init
+#SET( ENABLE_PYTHON  OFF CACHE BOOL "Disable Python")
