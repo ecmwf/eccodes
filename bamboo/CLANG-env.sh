bb1229ec134695f6309954857faa2c5702a29bc5
Shahram Najm <masn@ecmwf.int>
2015-07-28 11:14:00 +0100

    bamboo: no module environment on the Mac

--- bamboo/CLANG-env.sh
+++ bamboo/CLANG-env.sh
@@ -1,3 +1,5 @@
+# No module environment on the Mac
+[[ $(uname) -eq "Darwin" ]] && return
 # Initialise module environment if it is not
 if [[ ! $(command -v module > /dev/null 2>&1) ]]; then
   . /usr/local/apps/module/init/bash
