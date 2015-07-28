393fa18b3be7e85d746585e5fd94f476a95b54a3
Shahram Najm <masn@ecmwf.int>
2015-07-28 11:34:46 +0100

    bamboo: fix comparison operator in CLANG-env

--- bamboo/CLANG-env.sh
+++ bamboo/CLANG-env.sh
@@ -1,5 +1,5 @@
 # No module environment on the Mac
-[[ $(uname) -eq "Darwin" ]] && return
+[[ $(uname) == "Darwin" ]] && return
 # Initialise module environment if it is not
 if [[ ! $(command -v module > /dev/null 2>&1) ]]; then
   . /usr/local/apps/module/init/bash
