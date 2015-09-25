c2fc690c66906ffb9fe38719a226e3706740339b
Shahram Najm <masn@ecmwf.int>
2015-09-25 11:11:25 +0100

    Fix return code in funtion

--- src/grib_accessor_class_g1end_of_interval_monthly.c
+++ src/grib_accessor_class_g1end_of_interval_monthly.c
@@ -208,7 +208,7 @@ static void destroy(grib_context* c,grib_accessor* a)
 }
 
 static int compare(grib_accessor* a, grib_accessor* b) {
-  int retval=0;
+  int retval = GRIB_SUCCESS;
   double *aval=0;
   double *bval=0;
 
@@ -236,7 +236,6 @@ static int compare(grib_accessor* a, grib_accessor* b) {
   grib_unpack_double(a,aval,&alen);
   grib_unpack_double(b,bval,&blen);
 
-  retval = GRIB_SUCCESS;
   while (alen != 0) {
     if (*bval != *aval) retval = GRIB_DOUBLE_VALUE_MISMATCH;
     alen--;
@@ -245,7 +244,7 @@ static int compare(grib_accessor* a, grib_accessor* b) {
   grib_context_free(a->parent->h->context,aval);
   grib_context_free(b->parent->h->context,bval);
 
-  return GRIB_SUCCESS;
+  return retval;
 }
 
 
