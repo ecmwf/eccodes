e315921c4dedcfa1548765aba426969d49d280f1
Shahram Najm <masn@ecmwf.int>
2015-11-04 17:22:53 +0000

    Add assert for 'missing' functor

--- src/grib_expression_class_functor.c
+++ src/grib_expression_class_functor.c
@@ -90,100 +90,102 @@ static void init_class(grib_expression_class* c)
 
 static int evaluate_long(grib_expression* g,grib_handle* h,long* lres)
 {
-	grib_expression_functor* e = (grib_expression_functor*)g;
-  
-	/*
+    grib_expression_functor* e = (grib_expression_functor*)g;
+
+    /*
 	TODO: needs OO code here
-	*/
-
-	if(strcmp(e->name,"lookup") == 0) {
-		return GRIB_SUCCESS;
-	}
-	
-	if(strcmp(e->name,"new") == 0) {
-		*lres=h->loader != NULL;
-		return GRIB_SUCCESS;
-	}
-		
-	if(strcmp(e->name,"missing") == 0)
-	{
-		const char *p = grib_arguments_get_name(h,e->args,0);
-
-		if(p)
-		{
-
-			long val = 0;
-			grib_get_long_internal(h,p,&val);
-			*lres = (val == GRIB_MISSING_LONG);
-			return GRIB_SUCCESS;
-		}
-		else
-			*lres=GRIB_MISSING_LONG;
-			return GRIB_SUCCESS;
-	}
-
-  if(strcmp(e->name,"defined") == 0)
-  {
-    const char *p = grib_arguments_get_name(h,e->args,0);
-
-    if(p)  {
-      grib_accessor* a=grib_find_accessor(h,p);
-      *lres=a!=NULL ? 1 : 0;
-      return GRIB_SUCCESS;
+     */
+    if(strcmp(e->name,"lookup") == 0) {
+        return GRIB_SUCCESS;
     }
-    *lres=0;
-    return GRIB_SUCCESS;
-  }
 
-  if(strcmp(e->name,"changed") == 0)
-  {
-    *lres=1;
-    return GRIB_SUCCESS;
-  }
+    if(strcmp(e->name,"new") == 0) {
+        *lres=h->loader != NULL;
+        return GRIB_SUCCESS;
+    }
 
-  if(strcmp(e->name,"gribex_mode_on") == 0)
-  {
-    *lres= h->context->gribex_mode_on ? 1 : 0;
-    return GRIB_SUCCESS;
-  }
-		
-	return GRIB_NOT_IMPLEMENTED;
+    if(strcmp(e->name,"missing") == 0)
+    {
+        const char *p = grib_arguments_get_name(h,e->args,0);
+        if(p)
+        {
+            long val = 0;
+            int ktype = 0;
+            grib_get_long_internal(h,p,&val);
+            if (grib_get_native_type(h, p, &ktype) == GRIB_SUCCESS) {
+                /* Currently only supported for integer keys. Not double or string */
+                Assert( ktype == GRIB_TYPE_LONG);
+            }
+            *lres = (val == GRIB_MISSING_LONG);
+            return GRIB_SUCCESS;
+        }
+        else
+            *lres=GRIB_MISSING_LONG;
+        return GRIB_SUCCESS;
+    }
+
+    if(strcmp(e->name,"defined") == 0)
+    {
+        const char *p = grib_arguments_get_name(h,e->args,0);
+
+        if(p)  {
+            grib_accessor* a=grib_find_accessor(h,p);
+            *lres=a!=NULL ? 1 : 0;
+            return GRIB_SUCCESS;
+        }
+        *lres=0;
+        return GRIB_SUCCESS;
+    }
+
+    if(strcmp(e->name,"changed") == 0)
+    {
+        *lres=1;
+        return GRIB_SUCCESS;
+    }
+
+    if(strcmp(e->name,"gribex_mode_on") == 0)
+    {
+        *lres= h->context->gribex_mode_on ? 1 : 0;
+        return GRIB_SUCCESS;
+    }
+
+    return GRIB_NOT_IMPLEMENTED;
 }
 
 static void print(grib_context* c,grib_expression* g,grib_handle* f)
 {
-	grib_expression_functor* e = (grib_expression_functor*)g;
-	printf("%s(",e->name);
-	/*grib_expression_print(c,e->args,f);*/
-	printf(")");
+    grib_expression_functor* e = (grib_expression_functor*)g;
+    printf("%s(",e->name);
+    /*grib_expression_print(c,e->args,f);*/
+    printf(")");
 }
 
 static void destroy(grib_context* c,grib_expression* g)
 {
-	grib_expression_functor* e = (grib_expression_functor*)g;
-	grib_context_free_persistent(c,e->name);
-	grib_arguments_free(c,e->args);
+    grib_expression_functor* e = (grib_expression_functor*)g;
+    grib_context_free_persistent(c,e->name);
+    grib_arguments_free(c,e->args);
 }
 
-
-static void  add_dependency(grib_expression* g, grib_accessor* observer){
-	grib_expression_functor* e = (grib_expression_functor*)g;
-	if (strcmp(e->name,"defined")) 
-		grib_dependency_observe_arguments(observer,e->args);
+static void  add_dependency(grib_expression* g, grib_accessor* observer)
+{
+    grib_expression_functor* e = (grib_expression_functor*)g;
+    if (strcmp(e->name,"defined"))
+        grib_dependency_observe_arguments(observer,e->args);
 }
 
 grib_expression* new_func_expression(grib_context* c,const char *name,grib_arguments* args)
 {
-	grib_expression_functor* e = (grib_expression_functor*)grib_context_malloc_clear_persistent(c,sizeof(grib_expression_functor));
-	e->base.cclass                 = grib_expression_class_functor;
-	e->name                   = grib_context_strdup_persistent(c,name);
-	e->args                  = args;
-	return (grib_expression*)e;
+    grib_expression_functor* e = (grib_expression_functor*)grib_context_malloc_clear_persistent(c,sizeof(grib_expression_functor));
+    e->base.cclass                 = grib_expression_class_functor;
+    e->name                   = grib_context_strdup_persistent(c,name);
+    e->args                  = args;
+    return (grib_expression*)e;
 }
 
 static void compile(grib_expression* g,grib_compiler* c)
 {
-	grib_expression_functor* e = (grib_expression_functor*)g;
+    grib_expression_functor* e = (grib_expression_functor*)g;
     fprintf(c->out,"new_func_expression(ctx,");
     fprintf(c->out,"\"%s\",",e->name);
     grib_compile_arguments(e->args,c);
@@ -192,5 +194,5 @@ static void compile(grib_expression* g,grib_compiler* c)
 
 static int native_type(grib_expression* g,grib_handle *h)
 {
-	return GRIB_TYPE_LONG;
+    return GRIB_TYPE_LONG;
 }
