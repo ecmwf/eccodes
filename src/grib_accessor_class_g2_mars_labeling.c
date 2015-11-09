bfe7e43b0aa2c8d836c02d72d4d3d3f59324adef
Shahram Najm <masn@ecmwf.int>
2015-11-09 13:30:58 +0000

    GRIB-873: Allow setting mars type to 'ssd'

--- src/grib_accessor_class_g2_mars_labeling.c
+++ src/grib_accessor_class_g2_mars_labeling.c
@@ -372,6 +372,7 @@ static int extra_set(grib_accessor* a,long val)
         case 81:	/* Forecast maximum     (fcmax) */
         case 82:	/* Forecast minimum     (fcmin) */
         case 83:	/* Forecast standard deviation  (fcstdev) */
+        case 87:	/* Simulated satellite data */
         case 88:	/* Gridded satellite data */
         case 89:	/* GFAS analysis */
             typeOfProcessedData=255;
