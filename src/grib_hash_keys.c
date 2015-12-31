45badf3729d985033b65522820071e16325fcf16
Shahram Najm <masn@ecmwf.int>
2015-12-31 12:44:51 +0000

    GRIB-902: Update copyright notices: 2015 -> 2016

--- src/grib_hash_keys.c
+++ src/grib_hash_keys.c
@@ -7186,7 +7186,7 @@ grib_keys_hash_get (const char *str, unsigned int len)
   return 0;
 }
 /*
- * Copyright 2005-2015 ECMWF.
+ * Copyright 2005-2016 ECMWF.
  *
  * This software is licensed under the terms of the Apache Licence Version 2.0
  * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
