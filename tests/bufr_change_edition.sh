a5f00816886afe6e82cc52bef4d0b95e0dea18c1
Shahram Najm <masn@ecmwf.int>
2016-02-08 18:00:41 +0000

    ECC-218: BUFR change edition tests

---
+++ tests/bufr_change_edition.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# Copyright 2005-2016 ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# 
+# In applying this licence, ECMWF does not waive the privileges and immunities granted to it by
+# virtue of its status as an intergovernmental organisation nor does it submit to any jurisdiction.
+#
+
+. ./include.sh
+
+cd ${data_dir}/bufr
+
+#Define a common label for all the tmp files
+label="bufr_change_edition"
+
+fBufrTmp=${label}".bufr.tmp"
+
+bufr_files=`cat ${data_dir}/bufr/bufr_data_files.txt`
+
+for f in ${bufr_files}; do
+  if [ "$f" = "ias1_240.bufr" ]; then continue; fi
+  if [ "$f" = "bad.bufr" ]; then      continue; fi
+  
+  # Convert to BUFR edition 4
+  ${tools_dir}bufr_set -s editionNumber=4 $f $fBufrTmp
+  ${tools_dir}bufr_compare $f $fBufrTmp
+done
+
+rm -f $fBufrTmp
