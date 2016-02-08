fd63a2ad9c8714bbb72dde1ae01d2b742ac2ad7d
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2016-02-08 19:09:20 +0000

    ECKIT-105 Add per-package trace functions

--- src/mir/output/GribOutput.cc
+++ src/mir/output/GribOutput.cc
@@ -32,7 +32,7 @@ namespace mir {
 namespace output {
 
 
-#define X(a) std::cout << "  GRIB encoding: " << #a << " = " << a << std::endl
+#define X(a) eckit::Log::trace<MIR>() << "  GRIB encoding: " << #a << " = " << a << std::endl
 
 
 class HandleFree {
@@ -76,7 +76,7 @@ void GribOutput::save(const param::MIRParametrisation &parametrisation, input::M
 
 #if 0
 
-    static const char* dump = getenv("MIR_DUMP_GRIB_HANDLES");
+    static const char *dump = getenv("MIR_DUMP_GRIB_HANDLES");
     if (dump) {
 
         static int n = 0;
@@ -133,10 +133,10 @@ void GribOutput::save(const param::MIRParametrisation &parametrisation, input::M
             // There is a bug in grib_api if the user ask 1 value and select second-order
             // Once this fixed, remove this code
             eckit::Log::trace<MIR>() << "Field has "
-                               << eckit::Plural(field.values(0).size(), "value")
-                               << ", ignoring packer "
-                               << packer
-                               << std::endl;
+                                     << eckit::Plural(field.values(0).size(), "value")
+                                     << ", ignoring packer "
+                                     << packer
+                                     << std::endl;
 
 
         } else {
@@ -146,62 +146,63 @@ void GribOutput::save(const param::MIRParametrisation &parametrisation, input::M
 #endif
     }
 
-    X(info.grid.grid_type);
-    X(info.grid.Ni);
-    X(info.grid.Nj);
-    X(info.grid.iDirectionIncrementInDegrees);
-    X(info.grid.jDirectionIncrementInDegrees);
-    X(info.grid.longitudeOfFirstGridPointInDegrees);
-    X(info.grid.longitudeOfLastGridPointInDegrees);
-    X(info.grid.latitudeOfFirstGridPointInDegrees);
-    X(info.grid.latitudeOfLastGridPointInDegrees);
-    X(info.grid.uvRelativeToGrid);
-    X(info.grid.latitudeOfSouthernPoleInDegrees);
-    X(info.grid.longitudeOfSouthernPoleInDegrees);
-    X(info.grid.iScansNegatively);
-    X(info.grid.jScansPositively);
-    X(info.grid.N);
-    X(info.grid.bitmapPresent);
-    X(info.grid.missingValue);
-    X(info.grid.pl_size);
-    for (size_t i = 0; i < info.grid.pl_size; i++) {
-        X(info.grid.pl[i]);
-        if (i > 4) break;
-    }
-
-    X(info.grid.truncation);
-    X(info.grid.orientationOfTheGridInDegrees);
-    X(info.grid.DyInMetres);
-    X(info.grid.DxInMetres);
-    X(info.packing.packing_type);
-    X(info.packing.packing);
-    X(info.packing.boustrophedonic);
-    X(info.packing.editionNumber);
-    X(info.packing.accuracy);
-    X(info.packing.bitsPerValue);
-    X(info.packing.decimalScaleFactor);
-    X(info.packing.computeLaplacianOperator);
-    X(info.packing.truncateLaplacian);
-    X(info.packing.laplacianOperator);
-    X(info.packing.deleteLocalDefinition);
-    // X(info.packing.extra_settings);
-    X(info.packing.extra_settings_count);
-    for (size_t i = 0; i < info.packing.extra_settings_count; i++) {
-        X(info.packing.extra_settings[i].name);
-        switch (info.packing.extra_settings[i].type) {
-        case GRIB_TYPE_LONG:
-            X(info.packing.extra_settings[i].long_value);
-            break;
-        case GRIB_TYPE_DOUBLE:
-            X(info.packing.extra_settings[i].double_value);
-            break;
-        case GRIB_TYPE_STRING:
-            X(info.packing.extra_settings[i].string_value);
-            break;
+    if (eckit::Log::tracing<MIR>()) {
+        X(info.grid.grid_type);
+        X(info.grid.Ni);
+        X(info.grid.Nj);
+        X(info.grid.iDirectionIncrementInDegrees);
+        X(info.grid.jDirectionIncrementInDegrees);
+        X(info.grid.longitudeOfFirstGridPointInDegrees);
+        X(info.grid.longitudeOfLastGridPointInDegrees);
+        X(info.grid.latitudeOfFirstGridPointInDegrees);
+        X(info.grid.latitudeOfLastGridPointInDegrees);
+        X(info.grid.uvRelativeToGrid);
+        X(info.grid.latitudeOfSouthernPoleInDegrees);
+        X(info.grid.longitudeOfSouthernPoleInDegrees);
+        X(info.grid.iScansNegatively);
+        X(info.grid.jScansPositively);
+        X(info.grid.N);
+        X(info.grid.bitmapPresent);
+        X(info.grid.missingValue);
+        X(info.grid.pl_size);
+        for (size_t i = 0; i < info.grid.pl_size; i++) {
+            X(info.grid.pl[i]);
+            if (i > 4) break;
         }
 
-    }
+        X(info.grid.truncation);
+        X(info.grid.orientationOfTheGridInDegrees);
+        X(info.grid.DyInMetres);
+        X(info.grid.DxInMetres);
+        X(info.packing.packing_type);
+        X(info.packing.packing);
+        X(info.packing.boustrophedonic);
+        X(info.packing.editionNumber);
+        X(info.packing.accuracy);
+        X(info.packing.bitsPerValue);
+        X(info.packing.decimalScaleFactor);
+        X(info.packing.computeLaplacianOperator);
+        X(info.packing.truncateLaplacian);
+        X(info.packing.laplacianOperator);
+        X(info.packing.deleteLocalDefinition);
+        // X(info.packing.extra_settings);
+        X(info.packing.extra_settings_count);
+        for (size_t i = 0; i < info.packing.extra_settings_count; i++) {
+            X(info.packing.extra_settings[i].name);
+            switch (info.packing.extra_settings[i].type) {
+            case GRIB_TYPE_LONG:
+                X(info.packing.extra_settings[i].long_value);
+                break;
+            case GRIB_TYPE_DOUBLE:
+                X(info.packing.extra_settings[i].double_value);
+                break;
+            case GRIB_TYPE_STRING:
+                X(info.packing.extra_settings[i].string_value);
+                break;
+            }
 
+        }
+    }
 
 
     int flags = 0;
