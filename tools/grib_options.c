1c12b01305aa58412d0ba040c174460e1ec560b9
Shahram Najm <masn@ecmwf.int>
2016-04-15 17:20:25 +0100

    Remove references to grib in bufr_filter help

--- tools/grib_options.c
+++ tools/grib_options.c
@@ -54,10 +54,10 @@ grib_options_help grib_options_help_list[] ={
   {"n:","namespace",
    "\n\t\tAll the keys belonging to namespace are printed.\n"},
   {"m",0,"Mars keys are printed.\n"},
-  {"o:","output_grib_file",
-   "\n\t\tOutput grib is written to output_grib_file."
-   "\n\t\tIf an output grib file is required and -o is not used, the"
-   " output grib is written to filtered.out\n"},
+  {"o:","output_file",
+   "\n\t\tOutput is written to output_file."
+   "\n\t\tIf an output file is required and -o is not used, the"
+   " output is written to filter.out\n"},
   {"p:","key[:{s/d/i}],key[:{s/d/i}],...",
    "\n\t\tDeclaration of keys to print."
    "\n\t\tFor each key a string (key:s), a double (key:d) or an integer (key:i)"
