1d3d67a38001d868318ea1212322ec09f4612343
Shahram Najm <masn@ecmwf.int>
2016-05-04 12:55:07 +0100

    Add copyright text

--- definitions/check_grib_defs.pl
+++ definitions/check_grib_defs.pl
@@ -1,5 +1,16 @@
 #!/usr/local/share/perl
 
+#
+# Copyright 2005-2016 ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+#
+
 #########################################################################################
 # Load in the definition files for GRIB "concepts" and check:
 #  1. They have the same number of parameters
