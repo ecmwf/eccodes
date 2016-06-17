b4561a903cd18fd045d90e5a9a64327552e6473b
Enrico Fucile <mac@ecmwf.int>
2016-06-17 09:46:44 +0100

    improved comments in example

--- examples/F90/bufr_get_string_array.f90
+++ examples/F90/bufr_get_string_array.f90
@@ -29,25 +29,35 @@ character(len=max_strsize) , dimension(:),allocatable   :: stationOrSiteName
 
   ! unpack the data values
   call codes_set(ibufr,'unpack',1)
+
+  ! get the width of the strings which is te same for all of them
   call codes_get(ibufr,'stationOrSiteName->width',strsize)
+
+  ! the width is given in bits
   strsize=strsize/8
+
+  ! max_strsize has to be set to a value >= to the size of the strings that we are getting 
+  ! back from the call to codes_get_string_array
   if (strsize > max_strsize) then
     print *,'stationOrSiteName array dimension is ',max_strsize,' and should be ',strsize
     call exit(1)
   end if
     
+  ! allocating the array of strings to be passed to codes_get_string_array is mandatory
   call codes_get_size(ibufr,'stationOrSiteName',n)
   allocate(stationOrSiteName(n))
+
   ! passing an array of strings stationOrSiteName which must be allocated beforehand 
   call codes_get_string_array(ibufr,'stationOrSiteName',stationOrSiteName)
   do i=1,n
     write(*,'(A)')trim(stationOrSiteName(i))
   end do
 
-  !remember to deallocate what was allocated by codes_get_string_array
+  !remember to deallocate 
   deallocate(stationOrSiteName)
     
-  ! release the bufr message
+  ! release memory associated with bufr handle
+  ! ibufr won't be accessible any more from codes_release on
   call codes_release(ibufr)
 
   ! close file  
