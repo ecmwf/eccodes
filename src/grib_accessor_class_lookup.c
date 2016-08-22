f2111f03e468633b0900e2efa3a8add5f15813b8
Shahram Najm <masn@ecmwf.int>
2016-08-22 10:54:47 +0100

    ECC-319: GRIBEditionNumber returns '?' if queried as a string

--- src/grib_accessor_class_lookup.c
+++ src/grib_accessor_class_lookup.c
@@ -141,127 +141,130 @@ static void init_class(grib_accessor_class* c)
 
 static void init(grib_accessor* a, const long len, grib_arguments *arg )
 {
-  grib_accessor_lookup* self = (grib_accessor_lookup*)a;
-  a->length = 0;
-  self->llength = len;
-  self->loffset = grib_arguments_get_long(grib_handle_of_accessor(a),arg,0);
-  a->flags |= GRIB_ACCESSOR_FLAG_READ_ONLY;
-  self->real_name = grib_arguments_get_expression(grib_handle_of_accessor(a),arg,1);
+    grib_accessor_lookup* self = (grib_accessor_lookup*)a;
+    a->length = 0;
+    self->llength = len;
+    self->loffset = grib_arguments_get_long(grib_handle_of_accessor(a),arg,0);
+    a->flags |= GRIB_ACCESSOR_FLAG_READ_ONLY;
+    self->real_name = grib_arguments_get_expression(grib_handle_of_accessor(a),arg,1);
 }
 
 static void post_init(grib_accessor* a)
 {
-  grib_accessor_lookup* self = (grib_accessor_lookup*)a; 
-  if(self->real_name )
-    grib_dependency_observe_expression(a,self->real_name );
-
+    grib_accessor_lookup* self = (grib_accessor_lookup*)a;
+    if(self->real_name ) {
+        grib_dependency_observe_expression(a,self->real_name );
+    }
 }
 
 static void dump(grib_accessor* a,grib_dumper* dumper)
 {
-  grib_accessor_lookup* self = (grib_accessor_lookup*)a;
-  unsigned char bytes[1024] = {0,};
-  char msg[1024]= {0,};
-  char buf[1024];
-  int i;
-  unsigned long v = 0;
+    grib_accessor_lookup* self = (grib_accessor_lookup*)a;
+    unsigned char bytes[1024] = {0,};
+    char msg[1024]= {0,};
+    char buf[1024];
+    int i;
+    unsigned long v = 0;
 
-  size_t llen = self->llength;
-  grib_unpack_bytes(a, bytes, &llen); /* TODO: Unpack byte unpack the wrong offset */
+    size_t llen = self->llength;
+    grib_unpack_bytes(a, bytes, &llen); /* TODO: Unpack byte unpack the wrong offset */
 
-  bytes[llen] = 0;
-  for(i = 0; i < llen; i++)
-  {
-    msg[i] = isprint(bytes[i]) ? bytes[i] : '?';
-    v <<= 8;
-    v |= bytes[i];
-  }
+    bytes[llen] = 0;
+    for(i = 0; i < llen; i++)
+    {
+        msg[i] = isprint(bytes[i]) ? bytes[i] : '?';
+        v <<= 8;
+        v |= bytes[i];
+    }
 
-  msg[llen] = 0;
+    msg[llen] = 0;
 
-  sprintf(buf,"%s %lu %ld-%ld", msg, v, (long)a->offset+self->loffset, (long)self->llength);
+    sprintf(buf,"%s %lu %ld-%ld", msg, v, (long)a->offset+self->loffset, (long)self->llength);
 
-  grib_dump_long(dumper,a,buf);
-
+    grib_dump_long(dumper,a,buf);
 }
 
-static int unpack_string(grib_accessor*a , char*  v, size_t *len){
+static int unpack_string(grib_accessor*a , char*  v, size_t *len)
+{
+    grib_accessor_lookup* self = (grib_accessor_lookup*)a;
+    unsigned char bytes[1024] = {0,};
+    int i;
 
-  grib_accessor_lookup* self = (grib_accessor_lookup*)a;
-  unsigned char bytes[1024] = {0,};
-  int i;
+    size_t llen = self->llength;
+    grib_unpack_bytes(a, bytes, &llen); /* TODO: Unpack byte unpack the wrong offset */
 
-  size_t llen = self->llength;
-  grib_unpack_bytes(a, bytes, &llen); /* TODO: Unpack byte unpack the wrong offset */
+    bytes[llen] = 0;
 
-  bytes[llen] = 0;
-  for(i = 0; i < llen; i++)
-  {
-    v[i] = isprint(bytes[i]) ? bytes[i] : '?';
-  }
+    for(i = 0; i < llen; i++)
+    {
+        v[i] = isprint(bytes[i]) ? bytes[i] : '?';
+    }
 
-  v[llen] = 0;
+    v[llen] = 0;
+    if (llen==1 && v[0] == '?') {
+        /* Try unpack as long */
+        size_t length = 10;
+        long lval = 0;
+        int err = unpack_long(a, &lval, &length);
+        if (!err) {
+            char str[5];
+            int conv = sprintf(str, "%ld", lval);
+            if (conv == 1) {
+                v[0] = str[0];
+            }
+        }
+    }
 
-
-  return GRIB_SUCCESS;
-
+    return GRIB_SUCCESS;
 }
 
 static int unpack_long(grib_accessor* a, long* val, size_t *len)
 {
-  grib_accessor_lookup* al = (grib_accessor_lookup*)a;
-  grib_handle *h = grib_handle_of_accessor(a);
+    grib_accessor_lookup* al = (grib_accessor_lookup*)a;
+    grib_handle *h = grib_handle_of_accessor(a);
 
+    long pos = (a->offset+al->loffset)*8;
 
-  long pos = (a->offset+al->loffset)*8;
+    if(len[0] < 1)
+    {
+        grib_context_log(a->context, GRIB_LOG_ERROR, "Wrong size for %s it contains %d values ", a->name , 1 );
+        len[0] = 0;
+        return GRIB_ARRAY_TOO_SMALL;
+    }
 
+    /* This is used when reparsing or rebuilding */
+    if(h->loader) {
+        Assert(*len == 1);
+        return h->loader->lookup_long(h->context,h->loader,a->name,val);
+    }
 
-  if(len[0] < 1)
-  {
-    grib_context_log(a->context, GRIB_LOG_ERROR, "Wrong size for %s it contains %d values ", a->name , 1 );
-    len[0] = 0;
-    return GRIB_ARRAY_TOO_SMALL;
-  }
+    val[0] = grib_decode_unsigned_long(h->buffer->data , &pos, al->llength*8);
+    len[0] = 1;
 
-  /* This is used when reparsing or rebuilding */
-  if(h->loader) {
-    Assert(*len == 1);
-    return h->loader->lookup_long(h->context,h->loader,a->name,val);
-  }
+    /*printf("###########lookup unpack_long: %s %ld %ld\n",a->name, pos/8, val[0]);*/
 
-  val[0] = grib_decode_unsigned_long(h->buffer->data , &pos, al->llength*8);
-  len[0] = 1;
-
-/* printf("lookup: %s %ld %ld\n",a->name,pos/8,val[0]); */
-
-  return GRIB_SUCCESS;
-
+    return GRIB_SUCCESS;
 }
 
-static int    pack_long   (grib_accessor* a, const long* val, size_t *len)
+static int pack_long(grib_accessor* a, const long* val, size_t *len)
 {
-  return GRIB_NOT_IMPLEMENTED;
+    return GRIB_NOT_IMPLEMENTED;
 }
-
 
 static long byte_count(grib_accessor* a)
 {
-  grib_accessor_lookup* al = (grib_accessor_lookup*)a;
-  return al->llength;
+    grib_accessor_lookup* al = (grib_accessor_lookup*)a;
+    return al->llength;
 }
 
 static long byte_offset(grib_accessor* a)
 {
-  grib_accessor_lookup* al = (grib_accessor_lookup*)a;
-  return al->loffset;
+    grib_accessor_lookup* al = (grib_accessor_lookup*)a;
+    return al->loffset;
 }
 
 static int notify_change(grib_accessor* self,grib_accessor* changed)
 {
-  /* Forward changes */
-  return grib_dependency_notify_change(self);
+    /* Forward changes */
+    return grib_dependency_notify_change(self);
 }
-
-
-
-
