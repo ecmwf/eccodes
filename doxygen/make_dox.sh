538b5a542eca7a06b79084685feab797433f913c
Shahram Najm <masn@ecmwf.int>
2016-08-26 09:46:02 +0100

    ECC-200: Script to fixup links to confluence

--- doxygen/make_dox.sh
+++ doxygen/make_dox.sh
@@ -20,48 +20,9 @@ doxygen grib_api_wiz.cfg
 # Do not copy this. Use default generated doxygen.css
 # cp doxygen.css ../html/
 
+# ./convert_links.sh
+
 # Remove temp files
 rm -f eccodes.h
 rm -f ecCodes.py
-
-# Change the links from any example code back to confluence
-fnames="
-bufr_get_keys
-grib_index
-grib_get_keys
-grib_set_keys
-grib_get_data
-grib_clone
-grib_print_data
-grib_copy_message
-count_messages
-grib_nearest
-grib_samples
-grib_set_missing
-"
-
-for fn in $fnames; do
-  perl -p -i -e "s|$fn\.f90|<a href=\"https://software.ecmwf.int/wiki/display/ECC/$fn\" target=\"_blank\">$fn.f90</a>|" ../html/classeccodes.html
-  # Do all F90 interface files too
-  perl -p -i -e "s|$fn\.f90|<a href=\"https://software.ecmwf.int/wiki/display/ECC/$fn\" target=\"_blank\">$fn.f90</a>|" ../html/interfaceeccodes*.html
-done
-
-pnames="
-grib_get_keys
-grib_set_keys
-grib_samples
-grib_set_missing
-grib_nearest
-grib_index
-grib_print_data
-grib_multi_write
-grib_iterator
-grib_clone
-grib_keys_iterator
-bufr_get_keys
-"
-for fn in $pnames; do
-  perl -p -i -e "s|$fn\.py|<a href=\"https://software.ecmwf.int/wiki/display/ECC/$fn\" target=\"_blank\">$fn.py</a>|" ../html/namespaceec_codes.html
-done
-
 echo DONE
