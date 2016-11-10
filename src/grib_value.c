e914d7ac9bd844a0c656c68819beb71125a9d2a8
Shahram Najm <masn@ecmwf.int>
2016-11-10 16:31:16 +0000

    Add assert check (in debug build)

--- src/grib_value.c
+++ src/grib_value.c
@@ -1039,6 +1039,8 @@ int grib_get_native_type(grib_handle* h, const char* name,int* type)
     grib_accessor* a =NULL;
     *type = GRIB_TYPE_UNDEFINED;
 
+    DebugAssert( name != NULL && strlen(name) > 0 );
+
     if (name[0] == '/' ) {
         al=grib_find_accessors_list(h,name);
         if (!al) return GRIB_NOT_FOUND;
