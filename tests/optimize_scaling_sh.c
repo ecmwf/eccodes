676ec3c342ed8876ebcacd6a8af823c66b3fb111
Shahram Najm <masn@ecmwf.int>
2016-11-21 15:56:34 +0000

    Fix broken test

--- tests/optimize_scaling_sh.c
+++ tests/optimize_scaling_sh.c
@@ -266,7 +266,7 @@ int main (int argc, char * argv[])
         for(i=0; i<ILCHAM; ++i)
         {
             const double diff = fabs(zval[i]-values[i]);
-            if (diff > 1e-8) {
+            if (diff > 1e-5) {
                 fprintf(stderr,"Unpacked value different: i=%d values[i]=%g zval[i]=%g\n", i, values[i], zval[i]);
                 return 1;
             }
