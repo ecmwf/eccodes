ef2eddf009d3ed94f999cfa9be6632cb297472cb
Daniel Lee <erget2005@gmail.com>
2016-12-14 10:53:20 +0100

    SUP-1861: Add high-level GRIB interface

--- python/eccodes/__init__.py
+++ python/eccodes/__init__.py
@@ -1,2 +1,8 @@
+from __future__ import absolute_import
+
 from .eccodes import *
 from .eccodes import __version__
+
+from .high_level.gribfile import GribFile
+from .high_level.gribmessage import GribMessage
+from .high_level.gribindex import GribIndex
