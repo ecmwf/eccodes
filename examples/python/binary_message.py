ae140b9fb603276641aed8ec9c17ed862d24cc2f
Shahram Najm <masn@ecmwf.int>
2017-01-03 11:24:37 +0000

    Python examples

--- examples/python/binary_message.py
+++ examples/python/binary_message.py
@@ -19,7 +19,7 @@ VERBOSE = 1  # verbose error reporting
 
 def example():
     f = open(sys.argv[1])
-    out = open(sys.argv[2], 'w')
+    fout = open(sys.argv[2], 'w')
 
     while 1:
         gid = codes_grib_new_from_file(f)
@@ -29,12 +29,12 @@ def example():
         message = codes_get_message(gid)
 
         newgid = codes_new_from_message(message)
-        codes_write(newgid, out)
+        codes_write(newgid, fout)
         codes_release(newgid)
 
         codes_release(gid)
 
-    out.close()
+    fout.close()
     f.close()
 
 
