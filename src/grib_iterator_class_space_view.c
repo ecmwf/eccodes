26961f89cb070e0ddd24de45e54a01c0cc03ea05
Shahram Najm <masn@ecmwf.int>
2017-01-04 14:23:38 +0000

    ECC-383: Clean up

--- src/grib_iterator_class_space_view.c
+++ src/grib_iterator_class_space_view.c
@@ -101,31 +101,32 @@ static int next(grib_iterator* i, double *lat, double *lon, double *val)
 
 static int init(grib_iterator* iter,grib_handle* h,grib_arguments* args)
 {
-    int ret=0;
-    double *lats,*lons;
+    /* REFERENCE:
+    *  LRIT/HRIT Global Specification (CGMS 03, Issue 2.6, 12.08.1999)
+    */
+    int ret = GRIB_SUCCESS;
+    double *lats,*lons; /* arrays of latitudes and longitudes */
     double latOfSubSatellitePointInDegrees,lonOfSubSatellitePointInDegrees;
     double orientationInDegrees, nrInRadiusOfEarth;
     double radius=0,xpInGridLengths=0,ypInGridLengths=0;
-    long nx, ny, earthIsOblate;
+    long nx, ny, earthIsOblate=0;
     long alternativeRowScanning,iScansNegatively;
-    long Xo, Yo;
-    long jScansPositively,jPointsAreConsecutive;
-    long i;
+    long Xo, Yo, jScansPositively,jPointsAreConsecutive, i;
 
-    double major, minor, r_eq, r_pol, sat_height;
+    double major, minor, r_eq, r_pol, height;
     double lap, lop, orient_angle, angular_size;
     double xp, yp, dx, dy, rx, ry, x, y;
     double cos_x, cos_y, sin_x, sin_y;
     double factor_1, factor_2, tmp1, Sd, Sn, Sxy, S1, S2, S3;
     int x0, y0, ix, iy;
-    double *s_x, *c_x;
+    double *s_x, *c_x; /* arrays storing sin and cos values */
     
     grib_iterator_space_view* self = (grib_iterator_space_view*)iter;
 
     const char* sradius                 = grib_arguments_get_name(h,args,self->carg++);
     const char* sEarthIsOblate          = grib_arguments_get_name(h,args,self->carg++);
-    const char* sMajorAxisInMetres   = grib_arguments_get_name(h,args,self->carg++);
-    const char* sMinorAxisInMetres   = grib_arguments_get_name(h,args,self->carg++);
+    const char* sMajorAxisInMetres      = grib_arguments_get_name(h,args,self->carg++);
+    const char* sMinorAxisInMetres      = grib_arguments_get_name(h,args,self->carg++);
     const char* snx                     = grib_arguments_get_name(h,args,self->carg++);
     const char* sny                     = grib_arguments_get_name(h,args,self->carg++);
     const char* sLatOfSubSatellitePointInDegrees = grib_arguments_get_name(h,args,self->carg++);
@@ -136,8 +137,8 @@ static int init(grib_iterator* iter,grib_handle* h,grib_arguments* args)
     const char* sYpInGridLengths        = grib_arguments_get_name(h,args,self->carg++);
     const char* sOrientationInDegrees   = grib_arguments_get_name(h,args,self->carg++);
     const char* sNrInRadiusOfEarth      = grib_arguments_get_name(h,args,self->carg++);
-    const char* sXo   = grib_arguments_get_name(h,args,self->carg++);
-    const char* sYo   = grib_arguments_get_name(h,args,self->carg++);
+    const char* sXo                     = grib_arguments_get_name(h,args,self->carg++);
+    const char* sYo                     = grib_arguments_get_name(h,args,self->carg++);
 
     const char* siScansNegatively       = grib_arguments_get_name(h,args,self->carg++);
     const char* sjScansPositively       = grib_arguments_get_name(h,args,self->carg++);
@@ -208,12 +209,11 @@ static int init(grib_iterator* iter,grib_handle* h,grib_arguments* args)
         r_eq = r_pol = radius * 0.001;/*conv to km*/
     }
     angular_size = 2.0 * asin(1.0 / nrInRadiusOfEarth);
-    sat_height = nrInRadiusOfEarth * r_eq;
+    height = nrInRadiusOfEarth * r_eq;
 
     lap = latOfSubSatellitePointInDegrees;
     lop = lonOfSubSatellitePointInDegrees;
-    /* apply default scaling factor */
-    lap *= 1e-6;
+    lap *= 1e-6; /* default scaling factor */
     lop *= 1e-6;
     lap *= DEG2RAD;
     lop *= DEG2RAD;
@@ -256,8 +256,9 @@ static int init(grib_iterator* iter,grib_handle* h,grib_arguments* args)
     }
     i = 0;
     factor_2 = (r_eq/r_pol)*(r_eq/r_pol);
-    factor_1 = sat_height * sat_height - r_eq * r_eq;
+    factor_1 = height * height - r_eq * r_eq;
 
+    /* Store array of sin and cosine values to avoid recalculation */
     s_x = (double *) grib_context_malloc(h->context, nx*sizeof(double));
     if (!s_x) {
         grib_context_log(h->context,GRIB_LOG_ERROR, "unable to allocate %ld bytes",nx*sizeof(double));
@@ -269,7 +270,6 @@ static int init(grib_iterator* iter,grib_handle* h,grib_arguments* args)
         return GRIB_OUT_OF_MEMORY;
     }
 
-    /* Store results of sin/cos to avoid recalculation */
     for (ix = 0; ix < nx; ix++) {
         x = (ix - xp) * rx;
         s_x[ix] = sin(x);
@@ -289,15 +289,15 @@ static int init(grib_iterator* iter,grib_handle* h,grib_arguments* args)
             /* Use sin/cos previously computed */
             sin_x = s_x[ix];  cos_x = c_x[ix];
             
-            Sd = sat_height * cos_x * cos_y;
+            Sd = height * cos_x * cos_y;
             Sd = Sd * Sd - tmp1*factor_1;
             if (Sd <= 0.0) {    // outside of view
                 lats[i] = lons[i] = 0; /* TODO: error? */
             }
             else {
                 Sd = sqrt(Sd);
-                Sn = (sat_height*cos_x*cos_y - Sd) / tmp1;
-                S1 = sat_height - Sn * cos_x * cos_y;
+                Sn = (height*cos_x*cos_y - Sd) / tmp1;
+                S1 = height - Sn * cos_x * cos_y;
                 S2 = Sn * sin_x * cos_y;
                 S3 = Sn * sin_y;
                 Sxy = sqrt(S1*S1 + S2*S2);
