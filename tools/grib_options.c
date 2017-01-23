6d9bc2a811e33b9d7a3e27507e733dae0f6f5acc
Shahram Najm <masn@ecmwf.int>
2017-01-23 10:55:33 +0000

    Documentation: on sorting numerically

--- tools/grib_options.c
+++ tools/grib_options.c
@@ -29,7 +29,7 @@ grib_options_help grib_options_help_list[] ={
      "\n\t\tAll the keys in this list are skipped in the comparison. Bit-by-bit compare on.\n"},
   {"B:","order by directive",
      "\n\t\tOrder by. The output will be ordered according to the order by directive."
-     "\n\t\tOrder by example: \"step asc, centre desc\" (step ascending and centre descending)\n"},
+     "\n\t\tOrder by example: \"step:i asc, centre desc\" (step numeric ascending and centre descending)\n"},
   {"c:","key[:i/d/s/n],key[:i/d/s/n],...",
    "\n\t\tOnly the listed keys or namespaces (:n) are compared. The optional letter after the colon is used "
    "\n\t\tto force the type in the comparison: i->integer, d->float, s->string, n->namespace."
