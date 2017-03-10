9187dc093f7765ea29263b5eb84907aa585a80e2
Shahram Najm <masn@ecmwf.int>
2017-03-10 15:48:34 +0000

    Clean up

--- examples/F90/grib_nearest.f90
+++ examples/F90/grib_nearest.f90
@@ -22,7 +22,7 @@ program find
   real(8), dimension(:), allocatable  :: distances, values, lsm_values
   integer(kind=kindOfInt), dimension(:), allocatable  :: indexes
 
-! initialization
+  ! initialization
   open( unit=1, file="../../data/list_points",form="formatted",action="read")
   read(unit=1,fmt=*) npoints
   allocate(lats(npoints))
@@ -34,27 +34,26 @@ program find
   allocate(values(npoints))
   allocate(indexes(npoints))
   do i=1,npoints
-     read(unit=1,fmt=*, iostat=ios) lats(i), lons(i)
-     if (ios /= 0) then
-        npoints = i - 1
-        exit
-     end if
+    read(unit=1,fmt=*, iostat=ios) lats(i), lons(i)
+    if (ios /= 0) then
+      npoints = i - 1
+      exit
+    end if
   end do
   close(unit=1)
   call codes_open_file(infile, &
        '../../data/reduced_gaussian_lsm.grib1','r')
 
-  !     a new grib message is loaded from file
-  !     igrib is the grib id to be used in subsequent calls
+  ! A new grib message is loaded from file
+  ! igrib is the grib id to be used in subsequent calls
   call codes_grib_new_from_file(infile,igrib)
-
 
   call codes_grib_find_nearest(igrib, .true., lats, lons, nearest_lats, nearest_lons,lsm_values, distances, indexes)
   call codes_release(igrib)
 
   call codes_close_file(infile)
 
-! will apply it to another GRIB
+  ! will apply it to another GRIB
   call codes_open_file(infile, &
        '../../data/reduced_gaussian_pressure_level.grib1','r')
   call codes_grib_new_from_file(infile,igrib)
@@ -64,7 +63,7 @@ program find
   call codes_close_file(infile)
 
   do i=1, npoints
-     print*,lats(i), lons(i), nearest_lats(i), nearest_lons(i), distances(i), lsm_values(i), values(i)
+    print*,lats(i), lons(i), nearest_lats(i), nearest_lons(i), distances(i), lsm_values(i), values(i)
   end do
 
   deallocate(lats)
