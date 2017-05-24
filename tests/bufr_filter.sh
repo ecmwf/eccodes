f26599924be715c9d5e386e8bbcb2ca827a86166
Enrico Fucile <mac@ecmwf.int>
2017-05-24 08:59:48 +0100

    ECC-482 changed variables name (JulianDate -> DateTime)

--- tests/bufr_filter.sh
+++ tests/bufr_filter.sh
@@ -1543,7 +1543,7 @@ diff ${f}.log.ref ${f}.log
 
 rm -f ${f}.log ${f}.log.ref ${f}.out $fLog $fRules
 #-----------------------------------------------------------
-# Test: Julian Date
+# Test: DateTime
 #-----------------------------------------------------------
 cat > $fRules <<EOF
 transient myStartYear=2012;
@@ -1556,30 +1556,30 @@ transient myStartSecond=0;
 transient myEndDate=20121030;
 transient myEndTime=050000;
 
-meta myStartJulianDate julian_date(myStartYear,myStartMonth,myStartDay,myStartHour,myStartMinute,myStartSecond);
-meta myEndJulianDate julian_date(myEndDate,myEndTime);
+meta myStartDateTime julian_date(myStartYear,myStartMonth,myStartDay,myStartHour,myStartMinute,myStartSecond);
+meta myEndDateTime julian_date(myEndDate,myEndTime);
 
-if (rdbtimeJulianDate > myStartJulianDate && rdbtimeJulianDate < myEndJulianDate) {
+if (rdbDateTime > myStartDateTime && rdbDateTime < myEndDateTime) {
   print "match";
 } else {
   print "no match";
 }
-print "rdbtimeDate=[rdbtimeDate] rdbtimeTime=[rdbtimeTime] rdbtimeJulianDate=[rdbtimeJulianDate%f] mystart=[myEndJulianDate] myend=[myEndJulianDate]";
+print "rdbtimeDate=[rdbtimeDate] rdbtimeTime=[rdbtimeTime] rdbDateTime=[rdbDateTime%f] mystart=[myEndDateTime] myend=[myEndDateTime]";
 
-print "rdbtimeJulianDate=[rdbtimeJulianDate:s]";
+print "rdbDateTime=[rdbDateTime:s]";
 
-set myEndJulianDate="2017-05-22 12:15:23";
-print "myEndDate=[myEndJulianDate:s] myEndJulianDate=[myEndJulianDate] myEndDate=[myEndDate] myEndTime=[myEndTime]";
+set myEndDateTime="2017-05-22 12:15:23";
+print "myEndDate=[myEndDateTime:s] myEndDateTime=[myEndDateTime] myEndDate=[myEndDate] myEndTime=[myEndTime]";
 
-set myEndJulianDate=rdbtimeJulianDate;
-print "myEndDate=[myEndJulianDate:s] myEndJulianDate=[myEndJulianDate] myEndDate=[myEndDate] myEndTime=[myEndTime]";
+set myEndDateTime=rdbDateTime;
+print "myEndDate=[myEndDateTime:s] myEndDateTime=[myEndDateTime] myEndDate=[myEndDate] myEndTime=[myEndTime]";
 
-set userJulianDateStart="2017/05/23 09-12:12";
-print "userJulianDateStart=[userJulianDateStart] userDateStart=[userDateStart] userTimeStart=[userTimeStart] [userJulianDateStart:s]";
-set userJulianDateStart="20170523 091212";
-print "userJulianDateStart=[userJulianDateStart] userDateStart=[userDateStart] userTimeStart=[userTimeStart] [userJulianDateStart:s]";
-set userJulianDateStart="20170523091212";
-print "userJulianDateStart=[userJulianDateStart] userDateStart=[userDateStart] userTimeStart=[userTimeStart] [userJulianDateStart:s]";
+set userDateTimeStart="2017/05/23 09-12:12";
+print "userDateTimeStart=[userDateTimeStart] userDateStart=[userDateStart] userTimeStart=[userTimeStart] [userDateTimeStart:s]";
+set userDateTimeStart="20170523 091212";
+print "userDateTimeStart=[userDateTimeStart] userDateStart=[userDateStart] userTimeStart=[userTimeStart] [userDateTimeStart:s]";
+set userDateTimeStart="20170523091212";
+print "userDateTimeStart=[userDateTimeStart] userDateStart=[userDateStart] userTimeStart=[userTimeStart] [userDateTimeStart:s]";
 EOF
 
 f="syno_1.bufr"
@@ -1591,13 +1591,13 @@ ${tools_dir}/bufr_filter $fRules $f  > ${f}.log
 
 cat > ${f}.log.ref <<EOF
 match
-rdbtimeDate=20121030 rdbtimeTime=001019 rdbtimeJulianDate=2456230.507164 mystart=2456230.708333 myend=2456230.708333
-rdbtimeJulianDate=20121030 001019
-myEndDate=2017-05-22 12:15:23 myEndJulianDate=2457896.01068 myEndDate=20170522 myEndTime=121523
-myEndDate=2012-10-30 00:10:19 myEndJulianDate=2456230.50716 myEndDate=20121030 myEndTime=1019
-userJulianDateStart=2457896.88347 userDateStart=20170523 userTimeStart=91212 2017/05/23 09-12:12
-userJulianDateStart=2457896.88347 userDateStart=20170523 userTimeStart=91212 20170523 091212
-userJulianDateStart=2457896.88347 userDateStart=20170523 userTimeStart=91212 20170523091212
+rdbtimeDate=20121030 rdbtimeTime=001019 rdbDateTime=2456230.507164 mystart=2456230.708333 myend=2456230.708333
+rdbDateTime=20121030 001019
+myEndDate=2017-05-22 12:15:23 myEndDateTime=2457896.01068 myEndDate=20170522 myEndTime=121523
+myEndDate=2012-10-30 00:10:19 myEndDateTime=2456230.50716 myEndDate=20121030 myEndTime=1019
+userDateTimeStart=2457896.88347 userDateStart=20170523 userTimeStart=91212 2017/05/23 09-12:12
+userDateTimeStart=2457896.88347 userDateStart=20170523 userTimeStart=91212 20170523 091212
+userDateTimeStart=2457896.88347 userDateStart=20170523 userTimeStart=91212 20170523091212
 EOF
 
 diff ${f}.log.ref ${f}.log 
