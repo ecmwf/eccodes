47695e91688d3b1e9763c09cd1fa9854ff5ae35a
Shahram Najm <masn@ecmwf.int>
2017-06-14 16:02:13 +0100

    Compiler warnings

--- src/grib_accessor_class_bufr_extract_datetime_subsets.c
+++ src/grib_accessor_class_bufr_extract_datetime_subsets.c
@@ -391,7 +391,7 @@ static int select_datetime(grib_accessor* a)
 static int pack_long(grib_accessor* a, const long* val, size_t *len)
 {
     int err=0;
-    grib_accessor_bufr_extract_datetime_subsets *self =(grib_accessor_bufr_extract_datetime_subsets*)a;
+    /*grib_accessor_bufr_extract_datetime_subsets *self =(grib_accessor_bufr_extract_datetime_subsets*)a;*/
 
     if (*len==0) return GRIB_SUCCESS;
     err=select_datetime(a);
