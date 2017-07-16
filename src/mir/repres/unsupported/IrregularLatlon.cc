5781e4436db345b099c2b122ad91f895ac8b4f16
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2017-07-16 21:49:14 +0100

    More netcdf

--- src/mir/repres/unsupported/IrregularLatlon.cc
+++ src/mir/repres/unsupported/IrregularLatlon.cc
@@ -16,10 +16,9 @@
 #include <iostream>
 
 #include "eckit/exception/Exceptions.h"
-
 #include "mir/param/MIRParametrisation.h"
-
 #include "mir/repres/unsupported/IrregularLatlon.h"
+#include "eckit/utils/MD5.h"
 
 
 namespace mir {
@@ -27,6 +26,8 @@ namespace repres {
 
 
 IrregularLatlon::IrregularLatlon(const param::MIRParametrisation &parametrisation) {
+    ASSERT(parametrisation.get("latitudes", latitudes_));
+    ASSERT(parametrisation.get("longitudes", longitudes_));
 }
 
 
@@ -37,10 +38,33 @@ IrregularLatlon::IrregularLatlon() {
 IrregularLatlon::~IrregularLatlon() {
 }
 
+size_t IrregularLatlon::numberOfPoints() const {
+    return latitudes_.size() * longitudes_.size();
+}
+
 
 void IrregularLatlon::print(std::ostream &out) const {
-    out << "IrregularLatlon["
+    out << "IrregularLatlon[latitudes=" << latitudes_.size()
+        << ",longitudes=" << longitudes_.size()
         << "]";
+}
+
+void IrregularLatlon::makeName(std::ostream& out) const {
+    out << "irregular-latlon-" << latitudes_.size() << "-" << longitudes_.size() << "-";
+    eckit::MD5 md5;
+    for (double d : latitudes_) {
+        md5 << d;
+    }
+    for (double d : longitudes_) {
+        md5 << d;
+    }
+    out << std::string(md5);
+}
+
+
+bool IrregularLatlon::sameAs(const Representation& other) const {
+    const IrregularLatlon* o = dynamic_cast<const IrregularLatlon*>(&other);
+    return o && (latitudes_ == o->latitudes_) && (longitudes_ == o->longitudes_) ;
 }
 
 
@@ -48,6 +72,21 @@ void IrregularLatlon::fill(grib_info &info) const  {
     NOTIMP;
 }
 
+
+
+atlas::Grid IrregularLatlon::atlasGrid() const {
+
+    std::vector<atlas::PointXY> *pts = new std::vector<atlas::PointXY>();
+    pts->reserve(latitudes_.size() * longitudes_.size());
+
+    for(double lat: latitudes_) {
+        for(double lon: longitudes_) {
+             pts->push_back(atlas::PointXY(lon, lat));
+        }
+    }
+
+    return atlas::grid::UnstructuredGrid(pts);
+}
 
 namespace {
 static RepresentationBuilder<IrregularLatlon> irregularLatlon("irregular_latlon"); // Name is what is returned by grib_api
