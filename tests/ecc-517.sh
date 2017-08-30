90a86d93d754007682887e0df896892f6f5bfc94
Shahram Najm <masn@ecmwf.int>
2017-08-30 11:11:16 +0100

    ECC-517: crash/error setting inputExtendedDelayedDescriptorReplicationFactor > 486

---
+++ tests/ecc-517.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Copyright 2005-2017 ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# 
+# In applying this licence, ECMWF does not waive the privileges and immunities granted to it by
+# virtue of its status as an intergovernmental organisation nor does it submit to any jurisdiction.
+#
+
+. ./include.sh
+
+# The program produces a BUFR file as output
+OUTPUT=temp.ecc-517.bufr
+${test_dir}/ecc-517 $OUTPUT
+
+rm $OUTPUT
