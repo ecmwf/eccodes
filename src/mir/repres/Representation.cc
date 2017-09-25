df540afc8dc8f0d36fb232e9edc87229c1c2a464
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2017-09-25 14:36:46 +0100

    Fix for 0037.test by removing Representation::pentagonalResolutionTs which is specific to spectral_complex, not available for spectral_simple (archived spectral data from 19930202)

--- src/mir/repres/Representation.cc
+++ src/mir/repres/Representation.cc
@@ -199,13 +199,6 @@ size_t Representation::truncation() const {
 }
 
 
-size_t Representation::pentagonalResolutionTs() const {
-    std::ostringstream os;
-    os << "Representation::pentagonalResolutionTs() not implemented for " << *this;
-    throw eckit::SeriousBug(os.str());
-}
-
-
 size_t Representation::numberOfPoints() const {
     std::ostringstream os;
     os << "Representation::numberOfPoints() not implemented for " << *this;
