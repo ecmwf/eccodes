c8ace539078a160fd00ea15f43629eaf2bd95070
Shahram Najm <masn@ecmwf.int>
2018-01-08 16:11:43 +0000

    Decode performance: speed up the BIT_MASK macro

--- src/grib_bits_any_endian.c
+++ src/grib_bits_any_endian.c
@@ -156,7 +156,7 @@ char* grib_decode_string(const unsigned char* bitStream, long *bitOffset, size_t
 /* A mask with x least-significant bits set, possibly 0 or >=32 */
 /* -1UL is 1111111... in every bit in binary representation */
 #define BIT_MASK(x) \
-        (((x) >= max_nbits) ? \
+        (((x) == max_nbits) ? \
                 (unsigned long) -1UL : (1UL << (x)) - 1)
 /**
  * decode a value consisting of nbits from an octet-bitstream to long-representation
