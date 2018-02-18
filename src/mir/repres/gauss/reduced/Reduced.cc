1b7257faaf831f0791f35c653be944353b4e5dd1
Tiago Quintino <tiago.quintino@ecmwf.int>
2018-02-18 17:26:23 +0000

    Fix license dates do we dont have to change every year

--- src/mir/repres/gauss/reduced/Reduced.cc
+++ src/mir/repres/gauss/reduced/Reduced.cc
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 1996-2016 ECMWF.
+ * (C) Copyright 1996- ECMWF.
  *
  * This software is licensed under the terms of the Apache Licence Version 2.0
  * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
--- src/mir/repres/unsupported/Mercator.cc
+++ src/mir/repres/unsupported/Mercator.cc
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 1996-2015 ECMWF.
+ * (C) Copyright 1996- ECMWF.
  *
  * This software is licensed under the terms of the Apache Licence Version 2.0
  * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
