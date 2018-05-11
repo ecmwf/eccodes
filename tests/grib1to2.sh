6900ce19b90301997a8af8796d060e9ae53fa47c
Shahram Najm <masn@ecmwf.int>
2018-05-11 16:51:17 +0100

    ECC-689: GRIB decode performance: remove redundant keys from GRIB2 messages

--- tests/grib1to2.sh
+++ tests/grib1to2.sh
@@ -51,11 +51,21 @@ do
 done
 
 # ECC-457 ECMWF total precipitation
+# ----------------------------------
 input=${data_dir}/tp_ecmwf.grib
 output=temp.grib1to2.grib
 ${tools_dir}/grib_set -s edition=2 $input $output
 res=`${tools_dir}/grib_get -w count=1 -p edition,paramId,units $output`
 [ "$res" = "2 228228 kg m**-2" ]
+rm -f $output
+
+# Local Definition 30
+# --------------------
+sample_g1=$ECCODES_SAMPLES_PATH/GRIB1.tmpl
+${tools_dir}/grib_set -s setLocalDefinition=1,localDefinitionNumber=30,stepType=accum,edition=2 $sample_g1 $output
+grib_check_key_equals $output productDefinitionTemplateNumber 11
+${tools_dir}/grib_set -s setLocalDefinition=1,localDefinitionNumber=30,type=em,stepType=accum,edition=2 $sample_g1 $output
+grib_check_key_equals $output productDefinitionTemplateNumber 12
 rm -f $output
 
 #sed "s:toolsdir:${tools_dir}/:" ${tools_dir}/grib1to2.txt > ${tools_dir}/grib1to2.test
