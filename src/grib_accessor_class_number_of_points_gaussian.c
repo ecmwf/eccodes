53fffe8c65c3b9cdebe3c0210b4aa5620d3aa6fb
Shahram Najm <masn@ecmwf.int>
2018-09-26 17:17:58 +0100

    ECC-756: All ecCodes ctests pass (Part 1)

--- src/grib_accessor_class_number_of_points_gaussian.c
+++ src/grib_accessor_class_number_of_points_gaussian.c
@@ -171,6 +171,114 @@ static void init(grib_accessor* a,const long l, grib_arguments* c)
     a->flags |= GRIB_ACCESSOR_FLAG_FUNCTION;
     a->length=0;
 }
+#if 0
+//Legacy mode
+static long num_points_reduced_gauss_old(grib_handle* h, long nj, long pl[],
+                                         long max_pl, double lats[],
+                                         double angular_precision,
+                                         double lat_first, double lat_last,
+                                         double lon_first, double lon_last)
+{
+    long result=0;
+    int is_global=0;
+    size_t plsize=0;
+    long ilon_first=0,ilon_last=0;
+    double lon_first_row=0,lon_last_row=0;
+    float d = 0;
+    is_global=is_gaussian_global(lat_first,lat_last,lon_first,lon_last,max_pl,lats,angular_precision);
+    d=fabs(lats[0]-lats[1]);
+    if ( !is_global ) {
+        long j = 0;
+        /*sub area*/
+        (void)d;
+#if EFDEBUG
+        printf("-------- subarea fabs(lat_first-lats[0])=%g d=%g\n",fabs(lat_first-lats[0]),d);
+        printf("-------- subarea fabs(lat_last+lats[0])=%g d=%g\n",fabs(lat_last+lats[0]),d);
+        printf("-------- subarea lon_last=%g order=%ld 360.0-90.0/order=%g\n",
+                lon_last,order,360.0-90.0/order);
+        printf("-------- subarea lon_first=%g fabs(lon_last  -( 360.0-90.0/order))=%g 90.0/order=%g\n",
+                lon_first,fabs(lon_last  - (360.0-90.0/order)),90.0/order);
+#endif
+        for (j=0;j<nj;j++) {
+            long row_count=0;
+#if EFDEBUG
+            printf("--  %d ",j);
+#endif
+            grib_get_reduced_row(pl[j],lon_first,lon_last,&row_count,&ilon_first,&ilon_last);
+            lon_first_row=((ilon_first)*360.0)/pl[j];
+            lon_last_row=((ilon_last)*360.0)/pl[j];
+            result += row_count;
+            (void)lon_last_row;
+            (void)lon_first_row;
+#if EFDEBUG
+            printf("        ilon_first=%ld lon_first=%.10e ilon_last=%ld lon_last=%.10e count=%ld row_count=%ld\n",
+                    ilon_first,lon_first_row,ilon_last,lon_last_row,result,row_count);
+#endif
+        }
+    } else {
+        int i = 0;
+        result=0;
+        for (i=0;i<plsize;i++) result += pl[i];
+    }
+    return result;
+}
+
+// New MIR compatible way
+static long num_points_reduced_gauss_new(grib_handle* h, long nj, long pl[], double lon_first, double lon_last)
+{
+    long result = 0;
+    long j;
+    /* Always assume sub area */
+    for (j=0;j<nj;j++) {
+        long row_count=0;
+        long ilon_first=0,ilon_last=0;
+        grib_get_reduced_row2(pl[j], lon_first, lon_last, &row_count, &ilon_first, &ilon_last);
+        //lon_first_row=((ilon_first)*360.0)/pl[j];
+        //lon_last_row=((ilon_last)*360.0)/pl[j];
+        result += row_count;
+        //(void)lon_last_row;
+        //(void)lon_first_row;
+    }
+    return result;
+}
+#endif
+
+static int angleApproximatelyEqual(double A, double B, double angular_precision)
+{
+    return angular_precision > 0 ? (fabs(A-B)<=angular_precision) : (A == B);
+}
+
+static double longitude_normalise(double lon, double minimum)
+{
+    while (lon < minimum) {
+        lon += 360;
+    }
+    while (lon >= minimum + 360) {
+        lon -= 360;
+    }
+    return lon;
+}
+
+static void correctWestEast(long max_pl, double angular_precision, double* pWest, double* pEast)
+{
+    double w, e;
+    const double inc = 360.0/max_pl; //smallest increment
+    if (*pWest > *pEast) *pEast += 360;
+
+    w = *pWest;
+    e = *pEast;
+
+    if (angleApproximatelyEqual(0, w, angular_precision)) {
+        const int cond1 = angleApproximatelyEqual(360 - inc, e - w, angular_precision);
+        const int cond2 = (360 - inc < e - w);
+        const int cond3 = (e != w);
+        const int cond4 = longitude_normalise(e,w)==w;    //e.normalise(w) == w;
+        if ( cond1 || cond2 || (cond3 && cond4) ) {
+            *pWest = 0;
+            *pEast = 360 - inc;
+        }
+    }
+}
 
 static int unpack_long(grib_accessor* a, long* val, size_t *len)
 {
@@ -247,9 +355,10 @@ static int unpack_long(grib_accessor* a, long* val, size_t *len)
 
         is_global=is_gaussian_global(lat_first,lat_last,lon_first,lon_last,max_pl,lats,angular_precision);
         d=fabs(lats[0]-lats[1]);
-        if (expandedBoundingBox(h)) {
-            is_global = 0; /* ECC-445 */
-        }
+        is_global = 0; /* ECC-445 */
+
+        correctWestEast(max_pl, angular_precision, &lon_first, &lon_last);
+
         if ( !is_global ) {
             /*sub area*/
             (void)d;
@@ -268,6 +377,9 @@ static int unpack_long(grib_accessor* a, long* val, size_t *len)
                 printf("--  %d ",j);
 #endif
                 grib_get_reduced_row_wrapper(h, pl[j],lon_first,lon_last,&row_count,&ilon_first,&ilon_last);
+                //if ( row_count != pl[j] ) {
+                //    printf("oops...... rc=%ld but pl[%d]=%ld\n", row_count, j,pl[j]);
+                //}
                 lon_first_row=((ilon_first)*360.0)/pl[j];
                 lon_last_row=((ilon_last)*360.0)/pl[j];
                 *val+=row_count;
