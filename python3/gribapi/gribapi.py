5d9cbb77589998d849bf97bf13e88329ab0234ba
Shahram Najm <masn@ecmwf.int>
2018-12-07 16:25:10 +0000

    ECC-845: Initial attempt. Use of codes_close_file()

--- python3/gribapi/gribapi.py
+++ python3/gribapi/gribapi.py
@@ -297,7 +297,7 @@ def grib_new_from_file(fileobj, headers_only=False):
     """
     fd = fileobj.fileno()
     fn = fileobj.name
-    #print('Python grib_new_from_file: ', fd,'  ', fn)
+    #print('Python gribapi.py  grib_new_from_file: ', fd,'  ', fn)
     err, gribid = _internal.grib_c_new_from_file(fileobj, fd, fn, 0, headers_only)
     if err:
         if err == _internal.GRIB_END_OF_FILE:
@@ -308,9 +308,13 @@ def grib_new_from_file(fileobj, headers_only=False):
         return gribid
 
 
-@require(fd=int, fname=str)
-def codes_close_file(fd, fname):
-    err = _internal.codes_c_close_file(fd, fname)
+@require(fileobj=file)
+def codes_close_file(fileobj):
+    # The client must call this BEFORE calling close() on the file object
+    # so we can remove the entry in our cache
+    err = _internal.codes_c_close_file(fileobj.fileno(), fileobj.name)
+    # Note: it is safe calling close() here as subsequent calls will have no effect
+    fileobj.close()
     GRIB_CHECK(err)
 
 
