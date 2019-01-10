496c8cf19277b011d1224b3974c8e75b8b973a97
Daniel Tipping <daniel@oldreliable.tech>
2019-01-10 17:10:13 +0000

    Remove dead code

--- tools/grib_tools.c
+++ tools/grib_tools.c
@@ -1140,32 +1140,8 @@ void grib_tools_write_message(grib_runtime_options* options, grib_handle* h)
     }
 
     options->outfile->file = NULL;
-
-#if 0
-    if (!options->outfile->file)  {
-        options->outfile->file = fopen(options->outfile->name,"w");
-        if(!options->outfile->file) {
-            perror(options->outfile->name);
-            exit(1);
-        }
-    }
-    GRIB_CHECK_NOLINE(grib_get_message(h,&buffer,&size),0);
-    if (options->gts && h->gts_header)
-        fwrite(h->gts_header,1,h->gts_header_len,options->outfile->file);
-
-    if(fwrite(buffer,1,size,options->outfile->file) != size)
-    {
-        perror(options->outfile->name);
-        exit(1);
-    }
-
-    if (options->gts && h->gts_header) {
-        char gts_trailer[4]={'\x0D','\x0D','\x0A','\x03'};
-        fwrite(gts_trailer,1,4,options->outfile->file);
-    }
-#endif
-
 }
+
 int exit_if_input_is_directory(const char* tool_name, const char* filename)
 {
     struct stat s;
