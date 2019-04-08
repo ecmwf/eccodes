18595a0d104b762aca676c40d244626d6e7fe55c
Shahram Najm <masn@ecmwf.int>
2019-04-08 19:57:30 +0100

    Python3 mode

--- examples/python/high_level_api.py
+++ examples/python/high_level_api.py
@@ -264,7 +264,7 @@ class TestGribMessage(unittest.TestCase):
         """Message can be serialized to file."""
         with GribFile(TESTGRIB) as grib_file:
             msg = GribMessage(grib_file)
-            with open(TEST_OUTPUT, "w") as test:
+            with open(TEST_OUTPUT, "wb") as test:
                 msg.write(test)
         os.unlink(TEST_OUTPUT)
 
@@ -388,7 +388,7 @@ class TestBufrMessage(unittest.TestCase):
         """Message can be serialized to file."""
         with BufrFile(TESTBUFR) as bufr_file:
             msg = BufrMessage(bufr_file)
-            with open(TEST_OUTPUT, "w") as test:
+            with open(TEST_OUTPUT, "wb") as test:
                 msg.write(test)
         os.unlink(TEST_OUTPUT)
 
@@ -439,7 +439,7 @@ class TestBufrMessage(unittest.TestCase):
             bufr['unexpandedDescriptors'] = ivalues
             bufrin.unpack()
             bufrin.copy_data(bufr)
-            with open(TEST_OUTPUT, 'w') as test:
+            with open(TEST_OUTPUT, 'wb') as test:
                 bufr.write(test)
             os.unlink(TEST_OUTPUT)
 
