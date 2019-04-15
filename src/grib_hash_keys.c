02e934b688c091ef3c4186225b40ffa2435f49f7
Shahram Najm <masn@ecmwf.int>
2019-04-15 14:44:45 +0100

    Copyright updated

--- src/grib_hash_keys.c
+++ src/grib_hash_keys.c
@@ -10880,7 +10880,7 @@ grib_keys_hash_get (const char *str, unsigned int len)
   return 0;
 }
 /*
- * Copyright 2005-2018 ECMWF.
+ * Copyright 2005-2019 ECMWF.
  *
  * This software is licensed under the terms of the Apache Licence Version 2.0
  * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
