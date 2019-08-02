e17f0ec194f8e1b23d2dfc6b93c56f62f1650706
Shahram Najm <masn@ecmwf.int>
2019-08-02 11:07:57 +0100

    Testing: reinstate the GRIB bits per value test

--- tests/bpv_limit.c
+++ tests/bpv_limit.c
@@ -9,121 +9,107 @@
  */
 
 /*
- * =====================================================================================
- *
- *       Filename:  bpv_limit.c
- *
- *    Description:  test setting the number of bpv before decoding and then encoding;
- *                  setting the wrong number of bpv should result in an exception
- *
- *        Version:  1.0
- *        Created:  30/03/10 16:07:51
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  Cristian Codorean (mow), mow@ecmwf.int
- *        Company:  ECMWF
- *
- * =====================================================================================
+ *  Description:  test setting the number of bits per value before decoding and then encoding;
+ *                setting the wrong number of bpv should result in an error
  */
-#include <stdio.h>
-#include <stdlib.h>
 
+#include <assert.h>
 #include "grib_api.h"
 
-void usage(char* prog) {
-  printf("usage: %s filein\n",prog);
-  exit(1);
+static double compare_double_absolute(double a,double b,double tolerance)
+{
+    double ret=0;
+    double d=fabs(a-b);
+    if (d > tolerance) {
+        ret=d;
+    }
+    return ret;
 }
 
+static void usage(const char* prog) {
+    fprintf(stderr, "usage: %s input\n",prog);
+    exit(1);
+}
+
+static int check_error_code(int err)
+{
+    if (err == GRIB_INVALID_BPV || err == GRIB_DECODING_ERROR)
+        return GRIB_SUCCESS;
+    return GRIB_INVALID_ARGUMENT;
+}
 
 int main(int argc, char** argv) {
-  int err = 0;
-  size_t values_len=0;
-  double *values = NULL;
-  char error_msg[100] ;
- 
-  size_t slong = sizeof(long) * 8 ;
+    int err = 0, i;
+    size_t values_len=0;
+    double *values = NULL;
+    char error_msg[100] ;
+    const double tolerance = 1e-5;
+    size_t slong = sizeof(long) * 8 ;
+    grib_handle *h;
+    char* filename ;
 
-  int i;
+    if (argc<2) usage(argv[0]);
+    filename=argv[1];
 
-  FILE* in = NULL;
-  char* filename ;
+    for (i=0;i<255;i++) {
+        FILE* in = fopen(filename,"r"); assert(in);
+        h = grib_handle_new_from_file(0,in,&err); assert(h);
 
-  grib_handle *h = NULL;
+        /* get the size of the values array*/
+        GRIB_CHECK(grib_get_size(h,"values",&values_len),0);
 
-  if (argc<2) usage(argv[0]);
-  filename=argv[1];
+        values = (double*)malloc(values_len*sizeof(double));
 
-  in = fopen(filename,"r");
-  if(!in) {
-    printf("ERROR: unable to open file %s\n",filename);
-    return 1;
-  }
+        err = grib_get_double_array(h,"values",values,&values_len);
+        if (compare_double_absolute(values[0], 2.7900000000e+02, tolerance) != 0)
+            printf("oops i=%d  values[0]=%g\n", i,values[0]);
+        if (compare_double_absolute(values[1], 2.7996093750e+02, tolerance) != 0)
+            printf("oops i=%d  values[1]=%g\n", i,values[1]);
 
-  /* create new handle from a message in a file*/
-  h = grib_handle_new_from_file(0,in,&err);
-  if (h == NULL) {
-    printf("Error: unable to create handle from file %s\n",filename);
-  }
+        GRIB_CHECK(grib_set_long(h,"bitsPerValue",i),0);
 
-  for (i=0;i<255;i++) {
+        err = grib_set_double_array(h,"values",values,values_len);
 
-    /* get the size of the values array*/
-    GRIB_CHECK(grib_get_size(h,"values",&values_len),0);
+        free(values);
+        values = 0;
 
-    values = (double*)malloc(values_len*sizeof(double));
+        /*
+         * check if encoding of the data fails when the number of bpv
+         * is not supported; bpv allowed on decoding is bpv < size of long
+         */
 
-    err = grib_get_double_array(h,"values",values,&values_len);
+        if (i < slong && err == 0) {
+            /* do nothing */
+        } else if (i >= slong  && check_error_code(err)==GRIB_SUCCESS) {
+            /* do nothing  */
+        } else {
+            sprintf(error_msg,"Error decoding when bpv=%d. Error message:%s",i,grib_get_error_message(err));
+            perror(error_msg);
+            exit(1);
+        }
 
-    GRIB_CHECK(grib_set_long(h,"bitsPerValue",i),0);
+        values = (double*)malloc(values_len*sizeof(double));
+        err = grib_get_double_array(h,"values",values,&values_len);
 
-    err = grib_set_double_array(h,"values",values,values_len);
+        /*
+         * check if decoding of the data fails when the number of bpv
+         * is not supported; bpv allowed on decoding is bpv <= size of long
+         */
 
-    free(values);
-    values = 0;
+        if (i < slong && err == 0) {
+            /* do nothing */
+        } else if (i >= slong  && check_error_code(err)==GRIB_SUCCESS) {
+            /* do nothing  */
+        } else {
+            sprintf(error_msg,"Error decoding when bpv=%d. Error message:%s",i,grib_get_error_message(err));
+            perror(error_msg);
+            exit(1);
+        }
 
-    /*
-     * check if encoding of the data fails when the number of bpv
-     * is not supported; bpv allowed on decoding is bpv < size of long
-     */
-
-    if (i < slong && err == 0) {
-      /* do nothing */
-    } else if (i >= slong  && err == GRIB_INVALID_BPV) {
-      /* do nothing  */
-    } else {
-      sprintf(error_msg,"Error decoding when bpv=%d. Error message:%s",i,grib_get_error_message(err));
-      perror(error_msg);
-      exit(1);
+        free(values); values = 0;
+        grib_handle_delete(h); h = 0;
+        fclose(in);
     }
 
-    values = (double*)malloc(values_len*sizeof(double));
-    err = grib_get_double_array(h,"values",values,&values_len);
-
-    /*
-     * check if decoding of the data fails when the number of bpv
-     * is not supported; bpv allowed on decoding is bpv <= size of long
-     */
-
-    if (i <= slong && err == 0) {
-      /* do nothing */
-    } else if (i > slong  && err == GRIB_INVALID_BPV) {
-      /* do nothing  */
-    } else {
-      sprintf(error_msg,"Error decoding when bpv=%d. Error message:%s",i,grib_get_error_message(err));
-      perror(error_msg);
-      exit(1);
-    }
-
-    free(values);
-    values = 0;
-  }
-
-  grib_handle_delete(h);
-  h=0;
-
-  fclose(in);
-
-  return 0;
+    return 0;
 }
