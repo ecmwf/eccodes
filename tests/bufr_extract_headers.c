4ce294d0c89ad8f5d059e2833a444e3af9fe63a5
Shahram Najm <masn@ecmwf.int>
2019-09-20 18:30:06 +0100

    Performance: add test for header extraction

---
+++ tests/bufr_extract_headers.c
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2005-2019 ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * In applying this licence, ECMWF does not waive the privileges and immunities granted to it by
+ * virtue of its status as an intergovernmental organisation nor does it submit to any jurisdiction.
+ */
+#include "grib_api_internal.h"
+#include "eccodes.h"
+#include <assert.h>
+
+int main(int argc,char* argv[])
+{
+    char* filename;
+    int i, err = 0;
+    int num_messages = 0;
+    codes_bufr_header* headers = NULL;
+    codes_context* c = codes_context_get_default();
+
+    assert(argc == 2);
+
+    filename=argv[1];
+    err = codes_bufr_extract_headers_malloc(c, filename, &headers, &num_messages);
+    assert(!err);
+
+    for (i=0; i<num_messages; ++i) {
+        codes_bufr_header bh = headers[i];
+        printf("%ld %lu %ld %ld %ld\n",
+               bh.edition, bh.totalLength, bh.dataCategory,
+               bh.masterTablesVersionNumber, bh.rdbType);
+    }
+
+    free(headers);
+
+    return 0;
+}
