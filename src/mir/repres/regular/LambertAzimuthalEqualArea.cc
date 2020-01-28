6948e8cdf21d48a72e338663eda6899debf4f940
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2020-01-28 12:40:51 +0000

    MIR-443: Update license headers

--- src/mir/repres/regular/LambertAzimuthalEqualArea.cc
+++ src/mir/repres/regular/LambertAzimuthalEqualArea.cc
@@ -3,6 +3,7 @@
  *
  * This software is licensed under the terms of the Apache Licence Version 2.0
  * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ *
  * In applying this licence, ECMWF does not waive the privileges and immunities
  * granted to it by virtue of its status as an intergovernmental organisation nor
  * does it submit to any jurisdiction.
@@ -11,7 +12,7 @@
 
 #include "mir/repres/regular/LambertAzimuthalEqualArea.h"
 
-#include <cmath>
+#include <string>
 
 #include "mir/param/MIRParametrisation.h"
 #include "mir/util/Grib.h"
@@ -67,10 +68,10 @@ void LambertAzimuthalEqualArea::fill(grib_info& info) const {
     ASSERT(x_.size() > 1);
     ASSERT(y_.size() > 1);
 
-    auto Dx = (x_.max() - x_.min()) / (x_.size() - 1);
-    auto Dy = (y_.max() - y_.min()) / (y_.size() - 1);
-    ASSERT(Dx > 0);
-    ASSERT(Dy > 0);
+    auto Dx = (x_.max() - x_.min()) / (x_.size() - 1.);
+    auto Dy = (y_.max() - y_.min()) / (y_.size() - 1.);
+    ASSERT(Dx > 0.);
+    ASSERT(Dy > 0.);
 
     Point2 reference = grid_.projection().lonlat({0., 0.});
     Point2 firstLL   = grid_.projection().lonlat({x_.front(), y_.front()});
@@ -80,10 +81,10 @@ void LambertAzimuthalEqualArea::fill(grib_info& info) const {
     info.grid.Ni                                 = long(x_.size());
     info.grid.Nj                                 = long(y_.size());
 
-    GribExtraSetting::set(info, "xDirectionGridLengthInMillimetres", std::lround(Dx * 1.e3));
-    GribExtraSetting::set(info, "yDirectionGridLengthInMillimetres", std::lround(Dy * 1.e3));
-    GribExtraSetting::set(info, "standardParallelInMicrodegrees", std::lround(reference[LLCOORDS::LAT] * 1.e6));
-    GribExtraSetting::set(info, "centralLongitudeInMicrodegrees", std::lround(reference[LLCOORDS::LON] * 1.e6));
+    GribExtraSetting::set(info, "xDirectionGridLengthInMetres", Dx);
+    GribExtraSetting::set(info, "yDirectionGridLengthInMetres", Dy);
+    GribExtraSetting::set(info, "standardParallelInDegrees", reference[LLCOORDS::LAT]);
+    GribExtraSetting::set(info, "centralLongitudeInDegrees", reference[LLCOORDS::LON]);
 
     // some extra keys are edition-specific, so parent call is here
     RegularGrid::fill(info);
--- src/mir/repres/unsupported/IrregularLatlon.cc
+++ src/mir/repres/unsupported/IrregularLatlon.cc
@@ -3,14 +3,12 @@
  *
  * This software is licensed under the terms of the Apache Licence Version 2.0
  * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ *
  * In applying this licence, ECMWF does not waive the privileges and immunities
  * granted to it by virtue of its status as an intergovernmental organisation nor
  * does it submit to any jurisdiction.
  */
 
-/// @author Baudouin Raoult
-/// @author Pedro Maciel
-/// @date Apr 2015
 
 #include "mir/repres/unsupported/IrregularLatlon.h"
 
