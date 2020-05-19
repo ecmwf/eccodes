dcdaee094f43607156d0adc57a10a98c8f304153
Shahram Najm <masn@ecmwf.int>
2020-05-19 10:55:41 +0100

    ECC-1115: Refactoring

--- src/grib_accessor_class_proj_string.c
+++ src/grib_accessor_class_proj_string.c
@@ -48,46 +48,46 @@ typedef struct grib_accessor_proj_string
 extern grib_accessor_class* grib_accessor_class_gen;
 
 static grib_accessor_class _grib_accessor_class_proj_string = {
-    &grib_accessor_class_gen,                      /* super                     */
-    "proj_string",                      /* name                      */
-    sizeof(grib_accessor_proj_string),  /* size                      */
-    0,                           /* inited */
-    &init_class,                 /* init_class */
-    &init,                       /* init                      */
-    0,                  /* post_init                      */
-    0,                    /* free mem                       */
-    0,                       /* describes himself         */
-    0,                /* get length of section     */
-    0,              /* get length of string      */
-    0,                /* get number of values      */
-    0,                 /* get number of bytes      */
-    0,                /* get offset to bytes           */
-    &get_native_type,            /* get native type               */
-    0,                /* get sub_section                */
-    0,               /* grib_pack procedures long      */
-    0,                 /* grib_pack procedures long      */
-    0,                  /* grib_pack procedures long      */
-    0,                /* grib_unpack procedures long    */
-    0,                /* grib_pack procedures double    */
-    0,              /* grib_unpack procedures double  */
-    0,                /* grib_pack procedures string    */
-    &unpack_string,              /* grib_unpack procedures string  */
-    0,          /* grib_pack array procedures string    */
-    0,        /* grib_unpack array procedures string  */
-    0,                 /* grib_pack procedures bytes     */
-    0,               /* grib_unpack procedures bytes   */
-    0,            /* pack_expression */
-    0,              /* notify_change   */
-    0,                /* update_size   */
-    0,             /* preferred_size   */
-    0,                     /* resize   */
-    0,      /* nearest_smaller_value */
-    0,                       /* next accessor    */
-    0,                    /* compare vs. another accessor   */
-    0,      /* unpack only ith value          */
-    0,     /* unpack a subarray         */
-    0,                      /* clear          */
-    0,                 /* clone accessor          */
+    &grib_accessor_class_gen,          /* super                     */
+    "proj_string",                     /* name                      */
+    sizeof(grib_accessor_proj_string), /* size                      */
+    0,                                 /* inited */
+    &init_class,                       /* init_class */
+    &init,                             /* init                      */
+    0,                                 /* post_init                      */
+    0,                                 /* free mem                       */
+    0,                                 /* describes himself         */
+    0,                                 /* get length of section     */
+    0,                                 /* get length of string      */
+    0,                                 /* get number of values      */
+    0,                                 /* get number of bytes      */
+    0,                                 /* get offset to bytes           */
+    &get_native_type,                  /* get native type               */
+    0,                                 /* get sub_section                */
+    0,                                 /* grib_pack procedures long      */
+    0,                                 /* grib_pack procedures long      */
+    0,                                 /* grib_pack procedures long      */
+    0,                                 /* grib_unpack procedures long    */
+    0,                                 /* grib_pack procedures double    */
+    0,                                 /* grib_unpack procedures double  */
+    0,                                 /* grib_pack procedures string    */
+    &unpack_string,                    /* grib_unpack procedures string  */
+    0,                                 /* grib_pack array procedures string    */
+    0,                                 /* grib_unpack array procedures string  */
+    0,                                 /* grib_pack procedures bytes     */
+    0,                                 /* grib_unpack procedures bytes   */
+    0,                                 /* pack_expression */
+    0,                                 /* notify_change   */
+    0,                                 /* update_size   */
+    0,                                 /* preferred_size   */
+    0,                                 /* resize   */
+    0,                                 /* nearest_smaller_value */
+    0,                                 /* next accessor    */
+    0,                                 /* compare vs. another accessor   */
+    0,                                 /* unpack only ith value          */
+    0,                                 /* unpack a subarray         */
+    0,                                 /* clear          */
+    0,                                 /* clone accessor          */
 };
 
 
@@ -96,36 +96,36 @@ grib_accessor_class* grib_accessor_class_proj_string = &_grib_accessor_class_pro
 
 static void init_class(grib_accessor_class* c)
 {
-    c->dump    =    (*(c->super))->dump;
-    c->next_offset    =    (*(c->super))->next_offset;
-    c->string_length    =    (*(c->super))->string_length;
-    c->value_count    =    (*(c->super))->value_count;
-    c->byte_count    =    (*(c->super))->byte_count;
-    c->byte_offset    =    (*(c->super))->byte_offset;
-    c->sub_section    =    (*(c->super))->sub_section;
-    c->pack_missing    =    (*(c->super))->pack_missing;
-    c->is_missing    =    (*(c->super))->is_missing;
-    c->pack_long    =    (*(c->super))->pack_long;
-    c->unpack_long    =    (*(c->super))->unpack_long;
-    c->pack_double    =    (*(c->super))->pack_double;
-    c->unpack_double    =    (*(c->super))->unpack_double;
-    c->pack_string    =    (*(c->super))->pack_string;
-    c->pack_string_array    =    (*(c->super))->pack_string_array;
-    c->unpack_string_array    =    (*(c->super))->unpack_string_array;
-    c->pack_bytes    =    (*(c->super))->pack_bytes;
-    c->unpack_bytes    =    (*(c->super))->unpack_bytes;
-    c->pack_expression    =    (*(c->super))->pack_expression;
-    c->notify_change    =    (*(c->super))->notify_change;
-    c->update_size    =    (*(c->super))->update_size;
-    c->preferred_size    =    (*(c->super))->preferred_size;
-    c->resize    =    (*(c->super))->resize;
-    c->nearest_smaller_value    =    (*(c->super))->nearest_smaller_value;
-    c->next    =    (*(c->super))->next;
-    c->compare    =    (*(c->super))->compare;
-    c->unpack_double_element    =    (*(c->super))->unpack_double_element;
-    c->unpack_double_subarray    =    (*(c->super))->unpack_double_subarray;
-    c->clear    =    (*(c->super))->clear;
-    c->make_clone    =    (*(c->super))->make_clone;
+    c->dump                   = (*(c->super))->dump;
+    c->next_offset            = (*(c->super))->next_offset;
+    c->string_length          = (*(c->super))->string_length;
+    c->value_count            = (*(c->super))->value_count;
+    c->byte_count             = (*(c->super))->byte_count;
+    c->byte_offset            = (*(c->super))->byte_offset;
+    c->sub_section            = (*(c->super))->sub_section;
+    c->pack_missing           = (*(c->super))->pack_missing;
+    c->is_missing             = (*(c->super))->is_missing;
+    c->pack_long              = (*(c->super))->pack_long;
+    c->unpack_long            = (*(c->super))->unpack_long;
+    c->pack_double            = (*(c->super))->pack_double;
+    c->unpack_double          = (*(c->super))->unpack_double;
+    c->pack_string            = (*(c->super))->pack_string;
+    c->pack_string_array      = (*(c->super))->pack_string_array;
+    c->unpack_string_array    = (*(c->super))->unpack_string_array;
+    c->pack_bytes             = (*(c->super))->pack_bytes;
+    c->unpack_bytes           = (*(c->super))->unpack_bytes;
+    c->pack_expression        = (*(c->super))->pack_expression;
+    c->notify_change          = (*(c->super))->notify_change;
+    c->update_size            = (*(c->super))->update_size;
+    c->preferred_size         = (*(c->super))->preferred_size;
+    c->resize                 = (*(c->super))->resize;
+    c->nearest_smaller_value  = (*(c->super))->nearest_smaller_value;
+    c->next                   = (*(c->super))->next;
+    c->compare                = (*(c->super))->compare;
+    c->unpack_double_element  = (*(c->super))->unpack_double_element;
+    c->unpack_double_subarray = (*(c->super))->unpack_double_subarray;
+    c->clear                  = (*(c->super))->clear;
+    c->make_clone             = (*(c->super))->make_clone;
 }
 
 /* END_CLASS_IMP */
@@ -148,9 +148,10 @@ static int get_native_type(grib_accessor* a)
 
 /* Function pointer than takes a handle and returns the proj string */
 typedef int (*proj_func)(grib_handle*, char*);
-struct proj_mapping {
+struct proj_mapping
+{
     const char* gridType; /* key gridType */
-    proj_func   func;     /* function to compute proj string */
+    proj_func func;       /* function to compute proj string */
 };
 typedef struct proj_mapping proj_mapping;
 
@@ -173,20 +174,20 @@ static int get_major_minor_axes(grib_handle* h, double* pMajor, double* pMinor)
 
 static int proj_regular_latlon(grib_handle* h, char* result)
 {
-    int err = 0;
-    double earthMajorAxisInMetres = 0, earthMinorAxisInMetres = 0;
-    if ((err = get_major_minor_axes(h, &earthMajorAxisInMetres, &earthMinorAxisInMetres)) != GRIB_SUCCESS)
+    int err      = 0;
+    double major = 0, minor = 0;
+    if ((err = get_major_minor_axes(h, &major, &minor)) != GRIB_SUCCESS)
         return err;
-    sprintf(result,"+proj=latlong +a=%lf +b=%lf", earthMajorAxisInMetres, earthMinorAxisInMetres);
+    sprintf(result, "+proj=latlong +a=%lf +b=%lf", major, minor);
     return err;
 }
 
 static int proj_lambert_conformal(grib_handle* h, char* result)
 {
-    int err = 0;
-    double earthMajorAxisInMetres = 0, earthMinorAxisInMetres = 0;
+    int err      = 0;
+    double major = 0, minor = 0;
     double LoVInDegrees = 0, LaDInDegrees = 0, Latin1InDegrees = 0, Latin2InDegrees = 0;
-    if ((err = get_major_minor_axes(h, &earthMajorAxisInMetres, &earthMinorAxisInMetres)) != GRIB_SUCCESS)
+    if ((err = get_major_minor_axes(h, &major, &minor)) != GRIB_SUCCESS)
         return err;
     if ((err = grib_get_double_internal(h, "Latin1InDegrees", &Latin1InDegrees)) != GRIB_SUCCESS)
         return err;
@@ -196,36 +197,36 @@ static int proj_lambert_conformal(grib_handle* h, char* result)
         return err;
     if ((err = grib_get_double_internal(h, "LaDInDegrees", &LaDInDegrees)) != GRIB_SUCCESS)
         return err;
-    sprintf(result,"+proj=lcc +lon_0=%lf +lat_0=%lf +lat_1=%lf +lat_2=%lf +a=%lf +b=%lf",LoVInDegrees,
-            LaDInDegrees, Latin1InDegrees,Latin2InDegrees, earthMajorAxisInMetres, earthMinorAxisInMetres);
+    sprintf(result, "+proj=lcc +lon_0=%lf +lat_0=%lf +lat_1=%lf +lat_2=%lf +a=%lf +b=%lf", LoVInDegrees,
+            LaDInDegrees, Latin1InDegrees, Latin2InDegrees, major, minor);
     return err;
 }
 
 static int proj_lambert_azimuthal_equal_area(grib_handle* h, char* result)
 {
-    int err = 0;
-    double earthMajorAxisInMetres = 0, earthMinorAxisInMetres = 0;
+    int err      = 0;
+    double major = 0, minor = 0;
     double standardParallel = 0, centralLongitude = 0;
-    if ((err = get_major_minor_axes(h, &earthMajorAxisInMetres, &earthMinorAxisInMetres)) != GRIB_SUCCESS)
+    if ((err = get_major_minor_axes(h, &major, &minor)) != GRIB_SUCCESS)
         return err;
     if ((err = grib_get_double_internal(h, "standardParallel", &standardParallel)) != GRIB_SUCCESS)
         return err;
     if ((err = grib_get_double_internal(h, "centralLongitude", &centralLongitude)) != GRIB_SUCCESS)
         return err;
     sprintf(result, "+proj=laea +lon_0=%lf +lat_0=%lf +a=%lf +b=%lf",
-            centralLongitude, standardParallel, earthMajorAxisInMetres, earthMinorAxisInMetres);
+            centralLongitude, standardParallel, major, minor);
     return err;
 }
 
 static int proj_polar_stereographic(grib_handle* h, char* result)
 {
-    int err = 0;
-    double earthMajorAxisInMetres = 0, earthMinorAxisInMetres = 0;
+    int err      = 0;
+    double major = 0, minor = 0;
     double centralLongitude = 0, centralLatitude = 0;
     int has_northPole         = 0;
     long projectionCentreFlag = 0;
 
-    if ((err = get_major_minor_axes(h, &earthMajorAxisInMetres, &earthMinorAxisInMetres)) != GRIB_SUCCESS)
+    if ((err = get_major_minor_axes(h, &major, &minor)) != GRIB_SUCCESS)
         return err;
     if ((err = grib_get_double_internal(h, "orientationOfTheGridInDegrees", &centralLongitude)) != GRIB_SUCCESS)
         return err;
@@ -235,29 +236,29 @@ static int proj_polar_stereographic(grib_handle* h, char* result)
         return err;
     has_northPole = ((projectionCentreFlag & 128) == 0);
     sprintf(result, "+proj=stere +lat_ts=%lf +lat_0=%s +lon_0=%lf +k_0=1 +x_0=0 +y_0=0 +a=%lf +b=%lf",
-            centralLatitude, has_northPole ? "90" : "-90", centralLongitude, earthMajorAxisInMetres, earthMinorAxisInMetres);
+            centralLatitude, has_northPole ? "90" : "-90", centralLongitude, major, minor);
     return err;
 }
 
 static int proj_mercator(grib_handle* h, char* result)
 {
-    int err = 0;
-    double LaDInDegrees = 0, earthMajorAxisInMetres = 0, earthMinorAxisInMetres = 0;
-    if ((err = get_major_minor_axes(h, &earthMajorAxisInMetres, &earthMinorAxisInMetres)) != GRIB_SUCCESS)
+    int err             = 0;
+    double LaDInDegrees = 0, major = 0, minor = 0;
+    if ((err = get_major_minor_axes(h, &major, &minor)) != GRIB_SUCCESS)
         return err;
     if ((err = grib_get_double_internal(h, "LaDInDegrees", &LaDInDegrees)) != GRIB_SUCCESS)
         return err;
     sprintf(result, "+proj=merc +lat_ts=%lf +lat_0=0 +lon_0=0 +x_0=0 +y_0=0 +a=%lf +b=%lf",
-                LaDInDegrees, earthMajorAxisInMetres, earthMinorAxisInMetres);
+            LaDInDegrees, major, minor);
     return err;
 }
 
-#define NUMBER(a) (sizeof(a)/sizeof(a[0]))
-proj_mapping proj_mappings[] = {
+#define NUMBER(a) (sizeof(a) / sizeof(a[0]))
+static proj_mapping proj_mappings[] = {
     { "regular_ll", &proj_regular_latlon },
     { "mercator", &proj_mercator },
-    { "polar_stereographic", &proj_polar_stereographic },
     { "lambert", &proj_lambert_conformal },
+    { "polar_stereographic", &proj_polar_stereographic },
     { "lambert_azimuthal_equal_area", &proj_lambert_azimuthal_equal_area }
 };
 
@@ -265,15 +266,15 @@ static int unpack_string(grib_accessor* a, char* v, size_t* len)
 {
     grib_accessor_proj_string* self = (grib_accessor_proj_string*)a;
     int err = 0, found = 0;
-    size_t i = 0;
+    size_t i           = 0;
     char grid_type[64] = {0,};
     grib_handle* h = grib_handle_of_accessor(a);
-    size_t size = sizeof(grid_type) / sizeof(*grid_type);
+    size_t size    = sizeof(grid_type) / sizeof(*grid_type);
 
     err = grib_get_string(h, self->grid_type, grid_type, &size);
     if (err) return err;
 
-    for (i=0; !found && i < NUMBER(proj_mappings); ++i) {
+    for (i = 0; !found && i < NUMBER(proj_mappings); ++i) {
         proj_mapping pm = proj_mappings[i];
         if (strcmp(grid_type, pm.gridType) == 0) {
             found = 1;
