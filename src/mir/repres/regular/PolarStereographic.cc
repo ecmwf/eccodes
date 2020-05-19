425e1ac9127f1f2522ad06419ff6a65ae725dbb8
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2020-05-19 11:30:37 +0100

    MIR-471: polar_stereographic compares correctly with eccodes

---
+++ src/mir/repres/regular/PolarStereographic.cc
@@ -0,0 +1,47 @@
+/*
+ * (C) Copyright 1996- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+
+#include "eckit/exception/Exceptions.h"
+
+#include "mir/repres/regular/PolarStereographic.h"
+
+
+namespace mir {
+namespace repres {
+namespace regular {
+
+
+static RepresentationBuilder<PolarStereographic> __builder("polar_stereographic");
+
+
+PolarStereographic::PolarStereographic(const param::MIRParametrisation& param) :
+    RegularGrid(param, make_projection(param)) {}
+
+
+RegularGrid::Projection PolarStereographic::make_projection(const param::MIRParametrisation& param) {
+
+    // only PROJ-based projection is available
+    auto spec = make_proj_spec(param);
+    ASSERT(!spec.empty());
+
+    return spec;
+}
+
+
+void PolarStereographic::fill(grib_info& /*info*/) const {
+    NOTIMP;
+}
+
+
+}  // namespace regular
+}  // namespace repres
+}  // namespace mir
