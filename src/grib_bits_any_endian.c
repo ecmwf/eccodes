41f44aa18a475ab71c263e44d63406991c0c2bfd
Shahram Najm <masn@ecmwf.int>
2020-06-25 15:25:19 +0100

    Clean up

--- src/grib_bits_any_endian.c
+++ src/grib_bits_any_endian.c
@@ -420,7 +420,7 @@ int grib_encode_size_tb(unsigned char* p, size_t val, long* bitp, long nb)
 #if OMP_PACKING
 #include "grib_bits_any_endian_omp.c"
 #elif VECTOR
-#include "grib_bits_any_endian_vector.c"  /* Experimental */
+#include "grib_bits_any_endian_vector.c"
 #else
 #include "grib_bits_any_endian_simple.c"
 #endif
