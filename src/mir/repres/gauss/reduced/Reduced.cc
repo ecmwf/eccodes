99e4f4b857fda92a4954decce5b5d0c70e61d606
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2020-11-18 00:00:19 +0000

    Ability to build using Longitude = LongitudeDouble

--- src/mir/repres/gauss/reduced/Reduced.cc
+++ src/mir/repres/gauss/reduced/Reduced.cc
@@ -477,10 +477,10 @@ util::BoundingBox Reduced::extendBoundingBox(const util::BoundingBox& bbox) cons
                 auto Nw = (west / inc).integralPart() - 1;
                 auto Ne = (east / inc).integralPart() + 1;
 
-                if (w > Nw * inc || first) {
+                if (w > Longitude(Nw * inc) || first) {
                     w = Nw * inc;
                 }
-                if (e < Ne * inc || first) {
+                if (e < Longitude(Ne * inc) || first) {
                     e = Ne * inc;
                 }
                 first = false;
