7e4d24a5eed40def2a5c9b456e43751436783f83
Shahram Najm <masn@ecmwf.int>
2020-12-10 17:45:32 +0000

    ECC-1181: Add function to determine if a BUFR key is a header key or from the data section

--- src/eccodes.h
+++ src/eccodes.h
@@ -1205,6 +1205,9 @@ int codes_is_missing(const codes_handle* h, const char* key, int* err);
 /* Returns a bool i.e. 0 or 1 */
 int codes_is_defined(const codes_handle* h, const char* key);
 
+/* Returns 1 if the BUFR key is in the header and 1 if it is in the data section. The error code is an argument */
+int codes_bufr_key_is_header(const codes_handle* h, const char* key, int* err);
+
 int codes_set_missing(codes_handle* h, const char* key);
 /* The truncation is the Gaussian number (or order) */
 int codes_get_gaussian_latitudes(long truncation, double* latitudes);
