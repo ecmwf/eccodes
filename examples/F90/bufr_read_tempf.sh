67eb9a087a82c444bed5e11ebd8f1885667ef51a
Shahram Najm <masn@ecmwf.int>
2020-12-13 16:43:16 +0000

    Examples: BUFR radiosonde data (From Bruce Ingleby)

---
+++ examples/F90/bufr_read_tempf.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# (C) Copyright 2005- ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# 
+# In applying this licence, ECMWF does not waive the privileges and immunities granted to it by
+# virtue of its status as an intergovernmental organisation nor does it submit to any jurisdiction.
+#
+
+. ./include.sh
+
+
+#Define a common label for all the tmp files
+label="bufr_read_tempf_f"
+fTmp=${label}.tmp.txt
+rm -f $fTmp
+
+# The path to the BUFR file is hardcoded in the example
+
+REDIRECT=/dev/null
+
+# Run the example
+${examples_dir}/eccodes_f_bufr_read_tempf > $fTmp
+
+#TODO: check the results
+
+#Clean up
+rm -f $fTmp
+
