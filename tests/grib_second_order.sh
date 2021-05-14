ee4921b9093f91306a9785e8db601ccc73bf48dc
Shahram Najm <masn@ecmwf.int>
2021-05-14 23:40:30 +0100

    ECC-1219: packingType conversion from grid_ieee to grid_second_order

--- tests/grib_second_order.sh
+++ tests/grib_second_order.sh
@@ -141,6 +141,14 @@ ${tools_dir}/grib_set -r -s packingType=grid_second_order $temp2 $temp3
 grib_check_key_equals $temp3 packingType grid_second_order
 
 
+# ECC-1219: packingType conversion from grid_ieee to grid_second_order
+# ---------------------------------------------------------------------
+infile=grid_ieee.grib
+${tools_dir}/grib_set -r -s packingType=grid_second_order $infile $temp1
+grib_check_key_equals $temp1 packingType grid_second_order
+${tools_dir}/grib_compare -cdata:n -R all=3e-06 $infile $temp1
+
+
 # Clean up
 rm -f $temp_stat1 $temp_stat2
 rm -f $temp1 $temp2 $temp3 $sec_ord_bmp
