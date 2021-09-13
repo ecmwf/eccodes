7afdb78633393bd8613371ee8ab809ab00728eed
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2021-09-14 00:16:36 +0100

    MIR-540 GRIB gridType=space_view

---
+++ src/mir/repres/regular/SpaceView.cc
@@ -0,0 +1,45 @@
+/*
+ * (C) Copyright 1996- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+
+#include "mir/repres/regular/SpaceView.h"
+
+#include "mir/util/Exceptions.h"
+
+
+namespace mir {
+namespace repres {
+namespace regular {
+
+
+static RepresentationBuilder<SpaceView> __builder("space_view");
+
+
+SpaceView::SpaceView(const param::MIRParametrisation& param) : RegularGrid(param, make_projection(param)) {}
+
+
+RegularGrid::Projection SpaceView::make_projection(const param::MIRParametrisation& param) {
+    // only PROJ-based projection is available
+    auto spec = make_proj_spec(param);
+    ASSERT(!spec.empty());
+
+    return spec;
+}
+
+
+void SpaceView::fill(grib_info& /*info*/) const {
+    NOTIMP;
+}
+
+
+}  // namespace regular
+}  // namespace repres
+}  // namespace mir
