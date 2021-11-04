8bf71575472b270318e59a846456c65c90f9cbf6
Shahram Najm <masn@ecmwf.int>
2021-11-04 11:07:09 +0000

    Clang warning [-Wmissing-prototypes]

--- src/grib_iterator_class_lambert_azimuthal_equal_area.c
+++ src/grib_iterator_class_lambert_azimuthal_equal_area.c
@@ -119,7 +119,7 @@ static int next(grib_iterator* i, double* lat, double* lon, double* val)
 #define P11 .06640211640211640212 /* 251 /  3780 */
 #define P20 .01677689594356261023 /* 761 / 45360 */
 
-void pj_authset(double es, double* APA)
+static void pj_authset(double es, double* APA)
 {
     double t;
     APA[0] = es * P00;
@@ -131,7 +131,7 @@ void pj_authset(double es, double* APA)
     APA[1] += t * P11;
     APA[2] = t * P20;
 }
-double pj_authlat(double beta, double* APA)
+static double pj_authlat(double beta, double* APA)
 {
     double t = beta + beta;
     return (beta + APA[0] * sin(t) + APA[1] * sin(t + t) + APA[2] * sin(t + t + t));
