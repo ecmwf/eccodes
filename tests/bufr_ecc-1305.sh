eff3e2fb15ae8dd3fcf3b4c285c8a6464408b4e2
Shahram Najm <masn@ecmwf.int>
2021-11-16 16:46:41 +0000

    ECC-1305: BUFR: Invalid code in definitions/bufr/tables/operators.table

---
+++ tests/bufr_ecc-1305.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# (C) Copyright 2005- ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# 
+# In applying this licence, ECMWF does not waive the privileges and immunities granted to it by
+# virtue of its status as an intergovernmental organisation nor does it submit to any jurisdiction.
+#
+
+. ./include.sh
+set -u
+REDIRECT=/dev/null
+label="bufr_ecc-1305-test"
+temp=temp.$label
+
+# Check the operator name is printed
+infile=${data_dir}/bufr/maer_207.bufr
+${tools_dir}/bufr_dump -d $infile > $temp
+
+grep -q "224000.*firstOrderStatisticalValuesFollow" $temp
+grep -q "224255.*firstOrderStatisticalValuesMarkerOperator" $temp
+
+rm -f $temp
