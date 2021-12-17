f202308cebffe9e2b179ddd3c58cbb67844eeb4c
Shahram Najm <masn@ecmwf.int>
2021-12-17 20:15:31 +0000

    Testing: Add tigge for testing

--- tests/grib_check_param_concepts.sh
+++ tests/grib_check_param_concepts.sh
@@ -21,11 +21,13 @@ $EXEC ${test_dir}/grib_check_param_concepts paramId $ECCODES_DEFINITION_PATH/gri
 $EXEC ${test_dir}/grib_check_param_concepts paramId $ECCODES_DEFINITION_PATH/grib2/localConcepts/ecmf/paramId.def
 $EXEC ${test_dir}/grib_check_param_concepts paramId $ECCODES_DEFINITION_PATH/grib2/localConcepts/uerra/paramId.def
 $EXEC ${test_dir}/grib_check_param_concepts paramId $ECCODES_DEFINITION_PATH/grib2/localConcepts/hydro/paramId.def
+$EXEC ${test_dir}/grib_check_param_concepts paramId $ECCODES_DEFINITION_PATH/grib2/localConcepts/tigge/paramId.def
 
 $EXEC ${test_dir}/grib_check_param_concepts shortName $ECCODES_DEFINITION_PATH/grib2/shortName.def
 $EXEC ${test_dir}/grib_check_param_concepts shortName $ECCODES_DEFINITION_PATH/grib2/localConcepts/ecmf/shortName.def
 $EXEC ${test_dir}/grib_check_param_concepts shortName $ECCODES_DEFINITION_PATH/grib2/localConcepts/uerra/shortName.def
 $EXEC ${test_dir}/grib_check_param_concepts shortName $ECCODES_DEFINITION_PATH/grib2/localConcepts/hydro/shortName.def
+$EXEC ${test_dir}/grib_check_param_concepts shortName $ECCODES_DEFINITION_PATH/grib2/localConcepts/tigge/shortName.def
 
 # Check the group: name.def paramId.def shortName.def units.def cfVarName.def
 # ----------------------------------------------------------------------------
@@ -58,6 +60,7 @@ defs_dirs="
 
  $ECCODES_DEFINITION_PATH/grib2/localConcepts/uerra
  $ECCODES_DEFINITION_PATH/grib2/localConcepts/hydro
+ $ECCODES_DEFINITION_PATH/grib2/localConcepts/tigge
 
  $ECCODES_DEFINITION_PATH/grib2/localConcepts/egrr
  $ECCODES_DEFINITION_PATH/grib2/localConcepts/ekmi
