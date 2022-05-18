d94f3e4e7c7c45d27fa2b593efd41097573af39a
Shahram Najm <masn@ecmwf.int>
2022-05-18 17:00:55 +0100

    ECC-1388: GRIB2 CCSDS: Unpacked values different from simple packing

--- tests/grib_ccsds.sh
+++ tests/grib_ccsds.sh
@@ -33,8 +33,30 @@ grib_check_key_equals $outfile2     packingType,const "grid_ccsds 1"
 grib_check_key_equals $outfile2     accuracy 0
 rm -f $outfile1 $outfile2
 
+# ECC-1388: Tiny values
+# ---------------------
+tempFilt=temp.$label.filter
+IFS_SAMPLES_ROOT=${proj_dir}/ifs_samples
+IFS_SAMPLE_SIMPLE=$IFS_SAMPLES_ROOT/grib1_mlgrib2/gg_ml.tmpl
+IFS_SAMPLE_CCSDS=$IFS_SAMPLES_ROOT/grib1_mlgrib2_ccsds/gg_ml.tmpl
+echo 'set bitsPerValue=24; set values={ 1.00000007851413483E-25, 1.00000007851413529E-25 }; write;' > $tempFilt
+${tools_dir}/grib_filter -o $outfile1 $tempFilt $IFS_SAMPLE_SIMPLE
+${tools_dir}/grib_filter -o $outfile2 $tempFilt $IFS_SAMPLE_CCSDS
+${tools_dir}/grib_compare -c data:n $outfile1 $outfile2
+rm -f $tempFilt
+
+# ECC-1388: Constant field variant
+# --------------------------------
+echo 'set bitsPerValue=24; set values={ 1.00000007851413483E-25, 1.00000007851413483E-25 }; write;' > $tempFilt
+${tools_dir}/grib_filter -o $outfile1 $tempFilt $IFS_SAMPLE_SIMPLE
+${tools_dir}/grib_filter -o $outfile2 $tempFilt $IFS_SAMPLE_CCSDS
+# Must compare the referenceValue with 0 tolerance (override the referenceValueError key)
+${tools_dir}/grib_compare -R referenceValue=0 -c  referenceValue $outfile1 $outfile2
+rm -f $tempFilt
+
+
 # ECC-1387
-# --------
+# ---------
 echo 'set values={6, 6, 6};write;' | ${tools_dir}/grib_filter -o $outfile2 - ${data_dir}/ccsds.grib2
 grib_check_key_equals $outfile2 referenceValue,bitsPerValue '6 0'
 
