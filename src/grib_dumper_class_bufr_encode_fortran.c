776589abe9d4e32d4cbe9238e5e7a25a0dd5adcc
Shahram Najm <masn@ecmwf.int>
2022-08-16 17:36:02 +0100

    ECC-1442: bufr_dump should insert a question mark instead of dot for non-printable characters

--- src/grib_dumper_class_bufr_encode_fortran.c
+++ src/grib_dumper_class_bufr_encode_fortran.c
@@ -757,7 +757,7 @@ static void dump_string(grib_dumper* d, grib_accessor* a, const char* comment)
 
     while (*p) {
         if (!isprint(*p))
-            *p = '.';
+            *p = '?';
         p++;
     }
 
