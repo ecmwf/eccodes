2ba6982e8a9d7d1379727b44fe281674973e9c2c
Shahram Najm <masn@ecmwf.int>
2022-11-02 16:47:21 +0000

    Error messages: Use correct format string

--- src/grib_oarray.c
+++ src/grib_oarray.c
@@ -24,7 +24,7 @@ grib_oarray* grib_oarray_new(grib_context* c, size_t size, size_t incsize)
     v = (grib_oarray*)grib_context_malloc_clear(c, sizeof(grib_oarray));
     if (!v) {
         grib_context_log(c, GRIB_LOG_ERROR,
-                         "grib_oarray_new unable to allocate %ld bytes\n", sizeof(grib_oarray));
+                         "grib_oarray_new unable to allocate %lu bytes\n", sizeof(grib_oarray));
         return NULL;
     }
     v->size    = size;
@@ -34,7 +34,7 @@ grib_oarray* grib_oarray_new(grib_context* c, size_t size, size_t incsize)
     v->context = c;
     if (!v->v) {
         grib_context_log(c, GRIB_LOG_ERROR,
-                         "grib_oarray_new unable to allocate %ld bytes\n", sizeof(char*) * size);
+                         "grib_oarray_new unable to allocate %lu bytes\n", sizeof(char*) * size);
         return NULL;
     }
     return v;
@@ -51,7 +51,7 @@ static grib_oarray* grib_oarray_resize(grib_oarray* v)
     v->size = newsize;
     if (!v->v) {
         grib_context_log(c, GRIB_LOG_ERROR,
-                         "grib_oarray_resize unable to allocate %ld bytes\n", sizeof(char*) * newsize);
+                         "grib_oarray_resize unable to allocate %lu bytes\n", sizeof(char*) * newsize);
         return NULL;
     }
     return v;
