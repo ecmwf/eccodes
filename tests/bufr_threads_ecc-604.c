18d2440ecac2f56147195023e1e50042319ee6e7
Shahram Najm <masn@ecmwf.int>
2022-11-04 16:55:18 +0000

    cpplint warnings

--- tests/bufr_threads_ecc-604.c
+++ tests/bufr_threads_ecc-604.c
@@ -1,4 +1,13 @@
 /*
+ * (C) Copyright 2005- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * In applying this licence, ECMWF does not waive the privileges and immunities granted to it by
+ * virtue of its status as an intergovernmental organisation nor does it submit to any jurisdiction.
+ */
+/*
  * Test for ECC-604: Each thread creates a new BUFR handle, optionaly clone it and/or write it out
  */
 #include <time.h>
