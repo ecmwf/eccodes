8d55550732c727ad426c7fa82eba0d049bfd918d
Shahram Najm <masn@ecmwf.int>
2022-12-29 21:41:43 +0000

    ECC-1467: GRIB: Support data values array decoded as floats (Part 03)

--- src/grib_accessor_class_data_ccsds_packing.c
+++ src/grib_accessor_class_data_ccsds_packing.c
@@ -18,6 +18,7 @@
    SUPER      = grib_accessor_class_values
    IMPLEMENTS = init
    IMPLEMENTS = unpack_double
+   IMPLEMENTS = unpack_float
    IMPLEMENTS = pack_double
    IMPLEMENTS = unpack_double_element;unpack_double_element_set
    IMPLEMENTS = value_count
@@ -49,6 +50,7 @@ or edit "accessor.class" and rerun ./make_class.pl
 
 static int pack_double(grib_accessor*, const double* val, size_t* len);
 static int unpack_double(grib_accessor*, double* val, size_t* len);
+static int unpack_float(grib_accessor*, float* val, size_t* len);
 static int value_count(grib_accessor*, long*);
 static void init(grib_accessor*, const long, grib_arguments*);
 static void init_class(grib_accessor_class*);
@@ -103,7 +105,7 @@ static grib_accessor_class _grib_accessor_class_data_ccsds_packing = {
     &pack_double,                /* grib_pack procedures double */
     0,                 /* grib_pack procedures float */
     &unpack_double,              /* grib_unpack procedures double */
-    0,               /* grib_unpack procedures float */
+    &unpack_float,               /* grib_unpack procedures float */
     0,                /* grib_pack procedures string */
     0,              /* grib_unpack procedures string */
     0,          /* grib_pack array procedures string */
@@ -143,7 +145,6 @@ static void init_class(grib_accessor_class* c)
     c->pack_long    =    (*(c->super))->pack_long;
     c->unpack_long    =    (*(c->super))->unpack_long;
     c->pack_float    =    (*(c->super))->pack_float;
-    c->unpack_float    =    (*(c->super))->unpack_float;
     c->pack_string    =    (*(c->super))->pack_string;
     c->unpack_string    =    (*(c->super))->unpack_string;
     c->pack_string_array    =    (*(c->super))->pack_string_array;
@@ -322,6 +323,117 @@ static int unpack_double(grib_accessor* a, double* val, size_t* len)
 #endif
     /* ECC-1427: Performance improvement */
     grib_decode_double_array(decoded, &pos, bits8 , reference_value, bscale, dscale, n_vals, val);
+    *len = n_vals;
+
+cleanup:
+    grib_context_buffer_free(a->context, decoded);
+    return err;
+}
+
+// TODO: Not ideal!! First attempt at unpacking floats...
+//       Should be merged with unpack_double and refactored! Most probably using C++ templates
+static int unpack_float(grib_accessor* a, float* val, size_t* len)
+{
+    grib_accessor_data_ccsds_packing* self = (grib_accessor_data_ccsds_packing*)a;
+    grib_handle* hand = grib_handle_of_accessor(a);
+
+    int err = GRIB_SUCCESS, i = 0;
+    size_t buflen = 0;
+    struct aec_stream strm;
+    double bscale      = 0;
+    double dscale      = 0;
+    unsigned char* buf = NULL;
+    size_t n_vals      = 0;
+    size_t size        = 0;
+    unsigned char* decoded = NULL;
+    /*unsigned char* p       = NULL;*/
+    long pos               = 0;
+    long nn                = 0;
+
+    long binary_scale_factor  = 0;
+    long decimal_scale_factor = 0;
+    double reference_value    = 0;
+    long bits_per_value       = 0;
+    long bits8;
+
+    long ccsds_flags;
+    long ccsds_block_size;
+    long ccsds_rsi;
+
+    self->dirty = 0;
+
+    if ((err = grib_value_count(a, &nn)) != GRIB_SUCCESS)
+        return err;
+    n_vals = nn;
+
+    if ((err = grib_get_long_internal(hand, self->bits_per_value, &bits_per_value)) != GRIB_SUCCESS)
+        return err;
+    if ((err = grib_get_double_internal(hand, self->reference_value, &reference_value)) != GRIB_SUCCESS)
+        return err;
+    if ((err = grib_get_long_internal(hand, self->binary_scale_factor, &binary_scale_factor)) != GRIB_SUCCESS)
+        return err;
+    if ((err = grib_get_long_internal(hand, self->decimal_scale_factor, &decimal_scale_factor)) != GRIB_SUCCESS)
+        return err;
+
+    /* ECC-477: Don't call grib_get_long_internal to suppress error message being output */
+    if ((err = grib_get_long(hand, self->ccsds_flags, &ccsds_flags)) != GRIB_SUCCESS)
+        return err;
+
+    if ((err = grib_get_long_internal(hand, self->ccsds_block_size, &ccsds_block_size)) != GRIB_SUCCESS)
+        return err;
+    if ((err = grib_get_long_internal(hand, self->ccsds_rsi, &ccsds_rsi)) != GRIB_SUCCESS)
+        return err;
+
+    /* TODO: This should be called upstream */
+    if (*len < n_vals)
+        return GRIB_ARRAY_TOO_SMALL;
+
+    /* Special case */
+    if (bits_per_value == 0) {
+        for (i = 0; i < n_vals; i++)
+            val[i] = reference_value;
+        *len = n_vals;
+        return GRIB_SUCCESS;
+    }
+
+    bscale = grib_power(binary_scale_factor, 2);
+    dscale = grib_power(-decimal_scale_factor, 10);
+
+    buflen = grib_byte_count(a);
+    buf = (unsigned char*)hand->buffer->data;
+    buf += grib_byte_offset(a);
+
+    strm.flags           = ccsds_flags;
+    strm.bits_per_sample = bits_per_value;
+    strm.block_size      = ccsds_block_size;
+    strm.rsi             = ccsds_rsi;
+
+    strm.next_in  = buf;
+    strm.avail_in = buflen;
+
+    bits8   = ((bits_per_value + 7) / 8) * 8;
+    size    = n_vals * ((bits_per_value + 7) / 8);
+    decoded = (unsigned char*)grib_context_buffer_malloc_clear(a->context, size);
+    if (!decoded) {
+        err = GRIB_OUT_OF_MEMORY;
+        goto cleanup;
+    }
+    strm.next_out  = decoded;
+    strm.avail_out = size;
+
+    if (hand->context->debug) print_aec_stream_info(&strm, "unpack_float");
+
+    if ((err = aec_buffer_decode(&strm)) != AEC_OK) {
+        grib_context_log(a->context, GRIB_LOG_ERROR, "CCSDS unpack_float: aec_buffer_decode error %d (%s)\n",
+                         err, aec_get_error_message(err));
+        err = GRIB_ENCODING_ERROR;
+        goto cleanup;
+    }
+
+    pos = 0;
+
+    /* ECC-1427: Performance improvement */
+    grib_decode_float_array(decoded, &pos, bits8 , reference_value, bscale, dscale, n_vals, val);
     *len = n_vals;
 
 cleanup:
@@ -672,6 +784,11 @@ static void print_error_feature_not_enabled(grib_context* c)
                      "Please rebuild with -DENABLE_AEC=ON (Adaptive Entropy Coding library)");
 }
 static int unpack_double(grib_accessor* a, double* val, size_t* len)
+{
+    print_error_feature_not_enabled(a->context);
+    return GRIB_FUNCTIONALITY_NOT_ENABLED;
+}
+static int unpack_float(grib_accessor* a, float* val, size_t* len)
 {
     print_error_feature_not_enabled(a->context);
     return GRIB_FUNCTIONALITY_NOT_ENABLED;
