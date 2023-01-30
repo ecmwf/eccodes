c6717fffabfbc1f465173fbdb1b62a48f7f3fb86
Eugen Betke <maee@ad6-200.bullx>
2023-01-30 15:47:48 +0000

    ECC-1467: Basic shell constructs

--- tests/grib_ecc-1467.sh
+++ tests/grib_ecc-1467.sh
@@ -11,32 +11,32 @@
 . ./include.ctest.sh
 
 # Constant Fields
-gfiles+="constant_field.grib1 " # grid_simple, edition=1
-gfiles+="constant_field.grib2 " # grid_simple, edition=2
+gfiles="constant_field.grib1" # grid_simple, edition=1
+gfiles="$gfiles constant_field.grib2" # grid_simple, edition=2
 
 # Grid Simple
-gfiles+="sst_globus0083.grib " # grid_simple, edition=1
-gfiles+="reduced_latlon_surface.grib1 " # grid_simple, edition=1
-gfiles+="reduced_latlon_surface.grib2 " # grid_simple, edition=2
-gfiles+="regular_latlon_surface.grib1 " # grid_simple, edition=1
-gfiles+="regular_latlon_surface.grib2 " # grid_simple, edition=2
+gfiles="$gfiles sst_globus0083.grib" # grid_simple, edition=1
+gfiles="$gfiles reduced_latlon_surface.grib1" # grid_simple, edition=1
+gfiles="$gfiles reduced_latlon_surface.grib2" # grid_simple, edition=2
+gfiles="$gfiles regular_latlon_surface.grib1" # grid_simple, edition=1
+gfiles="$gfiles regular_latlon_surface.grib2" # grid_simple, edition=2
 
 # Spherical Complex
-gfiles+="spherical_pressure_level.grib1 " # spectral_complex, edition=1
-gfiles+="spherical_pressure_level.grib2 " # spectral_complex, edition=2
+gfiles="$gfiles spherical_pressure_level.grib1" # spectral_complex, edition=1
+gfiles="$gfiles spherical_pressure_level.grib2" # spectral_complex, edition=2
 
 # Grid Complex
-gfiles+=" " # grid_complex, edition=1
-gfiles+="gfs.complex.mvmu.grib2 " # grid_complex, edition=2, g22order_packing
+#gfiles="$gfiles " # grid_complex, edition=1
+gfiles="$gfiles gfs.complex.mvmu.grib2" # grid_complex, edition=2, g22order_packing
 
 # Second Order
-#gfiles+="lfpw.grib1 " # grid_second_order, edition=1
-#gfiles+=" " # grid_second_order, edition=2
+#gfiles="$gfiles lfpw.grib1" # grid_second_order, edition=1
+#gfiles="$gfiles " # grid_second_order, edition=2
 
 # CCSDS
 if [ $HAVE_AEC -eq 1 ]; then
     echo "Adding extra files (HAVE_AEC=1)"
-    gfiles+="ccsds.grib2 "
+    gfiles="$gfiles ccsds.grib2"
 fi
 
 for f in $gfiles; do
