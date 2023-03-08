dfed200cf0e22ad5d380099b5449956b146a0d02
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2023-03-08 00:54:46 +0000

    MIR-602 Refactor

--- src/mir/util/Grib.h
+++ src/mir/util/Grib.h
@@ -14,26 +14,11 @@
 
 #include <eccodes.h>
 
-#include <iosfwd>
 #include <sstream>
 #include <string>
 #include <vector>
 
 #include "mir/util/Exceptions.h"
-
-
-namespace eckit {
-class Fraction;
-}
-
-namespace mir {
-namespace param {
-class MIRParametrisation;
-}
-namespace repres {
-class Representation;
-}
-}  // namespace mir
 
 
 inline bool grib_call(int e, const char* call, bool missingOK = false) {
@@ -111,97 +96,3 @@ void grib_reorder(std::vector<double>& values, long scanningMode, size_t Ni, siz
 
 
 void grib_get_unique_missing_value(const std::vector<double>& values, double& missingValue);
-
-
-namespace mir::util::grib {
-
-
-void check_edition_conversion(const mir::param::MIRParametrisation&);
-
-
-struct Fraction {
-    using value_type = long;
-
-    explicit Fraction(double);
-    explicit Fraction(const eckit::Fraction&);
-    Fraction(value_type _numerator, value_type _denominator) :
-        num(_numerator), den(_denominator == 0 || _numerator == 0 ? 1 : _denominator) {}
-
-    const value_type num;
-    const value_type den;
-};
-
-
-Fraction::value_type lcm(Fraction::value_type a, Fraction::value_type b);
-
-
-template <typename... Longs>
-Fraction::value_type lcm(Fraction::value_type a, Fraction::value_type b, Longs... cde) {
-    return lcm(a, lcm(b, cde...));
-}
-
-
-long gcd(long a, long b);
-
-
-template <typename... Longs>
-long gcd(long a, long b, Longs... cde) {
-    return gcd(a, gcd(b, cde...));
-}
-
-
-struct BasicAngle : Fraction {
-    using Fraction::Fraction;
-    BasicAngle(Fraction, Fraction, Fraction, Fraction, Fraction, Fraction);
-    explicit BasicAngle(const grib_info&);
-
-    void fillGrib(grib_info&) const;
-    Fraction::value_type numerator(const Fraction&) const;
-
-    static void list(std::ostream&);
-};
-
-
-class Packing {
-public:
-    Packing(const std::string& name, const param::MIRParametrisation&);
-    Packing(const Packing&) = delete;
-    Packing(Packing&&)      = delete;
-
-    virtual ~Packing() = default;
-
-    void operator=(const Packing&) = delete;
-    void operator=(Packing&&)      = delete;
-
-    virtual void fill(const repres::Representation*, grib_info&) const  = 0;
-    virtual void set(const repres::Representation*, grib_handle*) const = 0;
-
-    bool sameAs(const Packing*) const;
-    bool printParametrisation(std::ostream&) const;
-    bool empty() const;
-
-    static Packing* build(const param::MIRParametrisation&);
-    static void list(std::ostream&);
-
-protected:
-    long bitsPerValue_;
-    long edition_;
-    std::string packing_;
-    long precision_;
-
-    bool defineBitsPerValue_;
-    bool defineBitsPerValueBeforePacking_;
-    bool defineEdition_;
-    bool definePacking_;
-    bool definePrecision_;
-
-    bool gridded() const { return gridded_; }
-    void fill(grib_info&, long) const;
-    void set(grib_handle*, const std::string&) const;
-
-private:
-    const bool gridded_;
-};
-
-
-}  // namespace mir::util::grib
