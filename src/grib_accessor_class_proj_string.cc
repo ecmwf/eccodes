c60c79fbcd4fb8aa078320ae152f26193e294fb3
Shahram Najm <masn@ecmwf.int>
2023-03-28 15:25:54 +0000

    ECC-1552: Alternative implementation using WGS84

--- src/grib_accessor_class_proj_string.cc
+++ src/grib_accessor_class_proj_string.cc
@@ -294,19 +294,20 @@ static int proj_polar_stereographic(grib_handle* h, char* result)
     return err;
 }
 
-#if 0
+
 // ECC-1552: This is for regular_ll, regular_gg, reduced_ll, reduced_gg
 static int proj_unprojected(grib_handle* h, char* result)
 {
     int err = 0;
-    char shape[64] = {0,};
-    if ((err = get_earth_shape(h, shape)) != GRIB_SUCCESS)
-        return err;
-
-    snprintf(result, 1024, "+proj=longlat %s", shape);
+    //char shape[64] = {0,};
+    //if ((err = get_earth_shape(h, shape)) != GRIB_SUCCESS)
+    //    return err;
+    //snprintf(result, 1024, "+proj=longlat %s", shape);
+    snprintf(result, 1024, "+proj=longlat +datum=WGS84 +no_defs +type=crs");
+    
     return err;
 }
-#endif
+
 
 static int proj_mercator(grib_handle* h, char* result)
 {
@@ -325,10 +326,10 @@ static int proj_mercator(grib_handle* h, char* result)
 
 #define NUMBER(a) (sizeof(a) / sizeof(a[0]))
 static proj_mapping proj_mappings[] = {
-    // { "regular_ll", &proj_unprojected },
-    // { "regular_gg", &proj_unprojected },
-    // { "reduced_ll", &proj_unprojected },
-    // { "reduced_gg", &proj_unprojected },
+    { "regular_ll", &proj_unprojected },
+    { "regular_gg", &proj_unprojected },
+    { "reduced_ll", &proj_unprojected },
+    { "reduced_gg", &proj_unprojected },
 
     { "mercator", &proj_mercator },
     { "lambert", &proj_lambert_conformal },
