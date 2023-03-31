e760d4375255a505d56b8af78cab2add9ffbb9ef
Dusan Figala <dusanfigala@protonmail.com>
2023-03-31 15:22:43 +0200

    Trigger nightly tests on HPC

---
+++ .github/ci-nightly-test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+module unload ecmwf-toolbox # Very important if you are testing your own build/install
+module load  cdo/new
+module load  numdiff
+module load  nccmp netcdf4/new
+module load  gnuparallel/new
+module load  python3
+
+cd ~masn/REGRESSION_TESTING/ecCodes
+./par-suite.sh -w $TMPDIR/install/eccodes
