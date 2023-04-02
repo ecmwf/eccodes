94c12b30ccb6ccb04031a64cfa881827813d0a46
Shahram Najm <masn@ecmwf.int>
2023-04-02 15:31:27 +0100

    ECC-1560: Add test

--- tests/grib_ecc-1560.sh
+++ tests/grib_ecc-1560.sh
@@ -15,7 +15,7 @@ REDIRECT=/dev/null
 label="grib_ecc-1560_test"
 temp_PDTN08_Grib=temp.$label.PDTN08.grib
 temp_PDTN11_Grib=temp.$label.PDTN11.grib
-
+tempGrib=temp.$label.grib
 sample_grib2=$ECCODES_SAMPLES_PATH/GRIB2.tmpl
 
 # We set PDTN=8 and level to 1.5 metres above ground
@@ -28,6 +28,17 @@ grib_check_key_equals $temp_PDTN08_Grib level:d 1.5
 ${tools_dir}/grib_set -s productDefinitionTemplateNumber=11 $temp_PDTN08_Grib $temp_PDTN11_Grib
 grib_check_key_equals $temp_PDTN11_Grib level:d 1.5
 
-${tools_dir}/grib_compare $temp_PDTN08_Grib $temp_PDTN11_Grib
+
+# Set integer value using the int/double encoding
+# -----------------------------------------------
+${tools_dir}/grib_set -s typeOfFirstFixedSurface=105,level:i=34 $sample_grib2 $tempGrib
+grib_check_key_equals $tempGrib scaleFactorOfFirstFixedSurface 0
+grib_check_key_equals $tempGrib scaledValueOfFirstFixedSurface 34
+
+${tools_dir}/grib_set -s typeOfFirstFixedSurface=105,level:d=34 $sample_grib2 $tempGrib
+grib_check_key_equals $tempGrib scaleFactorOfFirstFixedSurface 0
+grib_check_key_equals $tempGrib scaledValueOfFirstFixedSurface 34
+
+
 # Clean up
-rm -f $temp_PDTN08_Grib $temp_PDTN11_Grib
+rm -f $temp_PDTN08_Grib $temp_PDTN11_Grib $tempGrib
