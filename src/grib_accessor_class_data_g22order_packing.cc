98e11f31cbc464fbbdc408d9c96c503bdc164394
Shahram Najm <masn@ecmwf.int>
2023-05-09 18:48:39 +0100

    Compiler warning: [-Wmaybe-uninitialized]

--- src/grib_accessor_class_data_g22order_packing.cc
+++ src/grib_accessor_class_data_g22order_packing.cc
@@ -358,7 +358,7 @@ static int spatial_difference (grib_context *c, unsigned long* vals, long  len, 
 
 int post_process(grib_context* c, long* vals, long len, long order, long bias, const unsigned long extras[2])
 {
-    unsigned long last, penultimate, j = 0;
+    unsigned long last, penultimate = 0, j = 0;
     Assert(order > 0);
     Assert(order <= 3);
     if (!vals)
