ccf5ed8f55ea8e43efa7008db29291dae22d7054
Shahram Najm <masn@ecmwf.int>
2023-06-25 12:27:04 +0100

    Scripts: Legacy parameter encodings

--- definitions/create_legacy_wmo_defs.sh
+++ definitions/create_legacy_wmo_defs.sh
@@ -4,16 +4,17 @@ set -eu
 # Usage:
 #  create_legacy_def.sh $paramId $paramId...
 #
-# This script will insert the local ECMWF GRIB2 representation
-# for each paramId into the files:
+# This script will insert the WMO GRIB2 representation
+# for each given paramId into the files:
 #   definitions/grib2/paramId.legacy.def
 #   definitions/grib2/shortName.legacy.def
-#   etc
-# This is normally run for those GRIB2 parameters which had
+#   etc.
+# This is run for those GRIB2 parameters which had
 # a WMO representation which later became deprecated.
 #
 # Assumptions:
 #  The ecCodes tools grib_set and grib_get are available
+#  TMPDIR is defined and points to a temporary directory
 #
 
 if [ $# -eq 0 ]; then
@@ -36,10 +37,12 @@ create_legacy_def()
 
     _legacy=$(echo $_file | sed -e 's/\.def/.legacy.def/')
     #echo "Doing |$dir|  f=|$file| l=|$legacy|"
+    # First check the given name exists in the file
     grep -q "^#$_name$" $_dir/$_file
     if [ $? -eq 0 ]; then
         _name="#$_name"
         found=1
+        # Copy the parameter definition from the file to its legacy version
         echo "$_name" >> $_dir/$_legacy
         sed -e "1,/^$_name$/d" -e '/#/,$d' $_dir/$_file | tr -d '\t' >> $_dir/$_legacy
     fi
@@ -52,12 +55,13 @@ create_legacy_defs()
     create_legacy_def "$name" $defs shortName.def
     create_legacy_def "$name" $defs name.def
     create_legacy_def "$name" $defs units.def
+    create_legacy_def "$name" $defs cfVarName.def
 }
 
 # For each paramId, get its full name
 count=0
 for pid in $pids; do
-    echo "Doing $pid ..."
+    echo "Doing paramId=$pid ..."
     set +e
     grib_set -s paramId=$pid $sample2 $temp 2>/dev/null
     status=$?
