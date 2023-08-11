b3e4351635ddd45c18e8bd012aae6b8345c3619d
Shahram Najm <masn@ecmwf.int>
2023-08-11 22:03:09 +0100

    Tools: Cleanup

--- tools/gts_ls.cc
+++ tools/gts_ls.cc
@@ -78,6 +78,7 @@ int grib_tool_new_filename_action(grib_runtime_options* options, const char* fil
 
 int grib_tool_new_file_action(grib_runtime_options* options, grib_tools_file* file)
 {
+    exit_if_input_is_directory(tool_name, file->name);
     return 0;
 }
 
