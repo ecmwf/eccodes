3eaef920ba81642c7f3438830c1cb4d9db9aa7c1
Shahram Najm <masn@ecmwf.int>
2023-08-23 22:18:44 +0100

    Testing: Increase coverage

--- tests/gts_compare.sh
+++ tests/gts_compare.sh
@@ -35,7 +35,7 @@ fBufrInput2=${label}".bufr.input2"
 gts_file="EGRR20150317121020_00493212.DAT"
 echo "Test: comparing the same files" >> $fLog
 echo "file: $gts_file" >> $fLog
-${tools_dir}//gts_compare $gts_file $gts_file
+${tools_dir}/gts_compare $gts_file $gts_file
 
 #----------------------------------------------------
 # Test: comparing two different files
@@ -46,18 +46,22 @@ cat > $fRules<<EOF
 EOF
 ${tools_dir}/gts_filter -o $fGtsTmp $fRules $gts_file
 set +e
-${tools_dir}//gts_compare $gts_file $fGtsTmp
+${tools_dir}/gts_compare -v -d $gts_file $fGtsTmp
 status=$?
 set -e
 if [ $status -eq 0 ]; then
    echo "gts_compare should have failed if files are different" >&2
    exit 1
 fi
+# The -d option should have created these two files
+[ -f error1_1.gts ]
+[ -f error2_1.gts ]
+rm -f error1_1.gts error2_1.gts
 
 #----------------------------------------------------
 # Test: comparing with and without the -b switch
 #----------------------------------------------------
-${tools_dir}//gts_compare -b GG $gts_file $fGtsTmp >> $fLog
+${tools_dir}/gts_compare -b GG $gts_file $fGtsTmp >> $fLog
 
 #Clean up
 rm -f $fLog $fGtsTmp $fRules
