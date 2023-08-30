6baae519d9ab7b2103a80fe6d8573aa2d9760555
Shahram Najm <masn@ecmwf.int>
2023-08-30 11:02:52 +0000

    ECC-1674: Test added

--- tests/grib_run_length_packing.sh
+++ tests/grib_run_length_packing.sh
@@ -16,6 +16,8 @@ label="grib_run_length_packing_test"
 tempGrib=temp.$label.grib
 tempLog=temp.$label.log
 
+# Decoding
+# ------------------
 input=$data_dir/run_length_packing.grib2
 if [ ! -f "$input" ]; then
     echo " Input data file missing: $input"
@@ -30,4 +32,15 @@ stats=$(${tools_dir}/grib_get -F%.5f -p packingType,min,max,avg $input)
 grib_check_key_equals $input numberOfMissing 71493
 grib_check_key_equals $input missingValuesPresent 1
 
+# Encoding
+# -----------------
+$EXEC ${test_dir}/grib_run_length_packing $tempGrib
+${tools_dir}/grib_dump  $tempGrib
+grib_check_key_equals $tempGrib packingType grid_run_length
+
+stats=`${tools_dir}/grib_get -M -F%.1f -p min,max,avg $tempGrib`
+[ "$stats" = "10.1 10.3 10.2" ]
+
+
+# Clean up
 rm -f $tempGrib $tempLog
