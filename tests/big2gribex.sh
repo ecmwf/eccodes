1f335a011e88e85c4a6909cdbad071581fd25b48
shahramn <shahram.najm@ecmwf.int>
2024-05-01 21:53:06 +0100

    Testing: big2gribex

---
+++ tests/big2gribex.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# (C) Copyright 2005- ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# 
+# In applying this licence, ECMWF does not waive the privileges and immunities granted to it by
+# virtue of its status as an intergovernmental organisation nor does it submit to any jurisdiction.
+#
+
+. ./include.ctest.sh
+
+label="big2gribex_test"
+tempGrib=temp.$label.grib
+
+if [ ! -x ${tools_dir}/big2gribex ]; then
+    exit 0
+fi
+
+${tools_dir}/big2gribex $data_dir/test.grib1 $tempGrib
+
+# Clean up
+rm -f $tempGrib
