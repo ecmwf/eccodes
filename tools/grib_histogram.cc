beb8abb32c21291e433055baf3e44f10459a1e2a
shahramn <shahram.najm@ecmwf.int>
2024-05-02 16:23:06 +0100

    Dead code removal

--- tools/grib_histogram.cc
+++ tools/grib_histogram.cc
@@ -76,14 +76,6 @@ int grib_tool_new_handle_action(grib_runtime_options* options, grib_handle* h)
         names[name_count++] = options->requested_print_keys[i].name;
     }
 
-    if (!options->skip) {
-        if (options->set_values_count != 0)
-            err = grib_set_values(h, options->set_values, options->set_values_count);
-
-        if (err != GRIB_SUCCESS && options->fail)
-            exit(err);
-    }
-
     GRIB_CHECK(grib_get_long(h, "missingValuesPresent", &missingValuesPresent), 0);
 
     GRIB_CHECK(grib_get_size(h, "values", &size), 0);
