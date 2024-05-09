e47f0922f42e4eb328782c5ceaa3df69dcc79849
shahramn <shahram.najm@ecmwf.int>
2024-05-09 12:51:25 +0100

    Move functions from header to local static. Use 'pragma once'

--- src/accessor/grib_accessor_class_ibmfloat.h
+++ src/accessor/grib_accessor_class_ibmfloat.h
@@ -8,10 +8,8 @@
  * virtue of its status as an intergovernmental organisation nor does it submit to any jurisdiction.
  */
 
-#ifndef eccodes_accessor_ibmfloat_h
-#define eccodes_accessor_ibmfloat_h
+#pragma once
 
-#include "grib_api_internal.h"
 #include "grib_accessor_class_double.h"
 
 class grib_accessor_ibmfloat_t : public grib_accessor_double_t
@@ -36,36 +34,4 @@ public:
     void init(grib_accessor*, const long, grib_arguments*) override;
     void update_size(grib_accessor*, size_t) override;
     int nearest_smaller_value(grib_accessor*, double, double*) override;
-private:
-    template <typename T> int unpack(grib_accessor*, T*, size_t*);
 };
-
-
-template <typename T>
-int grib_accessor_class_ibmfloat_t::unpack(grib_accessor* a, T* val, size_t* len){
-    static_assert(std::is_floating_point<T>::value, "Requires floating point numbers");
-    unsigned long rlen = 0;
-    long count         = 0;
-    int err            = 0;
-    unsigned long i    = 0;
-    long bitp          = a->offset * 8;
-    grib_handle* hand  = grib_handle_of_accessor(a);
-
-    err = a->value_count(&count);    if (err)
-        return err;
-    rlen = count;
-
-    if (*len < rlen) {
-        grib_context_log(a->context, GRIB_LOG_ERROR, "Wrong size (%zu) for %s, it contains %lu values", *len, a->name, rlen);
-        *len = 0;
-        return GRIB_ARRAY_TOO_SMALL;
-    }
-
-    for (i = 0; i < rlen; i++)
-        val[i] = (T)grib_long_to_ibm(grib_decode_unsigned_long(hand->buffer->data, &bitp, 32));
-
-    *len = rlen;
-    return GRIB_SUCCESS;
-}
-
-#endif /* eccodes_accessor_ibmfloat_h */
--- src/accessor/grib_accessor_class_ieeefloat.cc
+++ src/accessor/grib_accessor_class_ieeefloat.cc
@@ -11,20 +11,23 @@
 
 #include "grib_accessor_class_ieeefloat.h"
 
-grib_accessor_class_ieeefloat_t _grib_accessor_class_ieeefloat{"ieeefloat"};
+grib_accessor_class_ieeefloat_t _grib_accessor_class_ieeefloat{ "ieeefloat" };
 grib_accessor_class* grib_accessor_class_ieeefloat = &_grib_accessor_class_ieeefloat;
 
 
-void grib_accessor_class_ieeefloat_t::init(grib_accessor* a, const long len, grib_arguments* arg){
+void grib_accessor_class_ieeefloat_t::init(grib_accessor* a, const long len, grib_arguments* arg)
+{
     grib_accessor_class_double_t::init(a, len, arg);
     grib_accessor_ieeefloat_t* self = (grib_accessor_ieeefloat_t*)a;
-    long count                    = 0;
-    self->arg                     = arg;
-    a->value_count(&count);    a->length = 4 * count;
+    long count = 0;
+    self->arg = arg;
+    a->value_count(&count);
+    a->length = 4 * count;
     Assert(a->length >= 0);
 }
 
-int grib_accessor_class_ieeefloat_t::value_count(grib_accessor* a, long* len){
+int grib_accessor_class_ieeefloat_t::value_count(grib_accessor* a, long* len)
+{
     grib_accessor_ieeefloat_t* self = (grib_accessor_ieeefloat_t*)a;
     *len = 0;
 
@@ -35,14 +38,16 @@ int grib_accessor_class_ieeefloat_t::value_count(grib_accessor* a, long* len){
     return grib_get_long_internal(grib_handle_of_accessor(a), grib_arguments_get_name(a->parent->h, self->arg, 0), len);
 }
 
-int grib_accessor_class_ieeefloat_t::pack_double(grib_accessor* a, const double* val, size_t* len){
+int grib_accessor_class_ieeefloat_t::pack_double(grib_accessor* a, const double* val, size_t* len)
+{
     grib_accessor_ieeefloat_t* self = (grib_accessor_ieeefloat_t*)a;
-    int ret                       = 0;
-    unsigned long i               = 0;
-    unsigned long rlen            = (unsigned long)*len;
-    size_t buflen                 = 0;
-    unsigned char* buf            = NULL;
-    long off                      = 0;
+
+    int ret            = 0;
+    unsigned long i    = 0;
+    unsigned long rlen = (unsigned long)*len;
+    size_t buflen      = 0;
+    unsigned char* buf = NULL;
+    long off           = 0;
 
     if (*len < 1) {
         grib_context_log(a->context, GRIB_LOG_ERROR, "Wrong size for %s, it packs at least 1 value", a->name);
@@ -55,7 +60,7 @@ int grib_accessor_class_ieeefloat_t::pack_double(grib_accessor* a, const double*
         ret = grib_encode_unsigned_long(grib_handle_of_accessor(a)->buffer->data, grib_ieee_to_long(val[0]), &off, 32);
         if (*len > 1)
             grib_context_log(a->context, GRIB_LOG_WARNING, "ieeefloat: Trying to pack %zu values in a scalar %s, packing first value",
-                            *len, a->name);
+                             *len, a->name);
         if (ret == GRIB_SUCCESS)
             len[0] = 1;
         return ret;
@@ -80,19 +85,52 @@ int grib_accessor_class_ieeefloat_t::pack_double(grib_accessor* a, const double*
     return ret;
 }
 
-int grib_accessor_class_ieeefloat_t::unpack_double(grib_accessor* a, double* val, size_t* len){
+
+template <typename T>
+static int unpack(grib_accessor* a, T* val, size_t* len)
+{
+    static_assert(std::is_floating_point<T>::value, "Requires floating point numbers");
+
+    long rlen         = 0;
+    int err           = 0;
+    long i            = 0;
+    long bitp         = a->offset * 8;
+    grib_handle* hand = grib_handle_of_accessor(a);
+
+    err = a->value_count(&rlen);
+    if (err)
+        return err;
+
+    if (*len < (size_t)rlen) {
+        grib_context_log(a->context, GRIB_LOG_ERROR, "Wrong size (%zu) for %s, it contains %ld values", *len, a->name, rlen);
+        *len = 0;
+        return GRIB_ARRAY_TOO_SMALL;
+    }
+
+    for (i = 0; i < rlen; i++)
+        val[i] = (T)grib_long_to_ieee(grib_decode_unsigned_long(hand->buffer->data, &bitp, 32));
+
+    *len = rlen;
+    return GRIB_SUCCESS;
+}
+
+int grib_accessor_class_ieeefloat_t::unpack_double(grib_accessor* a, double* val, size_t* len)
+{
     return unpack<double>(a, val, len);
 }
 
-int grib_accessor_class_ieeefloat_t::unpack_float(grib_accessor* a, float* val, size_t* len){
+int grib_accessor_class_ieeefloat_t::unpack_float(grib_accessor* a, float* val, size_t* len)
+{
     return unpack<float>(a, val, len);
 }
 
-void grib_accessor_class_ieeefloat_t::update_size(grib_accessor* a, size_t s){
+void grib_accessor_class_ieeefloat_t::update_size(grib_accessor* a, size_t s)
+{
     a->length = (long)s;
     Assert(a->length >= 0);
 }
 
-int grib_accessor_class_ieeefloat_t::nearest_smaller_value(grib_accessor* a, double val, double* nearest){
+int grib_accessor_class_ieeefloat_t::nearest_smaller_value(grib_accessor* a, double val, double* nearest)
+{
     return grib_nearest_smaller_ieee_float(val, nearest);
 }
