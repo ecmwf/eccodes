ad000e37137603bb09733a2b3b5d1670375d2fb1
shahramn <shahram.najm@ecmwf.int>
2024-07-25 15:42:46 +0100

    GitHub nightly: Add set -u for unbound variables

--- .github/ci-nightly-test.sh
+++ .github/ci-nightly-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -eu
 
 # We do not want to come across the ecCodes tools in the toolbox
 module unload ecmwf-toolbox
@@ -14,12 +14,16 @@ module load python3
 
 version=$(cat $TMPDIR/eccodes/VERSION)
 
+# Note:
+# The environment variable "GH_TOKEN" needs to be there for the clone to succeed
+#
+
 regression_suite_dir=$TMPDIR/eccodes-regression-tests
 mkdir -p $regression_suite_dir
 git clone https://${GH_TOKEN}@github.com/ecmwf/eccodes-regression-tests.git $regression_suite_dir
 cd $regression_suite_dir
 
-#cd ~masn/REGRESSION_TESTING/ecCodes
+# Launch the regression tests in parallel (This script uses GNU parallel)
 ./par-suite.sh -w $TMPDIR/install/eccodes/$version
 
 # For debugging specific test(s)
