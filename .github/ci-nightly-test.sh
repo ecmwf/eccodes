ec1a3a10f50aca73789ed3ca7409dc2d63a31bd3
shahramn <shahram.najm@ecmwf.int>
2024-07-31 15:32:54 +0100

    Regression test suite: For tests use the git repo and for DATA/REF point to elsewhere

--- .github/ci-nightly-test.sh
+++ .github/ci-nightly-test.sh
@@ -18,13 +18,13 @@ version=$(cat $TMPDIR/eccodes/VERSION)
 # The environment variable "GH_TOKEN" needs to be there for the clone to succeed
 #
 
-#regression_suite_dir=$TMPDIR/eccodes-regression-tests
-#mkdir -p $regression_suite_dir
-#git clone https://${GH_TOKEN}@github.com/ecmwf/eccodes-regression-tests.git $regression_suite_dir
-#cd $regression_suite_dir
+regression_suite_dir=$TMPDIR/eccodes-regression-tests
+mkdir -p $regression_suite_dir
+git clone https://${GH_TOKEN}@github.com/ecmwf/eccodes-regression-tests.git $regression_suite_dir
+cd $regression_suite_dir
 
 # Launch the regression tests in parallel (This script uses GNU parallel)
-cd ~masn/REGRESSION_TESTING/ecCodes
+# cd ~masn/REGRESSION_TESTING/ecCodes
 ./par-suite.sh -w $TMPDIR/install/eccodes/$version
 
 # For debugging specific test(s)
