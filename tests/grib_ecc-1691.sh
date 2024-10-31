3a6b657cf5f3beeb4921adea0f66f3e639812564
Matthew Griffith <Matthew.Griffith@ecmwf.int>
2024-10-31 13:27:11 +0000

    ECC-1832: Updated test to look at new key.

--- tests/grib_ecc-1691.sh
+++ tests/grib_ecc-1691.sh
@@ -97,5 +97,18 @@ ${tools_dir}/grib_ls -jm $temp_grib_a
 ${tools_dir}/grib_set -s productionStatusOfProcessedData=12 $sample_grib2 $temp_grib_a
 ${tools_dir}/grib_compare -b productionStatusOfProcessedData $sample_grib2 $temp_grib_a
 
+# ECC-1832 On-Demand DT metadata
+# Check setting dataset to on-demand-extremes-dt (4). Check keys are present and equal defaults
+${tools_dir}/grib_set -s dataset=4 $destine_sample $temp_grib_a
+
+grib_check_key_exists $temp_grib_a dataset,centroid
+grib_check_key_equals $temp_grib_a "dataset,dataset:s,centroid" "4 on-demand-extremes-dt unknown"
+
+# Check an example where a few additional things are set in on-demand-extremes-dt
+
+${tools_dir}/grib_set -s dataset=4,centroid=gcpkd2 $destine_sample $temp_grib_a
+
+grib_check_key_equals $temp_grib_a "centroid" "gcpkd2"
+
 # Clean up
 rm -f $temp_grib_a $temp_grib_b $destine_sample
