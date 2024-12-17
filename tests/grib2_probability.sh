38c6ca53e92a80d30b091a05d0d4341565a2c709
shahramn <shahram.najm@ecmwf.int>
2024-12-17 16:42:55 +0000

    Testing: Probability and Wave Spectra

---
+++ tests/grib2_probability.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# (C) Copyright 2005- ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# 
+# In applying this licence, ECMWF does not waive the privileges and immunities granted to it by
+# virtue of its status as an intergovernmental organisation nor does it submit to any jurisdiction.
+#
+
+. ./include.ctest.sh
+
+label="grib2_probability_test"
+temp=temp.$label
+tempGribA=temp.$label.A.grib
+tempGribB=temp.$label.B.grib
+tempSample=temp.$label.tmpl
+tempRef=temp.$label.ref
+tempOut=temp.$label.txt
+
+sample2=$ECCODES_SAMPLES_PATH/GRIB2.tmpl
+
+latest=`${tools_dir}/grib_get -p tablesVersionLatest $sample2`
+
+# Check PDTN
+# -----------
+${tools_dir}/grib_set -s tablesVersion=$latest,paramId=131256 $sample2 $tempGribA
+grib_check_key_equals $tempGribA productDefinitionTemplateNumber,shortName,probabilityType '5 10cogug25 3'
+
+# Clean up
+rm -f $tempSample $temp $tempGribA $tempGribB $tempRef $tempOut
