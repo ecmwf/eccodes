7059631bce405262640f0e9954b8a5645413b403
shahramn <shahram.najm@ecmwf.int>
2025-02-25 15:28:37 +0000

    ECC-2034: Test

--- tests/grib_ecc-2034.cc
+++ tests/grib_ecc-2034.cc
@@ -23,6 +23,13 @@ int main(int argc, char** argv)
     codes_handle* h = codes_grib_handle_new_from_samples(c,  "regular_ll_pl_grib1");
     assert(h);
 
+    // Check limits
+    long pMin, pMax;
+    CODES_CHECK(codes_get_long(h, "param_value_min", &pMin), 0);
+    CODES_CHECK(codes_get_long(h, "param_value_max", &pMax), 0);
+    assert( pMin == 160 );
+    assert( pMax == 370 );
+
     size_t values_len = 0;
     CODES_CHECK(codes_get_size(h, "values", &values_len), 0);
 
@@ -42,22 +49,29 @@ int main(int argc, char** argv)
     CODES_CHECK(codes_set_long(h, "bitsPerValue", 16), 0);
 
     // Set data values without any data quality checks
+    printf("Submit values. Checks disabled...\n");
     codes_context_set_data_quality_checks(c, 0);
     err = codes_set_double_array(h, "values", values, values_len);
     assert(!err);
 
     // Enable checks: Should fail
+    printf("Submit values. Checks enabled...\n");
     codes_context_set_data_quality_checks(c, 1);
     err = codes_set_double_array(h, "values", values, values_len);
     assert(err == GRIB_OUT_OF_RANGE);
 
     // Change paramId. Now should pass
-    CODES_CHECK(codes_set_long(h, "paramId", 49), 0);
+    printf("Change parameter. Submit values. Checks enabled...\n");
+    CODES_CHECK(codes_set_long(h, "paramId", 172), 0);
+    CODES_CHECK(codes_get_long(h, "param_value_min", &pMin), 0);
+    CODES_CHECK(codes_get_long(h, "param_value_max", &pMax), 0);
+    assert( pMin == 0 );
+    assert( pMax == 1 );
     err = codes_set_double_array(h, "values", values, values_len);
     assert(!err);
 
     free(values);
     codes_handle_delete(h);
-
+    printf("Fin\n");
     return 0;
 }
