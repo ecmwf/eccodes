704c09d4c56ce388df799a420670857b9e6581b3
shahramn <shahram.najm@ecmwf.int>
2025-03-20 16:31:56 +0000

    Testing: ECC-2045 and ECC-2049

--- tests/grib_ecc-2045.cc
+++ tests/grib_ecc-2045.cc
@@ -7,47 +7,67 @@
  * In applying this licence, ECMWF does not waive the privileges and immunities granted to it by
  * virtue of its status as an intergovernmental organisation nor does it submit to any jurisdiction.
  */
+
+ #include <cstdio>
 #include "grib_api_internal.h"
+
+static void check_validity(const grib_handle* handle)
+{
+    ECCODES_ASSERT(handle);
+    long lVal = -1;
+    GRIB_CHECK( grib_get_long(handle, "isMessageValid", &lVal), 0);
+    ECCODES_ASSERT(lVal == 1);
+}
+
+// Headers-only clone, convert to edition 2, then headers-only clone
+static void test1(const char* input)
+{
+    int err = 0;
+    FILE* fin = fopen(input, "rb");
+    ECCODES_ASSERT(fin);
+
+    grib_handle* h1 = grib_handle_new_from_file(0, fin, &err);
+    grib_handle* c = grib_handle_clone_headers_only(h1);
+    GRIB_CHECK( grib_set_long(c, "edition", 2), 0);
+    check_validity(c);
+    grib_handle* c2 = grib_handle_clone_headers_only(c);
+    check_validity(c2);
+
+    grib_handle_delete(h1);
+    grib_handle_delete(c);
+    grib_handle_delete(c2);
+    fclose(fin);
+}
+
+// Normal clone, convert to edition 2, then headers-only clone
+static void test2(const char* input)
+{
+    int err = 0;
+    FILE* fin = fopen(input, "rb");
+    ECCODES_ASSERT(fin);
+
+    grib_handle* h1 = grib_handle_new_from_file(0, fin, &err);
+    ECCODES_ASSERT(h1);
+    grib_handle* c = grib_handle_clone(h1);
+    ECCODES_ASSERT(c);
+    GRIB_CHECK( grib_set_long(c, "edition", 2), 0);
+    check_validity(c);
+    grib_handle* c2 = grib_handle_clone_headers_only(c);
+    check_validity(c2);
+
+    grib_handle_delete(h1);
+    grib_handle_delete(c);
+    grib_handle_delete(c2);
+    fclose(fin);
+}
 
 int main(int argc, char* argv[])
 {
-    int err = 0;
-    long lVal=0;
-
     ECCODES_ASSERT(argc == 2);
 
-    printf("\nDoing file %s...\n", argv[1]);
-    FILE* in  = fopen(argv[1], "rb");
-    ECCODES_ASSERT(in);
+    test1(argv[1]);
+    test2(argv[1]);
 
-    grib_handle* handle = grib_handle_new_from_file(0, in, &err);
-    ECCODES_ASSERT(handle);
-    ECCODES_ASSERT(!err);
-
-    grib_handle* c = grib_handle_clone_headers_only(handle);
-    ECCODES_ASSERT(c);
-    lVal=-1; GRIB_CHECK( grib_get_long(c, "isMessageValid", &lVal), 0); printf("0. isMessageValid=%ld\n", lVal);
-    printf("0. /////// dump the 1st clone (grib1 headers_only)\n");
-    grib_dump_content(c, stdout, "wmo", 0, 0);
-
-    printf("\nSetting edition=2...\n");
-    GRIB_CHECK( grib_set_long(c, "edition", 2), 0);
-    ECCODES_ASSERT( c->sections_count == 8 );
-    printf("1. /////// dump the 1st clone (grib2 headers_only)\n");
-    grib_dump_content(c, stdout, "wmo", 0, 0);
-    lVal=-1; GRIB_CHECK( grib_get_long(c, "isMessageValid", &lVal), 0); printf("1. isMessageValid=%ld\n", lVal);
-
-    printf("\nClone the clone...\n");
-    grib_handle* c2 = grib_handle_clone_headers_only(c);
-    ECCODES_ASSERT(c2);
-    printf("2. /////// dump the 2nd clone (grib2 headers_only)\n");
-    grib_dump_content(c2, stdout, "wmo", 0, 0);
-    lVal=-1; GRIB_CHECK( grib_get_long(c2, "isMessageValid", &lVal), 0); printf("2. isMessageValid=%ld\n", lVal);
-
-    grib_handle_delete(handle);
-    grib_handle_delete(c);
-    grib_handle_delete(c2);
-    fclose(in);
     printf("All OK.\n");
     return 0;
 }
