b5b4c0a2dcfd77d43ec20e84ef2d11ebfb368f31
shahramn <shahram.najm@ecmwf.int>
2025-03-20 14:34:21 +0000

    ECC-2051: GRIB: Getting/setting key 'gridSpec'

--- tests/grib_gridSpec.sh
+++ tests/grib_gridSpec.sh
@@ -15,16 +15,22 @@ if [ $HAVE_GEOGRAPHY -ne 1 ]; then
     exit 0
 fi
 
-if [ $HAVE_ECKIT_GEO -ne 1 ]; then
-    echo "$0: This test is disabled when HAVE_ECKIT_GEO=OFF"
-    exit 0
-fi
-
 label="grib_gridSpec_test"
 tempGrib=temp.$label.grib
 tempFilt=temp.$label.filt
 tempText=temp.$label.txt
 
+# Key is not there for spectral data (only gridded)
+infile=$ECCODES_SAMPLES_PATH/sh_ml_grib2.tmpl
+result=$( ${tools_dir}/grib_get -fp gridSpec $infile )
+[ "$result" = "not_found" ]
+
+if [ $HAVE_ECKIT_GEO -ne 1 ]; then
+    echo "$0: This test is disabled when HAVE_ECKIT_GEO=OFF"
+    exit 0
+fi
+
+# Gridded data
 infile=$ECCODES_SAMPLES_PATH/GRIB2.tmpl
 ${tools_dir}/grib_get -p gridSpec $infile
 grib_check_key_equals $infile gridSpec '{"east":30,"grid":[2,2],"north":60,"shape":[16,31],"south":0,"west":0}'
