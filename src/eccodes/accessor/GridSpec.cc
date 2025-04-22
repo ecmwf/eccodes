71f5fee54e0798c9c6b23be52a0b61ee93361f4f
shahramn <shahram.najm@ecmwf.int>
2025-04-22 15:12:15 +0100

    GridSpec: Warning re not yet implemented

--- src/eccodes/accessor/GridSpec.cc
+++ src/eccodes/accessor/GridSpec.cc
@@ -42,8 +42,14 @@ long GridSpec::get_native_type()
     return GRIB_TYPE_STRING;
 }
 
+static void print_warning_feature_not_implemented()
+{
+    fprintf(stderr, "ECCODES WARNING :  Key gridSpec is not yet implemented. Work in progress...\n");
+}
+
 int GridSpec::pack_string(const char* sval, size_t* len)
 {
+    print_warning_feature_not_implemented();
     return GRIB_NOT_IMPLEMENTED;
 
 #if defined(HAVE_GEOGRAPHY) && defined(HAVE_ECKIT_GEO)
@@ -97,6 +103,7 @@ int GridSpec::unpack_string(char* v, size_t* len)
 
     return GRIB_SUCCESS;
 #else
+    print_warning_feature_not_implemented();
     return GRIB_NOT_IMPLEMENTED;
 #endif
 }
