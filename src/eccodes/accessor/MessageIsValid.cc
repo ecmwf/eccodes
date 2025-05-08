81cce2b1888816e20d4d5080f7203ac61a749802
shahramn <shahram.najm@ecmwf.int>
2025-05-08 11:10:38 +0100

    GRIB message validity checker: Geoiter check

--- src/eccodes/accessor/MessageIsValid.cc
+++ src/eccodes/accessor/MessageIsValid.cc
@@ -309,12 +309,12 @@ int MessageIsValid::check_grid_pl_array()
 
 int MessageIsValid::check_geoiterator()
 {
-    if (handle_->context->debug)
-        fprintf(stderr, "ECCODES DEBUG %s: %s\n", TITLE, __func__);
-
     int err = 0;
 
 #if defined(HAVE_GEOGRAPHY)
+    if (handle_->context->debug)
+        fprintf(stderr, "ECCODES DEBUG %s: %s\n", TITLE, __func__);
+
     grib_iterator* iter = grib_iterator_new(handle_, GRIB_GEOITERATOR_NO_VALUES, &err);
     if (err == GRIB_NOT_IMPLEMENTED || err == GRIB_SUCCESS || err == GRIB_FUNCTIONALITY_NOT_ENABLED) {
         grib_iterator_delete(iter);
