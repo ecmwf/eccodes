f9941f3d01d4460fdb8244d4f8bf4c6a3d531ec2
Eugen Betke <eugen.betke@ecmwf.int>
2025-05-14 12:01:33 +0200

    ECC-2083: Recompute encoding parameters when scaleValuesBy is applied

--- src/eccodes/accessor/ScaleValues.cc
+++ src/eccodes/accessor/ScaleValues.cc
@@ -77,6 +77,20 @@ int ScaleValues::pack_double(const double* val, size_t* len)
         }
     }
 
+    // ECC-2083: GRIB: Applying the scaleValuesBy operation can produce constant fields
+    // Assigning 0 to both decimalScaleFactor and binaryScaleFactor automatically triggers a
+    // recalculation of binaryScaleFactor.
+
+    if ((ret = grib_set_long_internal(h, "decimalScaleFactor", 0) != GRIB_SUCCESS)) {
+        grib_context_free(c, values);
+        return ret;
+    }
+
+    if ((ret = grib_set_long_internal(h, "binaryScaleFactor", 0) != GRIB_SUCCESS)) {
+        grib_context_free(c, values);
+        return ret;
+    }
+
     if ((ret = grib_set_double_array_internal(h, values_, values, size)) != GRIB_SUCCESS) {
         grib_context_free(c, values);
         return ret;
