f45bd9d5a8b84c8d355f35090cd6230dbf70b2c4
shahramn <shahram.najm@ecmwf.int>
2025-05-19 15:28:27 +0100

    Regression test suite: Run the performance tests

--- .github/ci-nightly-test.sh
+++ .github/ci-nightly-test.sh
@@ -24,8 +24,12 @@ git clone https://${GH_TOKEN}@github.com/ecmwf/eccodes-regression-tests.git $reg
 cd $regression_suite_dir
 
 # Launch the regression tests in parallel (This script uses GNU parallel)
-# cd ~masn/REGRESSION_TESTING/ecCodes
 ./par-suite.sh -w $TMPDIR/install/eccodes/$version
+
+# Parallel tests succeeded
+# We should ideally run performance tests MULTIPLE times
+./seq-suite -w $TMPDIR/install/eccodes/$version -t PERFORMANCE
+
 
 # For debugging specific test(s)
 # ./seq-suite.sh -w $TMPDIR/install/eccodes/$version -d -t $test_name
