93979ba9385fce429c5601cc785d5b9b2af570d5
shahramn <shahram.najm@ecmwf.int>
2025-06-16 12:26:35 +0100

    ECC-2100: BUFR: Handle rdbtimeDate edge case

--- src/eccodes/accessor/RdbTimeGuessDate.cc
+++ src/eccodes/accessor/RdbTimeGuessDate.cc
@@ -59,6 +59,16 @@ int RdbTimeGuessDate::unpack_long(long* val, size_t* len)
             rdbMonth = typicalMonth + 1;
         }
     }
+    else if ((rdbDay - typicalDay) >= 27 and typicalDay == 1) {
+        if (typicalMonth == 1) {
+            rdbYear  = typicalYear - 1;
+            rdbMonth = 12;
+        }
+        else {
+            rdbYear  = typicalYear;
+            rdbMonth = typicalMonth - 1;
+        }
+    }
     else {
         rdbYear  = typicalYear;
         rdbMonth = typicalMonth;
