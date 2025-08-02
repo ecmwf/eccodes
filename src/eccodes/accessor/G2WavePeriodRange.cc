cfa96c1d33d638b04afc7cbf575f40e148880afd
shahramn <shahram.najm@ecmwf.int>
2025-08-02 13:12:28 +0100

    GRIB2: PDT selection for wave range period

---
+++ src/eccodes/accessor/G2WavePeriodRange.cc
@@ -0,0 +1,90 @@
+/*
+ * (C) Copyright 2005- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * In applying this licence, ECMWF does not waive the privileges and immunities granted to it by
+ * virtue of its status as an intergovernmental organisation nor does it submit to any jurisdiction.
+ */
+
+#include "G2WavePeriodRange.h"
+
+eccodes::accessor::G2WavePeriodRange _grib_accessor_g2_wave_period_range;
+eccodes::Accessor* grib_accessor_g2_wave_period_range = &_grib_accessor_g2_wave_period_range;
+
+namespace eccodes::accessor
+{
+
+void G2WavePeriodRange::init(const long l, grib_arguments* c)
+{
+    Unsigned::init(l, c);
+    grib_handle* hand = get_enclosing_handle();
+    int n = 0;
+
+    productDefinitionTemplateNumber_ = c->get_name(hand, n++);
+    stepType_ = c->get_name(hand, n++);
+}
+
+int G2WavePeriodRange::unpack_long(long* val, size_t* len)
+{
+    long productDefinitionTemplateNumber = 0;
+    grib_get_long(get_enclosing_handle(), productDefinitionTemplateNumber_, &productDefinitionTemplateNumber);
+    *val = (productDefinitionTemplateNumber == 103 || // instant det
+            productDefinitionTemplateNumber == 104 || // instant ens
+            productDefinitionTemplateNumber == 144 || // interval det
+            productDefinitionTemplateNumber == 145);  // interval ens
+
+    return GRIB_SUCCESS;
+}
+
+int G2WavePeriodRange::pack_long(const long* val, size_t* len)
+{
+    grib_handle* hand                       = get_enclosing_handle();
+    long productDefinitionTemplateNumber    = -1;
+    long productDefinitionTemplateNumberNew = -1;
+    char stepType[15] = {0,};
+    size_t slen = 15;
+    int isInstant = 0;
+    int ret = 0;
+
+    if (grib_get_long(hand, productDefinitionTemplateNumber_, &productDefinitionTemplateNumber) != GRIB_SUCCESS)
+        return GRIB_SUCCESS;
+
+    ret = grib_get_string(hand, stepType_, stepType, &slen);
+    ECCODES_ASSERT(ret == GRIB_SUCCESS);
+
+    const int eps = grib_is_defined(hand, "perturbationNumber");
+
+    if (!strcmp(stepType, "instant"))
+        isInstant = 1;
+
+    if (eps == 0) { // deterministic
+        if (isInstant) {
+            productDefinitionTemplateNumberNew = 103;
+        } else {
+            productDefinitionTemplateNumberNew = 144;
+        }
+    } else {
+        // ensemble
+        if (isInstant) {
+            productDefinitionTemplateNumberNew = 104;
+        } else {
+            productDefinitionTemplateNumberNew = 145;
+        }
+    }
+
+    if (productDefinitionTemplateNumber != productDefinitionTemplateNumberNew) {
+        grib_set_long(hand, productDefinitionTemplateNumber_, productDefinitionTemplateNumberNew);
+    }
+
+    return GRIB_SUCCESS;
+}
+
+int G2WavePeriodRange::value_count(long* count)
+{
+    *count = 1;
+    return 0;
+}
+
+}  // namespace eccodes::accessor
