5b1f6917546f1bd1284775ba1fc1fb02cb6cb6ff
shahramn <shahram.najm@ecmwf.int>
2025-08-18 16:41:05 +0100

    Examples: Further tests

--- examples/C/grib_list.sh
+++ examples/C/grib_list.sh
@@ -9,5 +9,35 @@
 
 . ./include.ctest.sh
 
-${examples_dir}/c_grib_list
+label="examples_c_grib_list"
+tempRef=temp.$label.ref
+tempText=temp.$label.txt
 
+cat > $tempRef <<EOF
+numberOfContributingSpectralBands=9
+count=9
+scaledValueOfCentralWaveNumber 0 = 26870
+scaledValueOfCentralWaveNumber 1 = 9272
+scaledValueOfCentralWaveNumber 2 = MISSING
+scaledValueOfCentralWaveNumber 3 = MISSING
+scaledValueOfCentralWaveNumber 4 = MISSING
+scaledValueOfCentralWaveNumber 5 = MISSING
+scaledValueOfCentralWaveNumber 6 = MISSING
+scaledValueOfCentralWaveNumber 7 = MISSING
+scaledValueOfCentralWaveNumber 8 = MISSING
+Now scaledValueOfCentralWaveNumber 0 = 1000
+Now scaledValueOfCentralWaveNumber 1 = 1001
+Now scaledValueOfCentralWaveNumber 2 = 1002
+Now scaledValueOfCentralWaveNumber 3 = 1003
+Now scaledValueOfCentralWaveNumber 4 = 1004
+Now scaledValueOfCentralWaveNumber 5 = 1005
+Now scaledValueOfCentralWaveNumber 6 = 1006
+Now scaledValueOfCentralWaveNumber 7 = 1007
+Now scaledValueOfCentralWaveNumber 8 = 1008
+EOF
+
+${examples_dir}/c_grib_list > $tempText
+diff $tempRef $tempText
+
+# Clean up
+rm -f $tempText $tempRef
