c5f6ac9c45b1d8c1e1f67b7e2ffe9322994f2c63
Shahram Najm <shahram.najm@ecmwf.int>
2025-09-09 16:09:30 +0100

    Fix bash error

--- tests/grib_uerra.sh
+++ tests/grib_uerra.sh
@@ -129,7 +129,7 @@ result=$(${tools_dir}/grib_get -fp mars.time,mars.step $temp1)
 # ----------
 # types em/es for class=rr and expver=prod/test (and suiteName=se-al-ec though it works generally)
 for pspd in 10 11 ; do
-  if [[ $pspd -eq 10 ]] ; then
+  if [ $pspd -eq 10 ]; then
     expver=prod
   else
     expver=test
