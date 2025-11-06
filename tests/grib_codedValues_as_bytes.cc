7ed8244b6b57c38a7b110603ff8908fb501d0a28
shahramn <shahram.najm@ecmwf.int>
2025-11-06 16:46:23 +0000

    Testing: codedValues as raw bytes (xfer all meta-data)

--- tests/grib_codedValues_as_bytes.cc
+++ tests/grib_codedValues_as_bytes.cc
@@ -34,12 +34,22 @@ int main(int argc, char** argv)
     assert(h);
     fclose(in);
 
+    // TODO assert grib edition == 2
+    // TODO check isGridded == 1
+    // TODO check packingType == grid_ccsds
+    // TODO check NO BITMAP
+
     // Get meta-data from input field
     double referenceValue = 0;
     long bitsPerValue = 0, ccsdsFlags = 0, numberOfValues = 0, numberOfDataPoints = 0;
+    long binaryScaleFactor = 0, decimalScaleFactor = 0, ccsdsBlockSize = 0, ccsdsRsi = 0;
     GRIB_CHECK(codes_get_double(h, "referenceValue", &referenceValue), 0);
+    GRIB_CHECK(codes_get_long(h, "binaryScaleFactor", &binaryScaleFactor), 0);
+    GRIB_CHECK(codes_get_long(h, "decimalScaleFactor", &decimalScaleFactor), 0);
     GRIB_CHECK(codes_get_long(h, "bitsPerValue", &bitsPerValue), 0);
     GRIB_CHECK(codes_get_long(h, "ccsdsFlags", &ccsdsFlags), 0);
+    GRIB_CHECK(codes_get_long(h, "ccsdsBlockSize", &ccsdsBlockSize), 0);
+    GRIB_CHECK(codes_get_long(h, "ccsdsRsi", &ccsdsRsi), 0);
     GRIB_CHECK(codes_get_long(h, "numberOfValues", &numberOfValues), 0);
     GRIB_CHECK(codes_get_long(h, "numberOfDataPoints", &numberOfDataPoints), 0);
 
@@ -49,12 +59,12 @@ int main(int argc, char** argv)
     long sec7Len            = -1;
 
     // CODES_CHECK(codes_get_length(h, "codedValues", &klen), 0); // no good, always 1025!
-    CODES_CHECK(codes_get_long(h, "section7Length", &sec7Len), 0);
+    CODES_CHECK(codes_get_long(h, "section7Length", &sec7Len), 0); //grib2 only
     printf("1. codes_get_length of sec7 = %zu\n", sec7Len);
 
     blen     = sec7Len;
     byte_val = (unsigned char*)malloc(blen * sizeof(unsigned char));
-    GRIB_CHECK(codes_get_bytes(h, "codedValues", byte_val, &blen), 0);
+    GRIB_CHECK(codes_get_bytes(h, "codedValues", byte_val, &blen), 0);//magic
     printf("1. codes_get_bytes blen = %zu\n", blen);
 
     FILE* fout = fopen(out_binfile, "wb");
@@ -84,10 +94,6 @@ int main(int argc, char** argv)
     }
     fclose(f);
     len = fsize;
-    // GRIB_CHECK( codes_set_bytes(h, "codedValues", p, &len), 0);
-    // CODES_CHECK(codes_write_message(h, "recoded1.grib", "w"), 0);
-    // codes_handle* h1 = codes_grib_handle_new_from_samples(0, "GRIB2");
-    // assert(h1);
     in = fopen(in_sample, "rb");
     assert(in);
     codes_handle* h1 = codes_handle_new_from_file(0, in, PRODUCT_GRIB, &err);
@@ -95,30 +101,28 @@ int main(int argc, char** argv)
     len = fsize;
     grib_context_set_debug(ctx, -1);
     GRIB_CHECK(codes_set_long(h1, "bitsPerValue", bitsPerValue), 0);
+    GRIB_CHECK(codes_set_long(h1, "decimalScaleFactor", decimalScaleFactor), 0);
 
-    // Normally one cannot set the referenceValue key as it is READ ONLY
-    // GRIB_CHECK( grib_set_double(h1, "referenceValue", referenceValue), 0);
+    // Normally one cannot set the read_only keys like referenceValue and binaryScaleFactor.
+    // e.g., GRIB_CHECK( grib_set_double(h1, "referenceValue", referenceValue), 0);
     // Workaround: Resort to trick and force it via grib_set_force_double_array
     grib_set_force_double_array(h1, "referenceValue", &referenceValue, 1);
+    double bsf = binaryScaleFactor;
+    grib_set_force_double_array(h1, "binaryScaleFactor", &bsf, 1);
 
     GRIB_CHECK(codes_set_long(h1, "ccsdsFlags", ccsdsFlags), 0);
+    GRIB_CHECK(codes_set_long(h1, "ccsdsBlockSize", ccsdsBlockSize), 0);
+    GRIB_CHECK(codes_set_long(h1, "ccsdsRsi", ccsdsRsi), 0);
     GRIB_CHECK(codes_set_long(h1, "numberOfValues", numberOfValues), 0);
     GRIB_CHECK(codes_set_long(h1, "numberOfDataPoints", numberOfDataPoints), 0);
-    GRIB_CHECK(codes_set_bytes(h1, "codedValues", p, &len), 0);
+    GRIB_CHECK(codes_set_bytes(h1, "codedValues", p, &len), 0);//magic
 
     CODES_CHECK(codes_write_message(h1, out_filename, "w"), 0);
     grib_context_set_debug(ctx, 0);
     codes_handle_delete(h1);
 
-    // size_t len = blen;
-    // byte_val[2]=0; // test for simple packing only
-    // byte_val[3]=0;
-    // GRIB_CHECK( codes_set_bytes(h, "codedValues", byte_val, &len), 0);
-    // CODES_CHECK(codes_write_message(h, "recoded2.grib", "w"), 0);
-
     free(data);
     free(byte_val);
-
     codes_handle_delete(h);
 
     return 0;
