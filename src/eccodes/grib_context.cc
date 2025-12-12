390fa4ec4584adc3a2ae14c712f938ae3ee60dd6
Eugen Betke <eugen.betke@ecmwf.int>
2025-12-12 13:49:26 +0100

    ECC-2142: Thread-safety modernisation

--- src/eccodes/grib_context.cc
+++ src/eccodes/grib_context.cc
@@ -44,6 +44,8 @@ static eccodes::sync::Mutex mutex;
 
 #else
 
+static eccodes::sync::Mutex mutex_c;
+
 static void default_long_lasting_free(const grib_context* c, void* p)
 {
     free(p);
@@ -212,18 +214,13 @@ void grib_context_set_data_quality_checks(grib_context* c, int val)
     // If val == 2, failure results in a warning
     ECCODES_ASSERT(val == 0 || val == 1 || val == 2);
 
-    GRIB_MUTEX_INIT_ONCE(&once, &init_mutex);
-    GRIB_MUTEX_LOCK(&mutex_c);
+    eccodes::sync::LockGuard<eccodes::sync::Mutex> lock(mutex_c);
     c->grib_data_quality_checks = val;
-    GRIB_MUTEX_UNLOCK(&mutex_c);
 }
 int grib_context_get_data_quality_checks(const grib_context* c)
 {
-    GRIB_MUTEX_INIT_ONCE(&once, &init_mutex);
-    GRIB_MUTEX_LOCK(&mutex_c);
-    int result = c->grib_data_quality_checks;
-    GRIB_MUTEX_UNLOCK(&mutex_c);
-    return result;
+    eccodes::sync::LockGuard<eccodes::sync::Mutex> lock(mutex_c);
+    return c->grib_data_quality_checks;
 }
 
 void grib_context_set_debug(grib_context* c, int mode)
