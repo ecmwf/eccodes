969c5e68a6b2e1c7be6aed0e676702c14612ae4e
shahramn <shahram.najm@ecmwf.int>
2026-02-20 17:10:44 +0000

    Improved error message when concept has too many values

--- src/eccodes/accessor/Concept.cc
+++ src/eccodes/accessor/Concept.cc
@@ -352,7 +352,7 @@ static bool blacklisted(grib_handle* h, long edition, const char* concept_name, 
     return false;
 }
 
-#define MAX_NUM_CONCEPT_VALUES 40
+#define MAX_NUM_CONCEPT_VALUES 50
 
 static void print_user_friendly_message(grib_handle* h, const char* name, grib_concept_value* concepts, grib_action* act)
 {
@@ -391,16 +391,19 @@ static void print_user_friendly_message(grib_handle* h, const char* name, grib_c
     }
 
     // Create a list of all possible values for this concept and sort it
+    bool hasMore = false;
     while (pCon) {
-        if (i >= MAX_NUM_CONCEPT_VALUES)
+        if (i >= MAX_NUM_CONCEPT_VALUES) {
+            hasMore = true;
             break;
+        }
         all_concept_vals[i++] = pCon->name;
         pCon                  = pCon->next;
     }
     concept_count = i;
     // Only print out all concepts if fewer than MAX_NUM_CONCEPT_VALUES.
     // Printing out all values for concepts like paramId would be silly!
-    if (concept_count <= MAX_NUM_CONCEPT_VALUES) {
+    if (!hasMore) {
         fprintf(stderr, "Here are some possible values for concept %s:\n", act->name_);
         qsort(&all_concept_vals, concept_count, sizeof(char*), cmpstringp);
         for (i = 0; i < concept_count; ++i) {
