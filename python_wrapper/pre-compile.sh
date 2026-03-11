e85c50c6a7ff388d44a7379a389b803605a23a37
Vojta Tuma <vojta.tuma@gmail.com>
2026-03-11 11:03:09 +0100

    [python] switch aec to cxx-deps

--- python_wrapper/pre-compile.sh
+++ python_wrapper/pre-compile.sh
@@ -47,23 +47,19 @@ if [ "$(uname)" != "Darwin" ] ; then
     cat /tmp/openjpeg/version.txt >> python_wrapper/src/versions.txt
 
     ### libaec
-    # NOTE we need at least 1.1.1 due to gribjump, thats why no dnf install atm
-    mkdir -p /tmp/libaec/build
-    wget https://gitlab.dkrz.de/k202009/libaec/-/archive/v1.1.3/libaec-v1.1.3.tar.gz -O /tmp/libaec.tar.gz
-    tar -xzf /tmp/libaec.tar.gz -C /tmp
-    mkdir /tmp/libaec-v1.1.3/build && cd /tmp/libaec-v1.1.3/build
-    cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/tmp/libaec ..
-    make install
-    cd -
-    echo "libaec v1.1.3" >> python_wrapper/src/versions.txt
+    # comes from cxx-deps
 
     # copy the libs, instead of having auditwheel done it later. This is a bit risky because cmake will later write in this
     # very directory... but it works
+    # png
     cp /lib64/libpng16.so.16 /tmp/eccodes/target/eccodes/lib64/libpng16.so.16
+    # openjpeg
     cp /tmp/openjpeg/target/lib64/libopenjp2.so.7 /tmp/eccodes/target/eccodes/lib64/libopenjp2.so.7
-    cp /tmp/libaec/lib64/libaec.so* /tmp/eccodes/target/eccodes/lib64
-    cp /tmp/libaec/include/libaec.h /tmp/eccodes/target/eccodes/include
-    cp /tmp/libaec/cmake/* /tmp/eccodes/target/eccodes/cmake
+
+    # aec -- needs headers and cmake due to dependent on by eg gribjump
+    cp /cxx-deps/lib64/libaec.so* /tmp/eccodes/target/eccodes/lib64
+    cp /cxx-deps/include/libaec.h /tmp/eccodes/target/eccodes/include
+    cp /cxx-deps/lib64/cmake/libaec/* /tmp/eccodes/target/eccodes/cmake
 
 
 else
@@ -71,25 +67,33 @@ else
     mkdir -p /tmp/eccodes/target/eccodes/lib/
 
     BREWBASE="$(brew --cellar)" # "/opt/homebrew/Cellar" or "/usr/local/Cellar"
-    LIBAEC="$(ls -d $BREWBASE/libaec/* | head -n1)"
     LIBOPENJP="$(ls -d $BREWBASE/openjpeg/* | head -n1)"
     LIBPNG="$(ls -d $BREWBASE/libpng/* | head -n1)"
 
-    for lib in $LIBAEC $LIBOPENJP $LIBPNG ; do
+    for lib in $LIBOPENJP $LIBPNG ; do
         echo "will copy from $lib/lib with contents $(ls $lib/lib/*dylib)"
         cp $lib/lib/*dylib /tmp/eccodes/target/eccodes/lib
         echo "will copy from $lib/include with contents $(ls $lib/include)"
         cp -R $lib/include/* /tmp/eccodes/target/eccodes/include
     done
-    rm /tmp/eccodes/target/eccodes/lib/libsz* # comes from the openjp, but we dont need it
+
+
+    # aec -- needs headers and cmake due to dependent on by eg gribjump
+    cp /tmp/cxx-deps/lib/libaec*dylib /tmp/eccodes/target/eccodes/lib
+    cp /tmp/cxx-deps/include/libaec.h /tmp/eccodes/target/eccodes/include
+    cp /tmp/cxx-deps/lib/cmake/libaec/* /tmp/eccodes/target/eccodes/cmake
 
 fi
+
+
+echo "libaec v1.1.4" >> python_wrapper/src/versions.txt # TODO read from somewhere... there aint any VERSION file, needs to parse
 
 echo "license setup"
 
 ## licenses
 wget https://raw.githubusercontent.com/glennrp/libpng/libpng16/LICENSE -O python_wrapper/src/copying/libpng.txt
 wget https://raw.githubusercontent.com/uclouvain/openjpeg/master/LICENSE -O python_wrapper/src/copying/libopenjpeg.txt
+# TODO switch libaec license to the checked out by cxxdeps
 wget https://raw.githubusercontent.com/MathisRosenhauer/libaec/master/LICENSE.txt -O python_wrapper/src/copying/libaec.txt
 cp LICENSE python_wrapper/src/copying/libeccodes.txt
 echo '{"libeccodes": {"path": "copying/libeccodes.txt", "home": "https://github.com/ecmwf/eccodes"}, "libaec": {"path": "copying/libaec.txt", "home": "https://github.com/MathisRosenhauer/libaec"}, "home": "https://github.com/uclouvain/openjpeg"}, "libpng": {"path": "copying/libpng.txt", "home": "https://github.com/glennrp/libpng"}}' > python_wrapper/src/copying/list.json
