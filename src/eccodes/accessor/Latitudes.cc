2bf692ca0dd184ea48fa76186c73f13bc4cae863
Shahram Najm <masn@ecmwf.int>
2026-04-15 11:37:43 +0000

    ECC-2257: GRIB: cannot get latitudes from ORCA grid

--- src/eccodes/accessor/Latitudes.cc
+++ src/eccodes/accessor/Latitudes.cc
@@ -94,6 +94,7 @@ int Latitudes::unpack_double(double* val, size_t* len)
     // ECC-1525 Performance: We do not need the values to be decoded
     iter = grib_iterator_new(get_enclosing_handle(), GRIB_GEOITERATOR_NO_VALUES, &ret);
     if (ret != GRIB_SUCCESS) {
+        *len = 0;
         grib_iterator_delete(iter);
         grib_context_log(context_, GRIB_LOG_ERROR, "latitudes: Unable to create iterator");
         return ret;
