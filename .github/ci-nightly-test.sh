15e51e0452e34c8bab95e45acf1c78c4576c7777
Eugen Betke <eugen.betke@mailbox.org>
2026-05-21 14:25:07 +0200

    Split the nightly workflow in testing and benchmarks workflows

--- .github/ci-nightly-test.sh
+++ .github/ci-nightly-test.sh
@@ -2,6 +2,21 @@
 
 set -eu
 
+usage() {
+    echo "Usage: $0 <testing|performance>"
+    exit 1
+}
+
+if [[ $# -ne 1 ]]; then
+    usage
+fi
+
+mode="$1"
+if [[ "$mode" != "testing" && "$mode" != "performance" ]]; then
+    echo "Error: unknown mode '$mode'"
+    usage
+fi
+
 # We do not want to come across the ecCodes tools in the toolbox
 module unload ecmwf-toolbox
 
@@ -9,8 +24,11 @@ module load cdo/new
 module load numdiff
 module load nccmp
 module load netcdf4
-module load gnuparallel/new
 module load python3
+
+if [[ "$mode" == "testing" ]]; then
+    module load gnuparallel/new
+fi
 
 version=$(cat $TMPDIR/eccodes/VERSION)
 
@@ -23,15 +41,15 @@ mkdir -p $regression_suite_dir
 git clone https://${GH_TOKEN}@github.com/ecmwf/eccodes-regression-tests.git $regression_suite_dir
 cd $regression_suite_dir
 
-# Launch the regression tests in parallel (This script uses GNU parallel)
-echo "Running ./par-suite.sh -w $TMPDIR/install/eccodes/$version"
-./par-suite.sh -w $TMPDIR/install/eccodes/$version
-
-# Parallel tests succeeded
-# We should ideally run performance tests MULTIPLE times
-echo "Running ./seq-suite.sh -w $TMPDIR/install/eccodes/$version -t PERFORMANCE"
-./seq-suite.sh -w $TMPDIR/install/eccodes/$version -t PERFORMANCE
-
+if [[ "$mode" == "testing" ]]; then
+    # Launch the regression tests in parallel (This script uses GNU parallel)
+    echo "Running ./par-suite.sh -w $TMPDIR/install/eccodes/$version"
+    ./par-suite.sh -w $TMPDIR/install/eccodes/$version
+elif [[ "$mode" == "performance" ]]; then
+    # We should ideally run performance tests MULTIPLE times
+    echo "Running ./seq-suite.sh -w $TMPDIR/install/eccodes/$version -t PERFORMANCE"
+    ./seq-suite.sh -w $TMPDIR/install/eccodes/$version -t PERFORMANCE
+fi
 
 # For debugging specific test(s)
 # ./seq-suite.sh -w $TMPDIR/install/eccodes/$version -d -t $test_name
