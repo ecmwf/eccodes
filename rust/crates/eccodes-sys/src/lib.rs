bccc3be1868cbdf1a3086fc79fffc3a16b2b1172
Vlad Pankratov <vlad@oxidian.com>
2026-06-12 01:26:27 +0200

    Add additional Clippy lint allowances

--- rust/crates/eccodes-sys/src/lib.rs
+++ rust/crates/eccodes-sys/src/lib.rs
@@ -8,5 +8,9 @@
 #![allow(clippy::missing_safety_doc)]
 #![allow(clippy::too_many_arguments)]
 #![allow(clippy::upper_case_acronyms)]
+#![allow(clippy::doc_markdown)]
+#![allow(clippy::unreadable_literal)]
+#![allow(clippy::pub_underscore_fields)]
+#![allow(clippy::items_after_statements)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
