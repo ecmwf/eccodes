b025ab675305d7ecb5d89088f5159bcd18e0dab1
Richard Mladek <richard.mladek@ecmwf.int>
2026-08-25 10:56:23 +0000

    cleanup and test amendments

--- tests/grib_lc-gcr.sh
+++ tests/grib_lc-gcr.sh
@@ -7,11 +7,21 @@
 # In applying this licence, ECMWF does not waive the privileges and immunities granted to it by
 # virtue of its status as an intergovernmental organisation nor does it submit to any jurisdiction.
 #
+# ---------------------------------------------------------
+# This is the test for JIRA issues:
+#     ECC-2004 MARS: Add new class 'lr' for WMO Lead Centre for Global Climate Re-Analyses (new MARS class support)
+#     ECC-2069 GRIB2: Update Code Table 1.3 for WMO Lead Centre for Global Climate Re-Analyses (new WMO codes support)
+#     ECC-2270 GRIB2: LC-GCR and WPMIP update (MARS support)
+#     ECC-2314 grib: LC-GCR project test case and other updates (ecCodes test etc)
+#     ECC-2297 GRIB2: Updates for LC-GCR (Support for the keyword model)
+# Parent data governance ticket with all details (Jira epic): DGOV-513
+# ---------------------------------------------------------
 
 . ./include.ctest.sh
 
 grib2_sample=$ECCODES_SAMPLES_PATH/lc-gcr_sttd_grib2.tmpl
 label="grib_lc-gcr_test"
+
 temp1=temp1.${label}.grib2
 temp2=temp2.${label}.grib2
 temp3=temp3.${label}.grib2
@@ -28,16 +38,16 @@ test_stream_and_type()
         pspd=14
     elif [ "$expver" = "test" ]; then
         pspd=15
+    else
+        echo "Abort: expver not prod/test (i.e. productionStatusOfProcessedData not 14/15)!"
+        exit -1
     fi
 
-    label="grib_lcgcr_test"
-    tempFilt=temp.${label}.filt
-
-    # create local section and use ccsds compression
-    ${tools_dir}/grib_set -s tablesVersion=$tablesVersionLatest \
+    # change productionStatusOfProcessedData
+    ${tools_dir}/grib_set -s tablesVersion=$tablesVersionLatest,productionStatusOfProcessedData=$pspd \
                            $grib2_sample $temp2
 
-    grib_check_key_equals $temp2 'class,centre,centre:i,model,stream,mars.stream,type,stattype,timespan,param' 'lr ecmf 98 ERA5 sttd sttd fc moav 24h 235055'
+    grib_check_key_equals $temp2 'class,expver,centre,centre:i,model,stream,mars.stream,type,stattype,timespan,param' "lr $expver ecmf 98 ERA5 sttd sttd fc moav 24h 235055"
     
     # Check mars domain is not set
     key=`${tools_dir}/grib_get -f -p mars.domain $temp2`
@@ -45,18 +55,18 @@ test_stream_and_type()
 
     # change to type=an
     ${tools_dir}/grib_set -s typeOfProcessedData=0 $temp2 $temp3
-    grib_check_key_equals $temp3 'class,model,stream,mars.stream,type,stattype,timespan,param' 'lr ERA5 sttd sttd an moav 24h 235055'
+    grib_check_key_equals $temp3 'class,expver,model,stream,mars.stream,type,stattype,timespan,param' "lr $expver ERA5 sttd sttd an moav 24h 235055"
 
     # test LC-GCR model options
     #   https://jira.ecmwf.int/browse/ECC-2297
     ${tools_dir}/grib_set -s centre=babj,backgroundProcess=1,generatingProcessIdentifier=1 $temp2 $temp3
-    grib_check_key_equals $temp3 'class,centre,centre:i,model,stream,type,stattype,timespan,param' 'lr babj 38 CRA-40 sttd fc moav 24h 235055'
+    grib_check_key_equals $temp3 'class,expver,centre,centre:i,model,stream,type,stattype,timespan,param' "lr $expver babj 38 CRA-40 sttd fc moav 24h 235055"
 
     ${tools_dir}/grib_set -s centre=rjtd,backgroundProcess=2,generatingProcessIdentifier=1 $temp2 $temp3
-    grib_check_key_equals $temp3 'class,centre,centre:i,model,stream,type,stattype,timespan,param' 'lr rjtd 34 JRA-3Q sttd fc moav 24h 235055'
+    grib_check_key_equals $temp3 'class,expver,centre,centre:i,model,stream,type,stattype,timespan,param' "lr $expver rjtd 34 JRA-3Q sttd fc moav 24h 235055"
 
     ${tools_dir}/grib_set -s centre=nasa,backgroundProcess=3,generatingProcessIdentifier=1 $temp2 $temp3
-    grib_check_key_equals $temp3 'class,centre,centre:i,model,stream,type,stattype,timespan,param' 'lr nasa 173 MERRA-2 sttd fc moav 24h 235055'
+    grib_check_key_equals $temp3 'class,expver,centre,centre:i,model,stream,type,stattype,timespan,param' "lr $expver nasa 173 MERRA-2 sttd fc moav 24h 235055"
 
 }
 
@@ -65,4 +75,4 @@ test_stream_and_type 'test'
 
 
 # Clean up
-#rm -f $temp1 $temp2 $temp3 $tempLog
+rm -f $temp1 $temp2 $temp3 $tempLog
