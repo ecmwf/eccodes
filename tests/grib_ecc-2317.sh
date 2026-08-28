1645103406099b06ec8a3009466f67764e40d1cd
Eugen Betke <eugen.betke@ecmwf.int>
2026-08-28 17:39:48 +0200

    ECC-2317: Add JSON parsing test

---
+++ tests/grib_ecc-2317.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+# (C) Copyright 2005- ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# 
+# In applying this licence, ECMWF does not waive the privileges and immunities granted to it by
+# virtue of its status as an intergovernmental organisation nor does it submit to any jurisdiction.
+#
+
+. ./include.ctest.sh
+
+# ---------------------------------------------------------
+# This is the test for JIRA issue ECC-XXXX
+# < Add issue summary here >
+# ---------------------------------------------------------
+
+if [ ${HAVE_ECKIT_GEO} -ne 1 ]
+then
+    echo "$0: This test is disabled when HAVE_ECKIT_GEO=OFF"
+    exit 0
+fi
+
+if [ "${ECCODES_ECKIT_GEO:-0}" -eq 0 ]
+then
+    echo "$0: This test is disabled (env. variable ECCODES_ECKIT_GEO=0)"
+    exit 0
+fi
+
+REDIRECT=/dev/null
+
+label=`basename $0 | sed -e 's/\.sh/_test/'`
+
+tempGrib=temp.$label.grib
+sample_grib2=$ECCODES_SAMPLES_PATH/sh_ml_grib2.tmpl
+
+${tools_dir}/grib_set -s gridSpec='{grid:[0,0]}' ${sample_grib2} $tempGrib
+# check return code
+if [ $? -ne 0 ]; then
+    echo "grib_set failed"
+    exit 1
+fi
+
+
+# Clean up
+rm -f $tempGrib
