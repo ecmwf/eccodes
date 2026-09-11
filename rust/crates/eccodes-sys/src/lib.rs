096b2f5c9a411a6a249ab379f59db05d90f47be8
Vlad Pankratov <vlad@oxidian.com>
2026-09-11 16:15:01 +0200

    Add allowance for unnecessary transmute warnings in lib.rs

--- rust/crates/eccodes-sys/src/lib.rs
+++ rust/crates/eccodes-sys/src/lib.rs
@@ -5,6 +5,7 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+#![allow(unnecessary_transmutes)]
 #![allow(clippy::missing_safety_doc)]
 #![allow(clippy::too_many_arguments)]
 #![allow(clippy::upper_case_acronyms)]
